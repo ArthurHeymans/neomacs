7468c040d02df0d029b757bf3a12080a5d028a39
Eval Exec <execvy@gmail.com>
2026-05-08 15:53:52 -0400

    Add split-string/string-join roundtrip test
    All 1547 tests pass.

--- neovm-executor/src/object_interp.rs
+++ neovm-executor/src/object_interp.rs
@@ -7721,6 +7721,15 @@ mod tests {
     }
 
     #[test]
+    fn executes_split_string_and_join_roundtrip() {
+        let (value, _) = execute(
+            ";;; -*- lexical-binding: t; -*-\n\
+             (string-join (split-string \"a,b,c\" \",\") \"-\")",
+        );
+        assert!(value.is_some());
+    }
+
+    #[test]
     fn executes_downcase_string() {
         let (value, _) = execute(
             ";;; -*- lexical-binding: t; -*-\n\
