7e6b9f72bc8ae3a810c4e1e4aab7598cee94372f
Eval Exec <execvy@gmail.com>
2026-05-09 17:53:38 -0400

    Add more GNU regexp divergence probes

--- neovm-oracle-tests/src/regexp-gnu-divergence.rs
+++ neovm-oracle-tests/src/regexp-gnu-divergence.rs
@@ -46,6 +46,79 @@ fn oracle_prop_regexp_gnu_backslash_d_is_literal() {
 }
 
 #[test]
+#[ignore = "known divergence: GNU Emacs treats escaped control-letter names as literals in regexps"]
+fn oracle_prop_regexp_gnu_escaped_control_letters_are_literals() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    let form = r#"(let ((probe (lambda (regexp string)
+                       (let ((pos (string-match regexp string)))
+                         (list regexp string pos
+                               (and pos (match-string 0 string)))))))
+      (list
+       (funcall probe "\\t" "t")
+       (funcall probe "\\t" "\t")
+       (funcall probe "\\n" "n")
+       (funcall probe "\\n" "\n")
+       (funcall probe "\\r" "r")
+       (funcall probe "\\r" "\r")
+       (funcall probe "\\f" "f")
+       (funcall probe "\\f" "\f")
+       (funcall probe "\\a" "a")
+       (funcall probe "\\a" (string 7))
+       (funcall probe "\\e" "e")
+       (funcall probe "\\e" (string 27))))"#;
+    assert_oracle_parity_with_bootstrap(form);
+}
+
+#[test]
+#[ignore = "known divergence: GNU Emacs string-match does not use \\= as a string-start assertion"]
+fn oracle_prop_regexp_gnu_at_point_anchor_is_not_for_string_match() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    let form = r#"(let ((probe (lambda (regexp string &optional start)
+                       (let ((pos (string-match regexp string start)))
+                         (list regexp string start pos
+                               (and pos (match-string 0 string)))))))
+      (list
+       (funcall probe "\\=" "")
+       (funcall probe "\\=" "abc")
+       (funcall probe "a\\=b" "ab")
+       (funcall probe "\\=b" "ab" 1)))"#;
+    assert_oracle_parity_with_bootstrap(form);
+}
+
+#[test]
+#[ignore = "known divergence: GNU Emacs treats interval syntax without a preceding atom as literal text"]
+fn oracle_prop_regexp_gnu_bare_intervals_are_literals() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    let form = r#"(let ((probe (lambda (regexp string)
+                       (let ((pos (string-match regexp string)))
+                         (list regexp string pos
+                               (and pos (match-string 0 string)))))))
+      (list
+       (funcall probe "\\{1\\}" "{1}")
+       (funcall probe "\\{1,2\\}" "{1,2}")
+       (funcall probe "\\{,2\\}" "{,2}")))"#;
+    assert_oracle_parity_with_bootstrap(form);
+}
+
+#[test]
+#[ignore = "known divergence: GNU Emacs rejects malformed \\_ symbol-boundary escapes"]
+fn oracle_prop_regexp_gnu_malformed_symbol_boundary_errors() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    let form = r#"(list
+      (condition-case err
+          (string-match "\\_x" "_x")
+        (error (list :error (car err) (cadr err))))
+      (condition-case err
+          (string-match "\\_" "_")
+        (error (list :error (car err) (cadr err)))))"#;
+    assert_oracle_parity_with_bootstrap(form);
+}
+
+#[test]
 #[ignore = "known divergence: Neomacs uses approximate built-in category predicates"]
 fn oracle_prop_regexp_gnu_category_tables() {
     return_if_neovm_enable_oracle_proptest_not_set!();
