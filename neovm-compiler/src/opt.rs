84467a53cf04c97d16bd1760690fbd5037d31f2a
Eval Exec <execvy@gmail.com>
2026-05-09 19:34:47 -0400

    Add char-to-string constant folding

--- neovm-compiler/src/opt.rs
+++ neovm-compiler/src/opt.rs
@@ -536,6 +536,10 @@ fn try_fold_call_named(name: &str, args: &[&SsaConst]) -> Option<SsaConst> {
             }
             _ => None,
         },
+        "char-to-string" if args.len() == 1 => match args[0] {
+            SsaConst::Char(c) => Some(SsaConst::String(char::from_u32(*c as u32)?.to_string())),
+            _ => None,
+        },
         "string-to-number" if args.len() >= 1 => match args[0] {
             SsaConst::String(s) => {
                 if let Ok(n) = s.parse::<i64>() {
