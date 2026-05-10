38dbc1dc2cfb7d19ac443f5bc37f7a6d71a0c202
Eval Exec <execvy@gmail.com>
2026-05-10 17:42:19 -0400

    Add test for cl-find nil sequence behavior

--- neovm-executor/src/object_interp.rs
+++ neovm-executor/src/object_interp.rs
@@ -13843,4 +13843,14 @@ mod tests {
         );
         assert_eq!(value, Some(LispValue::NIL));
     }
+
+    #[test]
+    fn executes_cl_find_returns_nil_for_nil_seq() {
+        let (value, _) = execute(
+            ";;; -*- lexical-binding: t; -*-\n\
+             (require 'cl-lib)\n\
+             (cl-find 42 nil)",
+        );
+        assert_eq!(value, Some(LispValue::NIL));
+    }
 }
