ff11c58bf0a933e353a6b8a6abfd33906a8ba927
Eval Exec <execvy@gmail.com>
2026-05-09 22:02:00 -0400

    Add atan 2-arg constant folding

--- neovm-compiler/src/opt.rs
+++ neovm-compiler/src/opt.rs
@@ -725,6 +725,13 @@ fn try_fold_call_named(name: &str, args: &[&SsaConst]) -> Option<SsaConst> {
             SsaConst::Int(n) => Some(SsaConst::Float((*n as f64).atan())),
             _ => None,
         },
+        "atan" if args.len() == 2 => match (args[0], args[1]) {
+            (SsaConst::Float(y), SsaConst::Float(x)) => Some(SsaConst::Float(y.atan2(*x))),
+            (SsaConst::Float(y), SsaConst::Int(x)) => Some(SsaConst::Float(y.atan2(*x as f64))),
+            (SsaConst::Int(y), SsaConst::Float(x)) => Some(SsaConst::Float((*y as f64).atan2(*x))),
+            (SsaConst::Int(y), SsaConst::Int(x)) => Some(SsaConst::Float((*y as f64).atan2(*x as f64))),
+            _ => None,
+        },
         "sqrt" if args.len() == 1 => match args[0] {
             SsaConst::Float(f) if *f >= 0.0 => Some(SsaConst::Float(f.sqrt())),
             SsaConst::Int(n) if *n >= 0 => Some(SsaConst::Float((*n as f64).sqrt())),
