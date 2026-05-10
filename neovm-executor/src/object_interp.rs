9afdc688444ae0530d9fa7b2f15f5f46c9bb54cb
Eval Exec <execvy@gmail.com>
2026-05-10 18:23:47 -0400

    Add test for cl-delete-duplicates nil behavior

--- neovm-executor/src/object_interp.rs
+++ neovm-executor/src/object_interp.rs
@@ -13913,4 +13913,14 @@ mod tests {
         );
         assert_eq!(value, Some(LispValue::NIL));
     }
+
+    #[test]
+    fn executes_cl_delete_duplicates_returns_nil_for_nil() {
+        let (value, _) = execute(
+            ";;; -*- lexical-binding: t; -*-\n\
+             (require 'cl-lib)\n\
+             (cl-delete-duplicates nil)",
+        );
+        assert_eq!(value, Some(LispValue::NIL));
+    }
 }
