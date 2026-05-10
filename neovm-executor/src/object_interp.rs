3c5bd6ec0b5c7185da3115871220fe28c3ec2f70
Eval Exec <execvy@gmail.com>
2026-05-10 01:06:50 -0400

    Add test for cdr-safe on non-cons

--- neovm-executor/src/object_interp.rs
+++ neovm-executor/src/object_interp.rs
@@ -12534,6 +12534,15 @@ mod tests {
     }
 
     #[test]
+    fn executes_cdr_safe_on_non_cons_returns_nil() {
+        let (value, _) = execute(
+            ";;; -*- lexical-binding: t; -*-\n\
+             (cdr-safe 99)",
+        );
+        assert_eq!(value, Some(LispValue::NIL));
+    }
+
+    #[test]
     fn executes_make_string_creates_char_repeat() {
         let (value, _) = execute(
             ";;; -*- lexical-binding: t; -*-\n\
