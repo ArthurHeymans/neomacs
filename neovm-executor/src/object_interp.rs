e336bea4389bdce4a1308c02b9af438a8f45917c
Eval Exec <execvy@gmail.com>
2026-05-10 16:52:00 -0400

    Add test for cl-nintersection nil list behavior

--- neovm-executor/src/object_interp.rs
+++ neovm-executor/src/object_interp.rs
@@ -13698,4 +13698,14 @@ mod tests {
         );
         assert_eq!(value, Some(LispValue::NIL));
     }
+
+    #[test]
+    fn executes_cl_nintersection_returns_nil_for_nil() {
+        let (value, _) = execute(
+            ";;; -*- lexical-binding: t; -*-\n\
+             (require 'cl-lib)\n\
+             (cl-nintersection nil '(1 2 3))",
+        );
+        assert_eq!(value, Some(LispValue::NIL));
+    }
 }
