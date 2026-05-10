64ec745808bf69783f0415c4f7b00e171773a862
Eval Exec <execvy@gmail.com>
2026-05-10 16:32:59 -0400

    Add test for cl-set-exclusive-or nil list behavior

--- neovm-executor/src/object_interp.rs
+++ neovm-executor/src/object_interp.rs
@@ -13688,4 +13688,14 @@ mod tests {
         );
         assert_eq!(value, Some(LispValue::NIL));
     }
+
+    #[test]
+    fn executes_cl_set_exclusive_or_returns_nil_for_nil() {
+        let (value, _) = execute(
+            ";;; -*- lexical-binding: t; -*-\n\
+             (require 'cl-lib)\n\
+             (cl-set-exclusive-or nil nil)",
+        );
+        assert_eq!(value, Some(LispValue::NIL));
+    }
 }
