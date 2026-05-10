7f01da81e02a3fe56547701c5f1370bb790a0a5f
Eval Exec <execvy@gmail.com>
2026-05-10 18:12:52 -0400

    Add test for cl-remove-if-not nil sequence behavior

--- neovm-executor/src/object_interp.rs
+++ neovm-executor/src/object_interp.rs
@@ -13903,4 +13903,14 @@ mod tests {
         );
         assert_eq!(value, Some(LispValue::NIL));
     }
+
+    #[test]
+    fn executes_cl_remove_if_not_returns_nil_for_nil() {
+        let (value, _) = execute(
+            ";;; -*- lexical-binding: t; -*-\n\
+             (require 'cl-lib)\n\
+             (cl-remove-if-not #'evenp nil)",
+        );
+        assert_eq!(value, Some(LispValue::NIL));
+    }
 }
