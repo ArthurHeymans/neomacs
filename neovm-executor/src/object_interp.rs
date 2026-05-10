4d54fe85eff0e4d5fd14062d8772ba6ff0718b9d
Eval Exec <execvy@gmail.com>
2026-05-09 22:38:54 -0400

    Add current-buffer C primitive (returns nil)

--- neovm-executor/src/object_interp.rs
+++ neovm-executor/src/object_interp.rs
@@ -1303,6 +1303,7 @@ impl Interpreter<'_, '_, '_> {
                 s.runtime_value(result)
             }),
             "buffer-size" => Some(LispValue::expect_fixnum(0)),
+            "current-buffer" => Some(LispValue::NIL),
             "boundp" => self.subr_1(name, args, |s| {
                 let result = s.runtime.is_bound_symbol(args[0]);
                 s.runtime_bool(result)
@@ -7057,6 +7058,7 @@ fn is_primitive_name(name: &str) -> bool {
             "copy-tree",
             "cos",
             "defalias",
+            "current-buffer",
             "define-error",
             "default-boundp",
             "default-value",
