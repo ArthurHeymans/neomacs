61413fe6a730bd8c751aab9114e9f72a11dcdef0
Eval Exec <execvy@gmail.com>
2026-05-10 20:03:18 -0400

    Add test for cl-assoc-if-not nil alist behavior

--- neovm-executor/src/object_interp.rs
+++ neovm-executor/src/object_interp.rs
@@ -14039,4 +14039,14 @@ mod tests {
         );
         assert_eq!(value, Some(LispValue::TRUE));
     }
+
+    #[test]
+    fn executes_cl_assoc_if_not_returns_nil_for_nil_alist() {
+        let (value, _) = execute(
+            ";;; -*- lexical-binding: t; -*-\n\
+             (require 'cl-lib)\n\
+             (cl-assoc-if-not #'evenp nil)",
+        );
+        assert_eq!(value, Some(LispValue::NIL));
+    }
 }
