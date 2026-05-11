9ccf7438cab4ef03ee4862ce7ed1fbf7ad4fbedb
Eval Exec <execvy@gmail.com>
2026-05-10 20:13:06 -0400

    Add test for cl-subst-if-not nil tree behavior

--- neovm-executor/src/object_interp.rs
+++ neovm-executor/src/object_interp.rs
@@ -14049,4 +14049,14 @@ mod tests {
         );
         assert_eq!(value, Some(LispValue::NIL));
     }
+
+    #[test]
+    fn executes_cl_subst_if_not_returns_nil_for_nil_tree() {
+        let (value, _) = execute(
+            ";;; -*- lexical-binding: t; -*-\n\
+             (require 'cl-lib)\n\
+             (cl-subst-if-not #'evenp 'new nil)",
+        );
+        assert_eq!(value, Some(LispValue::NIL));
+    }
 }
