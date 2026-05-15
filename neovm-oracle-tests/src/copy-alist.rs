7630462b3fc6136139fad1384267891517ca1b77
Eval Exec <execvy@gmail.com>
2026-05-15 19:10:36 -0400

    Add oracle test for copy-alist tail errors
    Studied GNU src/fns.c:Fcopy_alist before adding coverage. GNU CHECK_LIST reports the offending dotted tail while non-cons alist elements remain legal shared entries. Focused oracle run passed with no Neomacs/GNU divergence found for this edge.

--- neovm-oracle-tests/src/copy-alist.rs
+++ neovm-oracle-tests/src/copy-alist.rs
@@ -2,7 +2,10 @@
 
 use super::common::return_if_neovm_enable_oracle_proptest_not_set;
 
-use super::common::{assert_ok_eq, eval_oracle_and_neovm, eval_oracle_and_neovm_with_bootstrap};
+use super::common::{
+    assert_ok_eq, assert_oracle_parity_with_bootstrap, eval_oracle_and_neovm,
+    eval_oracle_and_neovm_with_bootstrap,
+};
 
 #[test]
 fn oracle_prop_copy_alist_basics() {
@@ -22,4 +25,25 @@ fn oracle_prop_copy_alist_basics() {
         "(let* ((orig '((k . 1))) (cp (copy-alist orig))) (setcdr (car cp) 99) (cdar orig))",
     );
     assert_ok_eq("1", &o, &n);
+}
+
+#[test]
+fn oracle_copy_alist_improper_tail_error_payload() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    // GNU src/fns.c:Fcopy_alist first CHECK_LISTs the alist, so dotted alists
+    // signal with the offending tail.  Non-cons alist elements are otherwise
+    // legal and copied as shared elements.
+    let form = r#"
+(list
+ (copy-alist '(loose (a . 1) atom (b . 2)))
+ (condition-case err
+     (copy-alist '(loose (a . 1) . tail))
+   (error (list (car err) (cdr err))))
+ (condition-case err
+     (copy-alist 42)
+   (error (list (car err) (cdr err)))))
+"#;
+
+    assert_oracle_parity_with_bootstrap(form);
 }
