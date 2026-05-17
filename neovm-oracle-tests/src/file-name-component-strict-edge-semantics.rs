8ebe99ed92db996d0ad14d4c22edd9b2805384e3
Eval Exec <execvy@gmail.com>
2026-05-17 01:51:17 -0400

    Add file-name extension oracle coverage
    Study GNU lisp/files.el file-name-sans-versions, file-name-sans-extension, file-name-extension, and file-name-base behavior, then add strict oracle coverage for hidden files, trailing dots, multi-extension names, backup/version suffixes, directory preservation, and arity/type errors. No divergence surfaced in this pass; the added oracle expands coverage for these pure Elisp filename helpers.

--- neovm-oracle-tests/src/file-name-component-strict-edge-semantics.rs
+++ neovm-oracle-tests/src/file-name-component-strict-edge-semantics.rs
@@ -43,3 +43,52 @@ fn oracle_file_name_directory_and_nondirectory_edges() {
 
     assert_oracle_parity_with_bootstrap(form);
 }
+
+#[test]
+fn oracle_file_name_extension_and_version_edges() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    let form = r#"
+(list
+ (mapcar (lambda (name)
+           (list name
+                 (file-name-extension name)
+                 (file-name-extension name t)
+                 (file-name-sans-extension name)
+                 (file-name-base name)
+                 (file-name-sans-versions name)
+                 (file-name-sans-versions name t)))
+         '("plain"
+           "plain."
+           ".emacs"
+           ".emacs.el"
+           "archive.tar.gz"
+           "/tmp/archive.tar.gz"
+           "/tmp/.hidden"
+           "/tmp/.hidden.el"
+           "/tmp/dir.with.dots/file"
+           "/tmp/dir.with.dots/file."
+           "foo.~12~"
+           "foo.el.~12~"
+           "foo.el.~12~.~3~"
+           "foo.~~"
+           "foo.js.~HEAD~1~"))
+ (condition-case err
+     (file-name-extension 42)
+   (error (list (car err) (cdr err))))
+ (condition-case err
+     (file-name-extension "x" nil nil)
+   (error (list (car err) (cdr err))))
+ (condition-case err
+     (file-name-sans-extension 42)
+   (error (list (car err) (cdr err))))
+ (condition-case err
+     (file-name-sans-versions 42)
+   (error (list (car err) (cdr err))))
+ (condition-case err
+     (file-name-base)
+   (error (list (car err) (cdr err)))))
+"#;
+
+    assert_oracle_parity_with_bootstrap(form);
+}
