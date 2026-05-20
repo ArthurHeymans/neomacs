6b6b54366d66fe5371cdf35bfb49a04e7709c6eb
Eval Exec <execvy@gmail.com>
2026-05-19 23:12:30 -0400

    Correct length predicate oracle cases

--- neovm-oracle-tests/src/string-length-value-comparison-semantics.rs
+++ neovm-oracle-tests/src/string-length-value-comparison-semantics.rs
@@ -3,7 +3,7 @@
 //!
 //! GNU implements `string<`/`string=` in `src/fns.c` (string-lessp/string-equal
 //! with symbol arguments), `value<` in `src/data.c`, and
-//! `length<`/`length=`/`length>` there too.
+//! `length<`/`length=`/`length>` in `src/fns.c`.
 
 use super::common::return_if_neovm_enable_oracle_proptest_not_set;
 
@@ -87,21 +87,21 @@ fn oracle_value_lt_wrong_number() {
 #[test]
 fn oracle_length_lt_true() {
     return_if_neovm_enable_oracle_proptest_not_set!();
-    let (oracle, neovm) = eval_oracle_and_neovm(r#"(length< '(a) '(a b))"#);
+    let (oracle, neovm) = eval_oracle_and_neovm(r#"(length< '(a) 2)"#);
     assert_ok_eq("t", &oracle, &neovm);
 }
 
 #[test]
 fn oracle_length_eq_true() {
     return_if_neovm_enable_oracle_proptest_not_set!();
-    let (oracle, neovm) = eval_oracle_and_neovm(r#"(length= '(a b) "ab")"#);
+    let (oracle, neovm) = eval_oracle_and_neovm(r#"(length= '(a b) 2)"#);
     assert_ok_eq("t", &oracle, &neovm);
 }
 
 #[test]
 fn oracle_length_gt_true() {
     return_if_neovm_enable_oracle_proptest_not_set!();
-    let (oracle, neovm) = eval_oracle_and_neovm(r#"(length> '(a b c) '(a))"#);
+    let (oracle, neovm) = eval_oracle_and_neovm(r#"(length> '(a b c) 1)"#);
     assert_ok_eq("t", &oracle, &neovm);
 }
 
@@ -110,4 +110,17 @@ fn oracle_length_eq_wrong_type() {
     return_if_neovm_enable_oracle_proptest_not_set!();
     let (oracle, neovm) = eval_oracle_and_neovm(r#"(length< 42 "foo")"#);
     assert_err_kind(&oracle, &neovm, "wrong-type-argument");
+}
+
+#[test]
+fn oracle_length_predicates_reject_non_fixnum_threshold() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+    for form in [
+        r#"(length< '(a) '(a b))"#,
+        r#"(length= '(a b) "ab")"#,
+        r#"(length> '(a b c) '(a))"#,
+    ] {
+        let (oracle, neovm) = eval_oracle_and_neovm(form);
+        assert_err_kind(&oracle, &neovm, "wrong-type-argument");
+    }
 }
