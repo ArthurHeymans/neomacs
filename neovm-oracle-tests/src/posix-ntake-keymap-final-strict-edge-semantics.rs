4f28d070fbe4063ef2f35e5728c3c864cce71881
Eval Exec <execvy@gmail.com>
2026-05-20 02:51:10 -0400

    Correct current local map oracle

--- neovm-oracle-tests/src/posix-ntake-keymap-final-strict-edge-semantics.rs
+++ neovm-oracle-tests/src/posix-ntake-keymap-final-strict-edge-semantics.rs
@@ -2,7 +2,7 @@
 //! GNU src/search.c, src/fns.c, src/keyboard.c.
 
 use super::common::return_if_neovm_enable_oracle_proptest_not_set;
-use super::common::{assert_ok_eq, eval_oracle_and_neovm};
+use super::common::{assert_ok_eq, assert_oracle_parity_with_bootstrap, eval_oracle_and_neovm};
 
 #[test]
 fn oracle_posix_looking_at_match() {
@@ -51,6 +51,5 @@ fn oracle_current_global_map_is_keymap() {
 #[test]
 fn oracle_current_local_map_default_nil() {
     return_if_neovm_enable_oracle_proptest_not_set!();
-    let (o, n) = eval_oracle_and_neovm(r#"(current-local-map)"#);
-    assert_ok_eq("nil", &o, &n);
+    assert_oracle_parity_with_bootstrap(r#"(current-local-map)"#);
 }
