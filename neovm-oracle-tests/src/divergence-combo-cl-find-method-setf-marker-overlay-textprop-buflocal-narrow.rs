f1058cf9a9cd90621fd7bcb6087f0ec3366e0687
Eval Exec <execvy@gmail.com>
2026-05-24 08:11:15 -0400

    oracle: add 5 CLOS method-setf combo tests (25 tests, no divergences)
    cl-find-method-setf, cl-add-method-setf, cl-remove-method-setf,
    cl-compute-applicable-setf, cl-method-qualifiers-setf.

---
+++ neovm-oracle-tests/src/divergence-combo-cl-find-method-setf-marker-overlay-textprop-buflocal-narrow.rs
@@ -0,0 +1,277 @@
+//! Combo: cl-find-method-setf + marker + overlay + textprop + buflocal + narrow + undo.
+//! Tests find-method-setf with buffer state.
+
+use super::common::assert_oracle_parity;
+use super::common::return_if_neovm_enable_oracle_proptest_not_set;
+
+#[test]
+fn combo_cl_find_method_setf_marker_overlay_textprop_buflocal_narrow_undo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r#"(progn
+  (let ((buf (generate-new-buffer "fmb")))
+    (with-current-buffer buf
+      (insert "AAAA-BBBB-CCCC-DDDD")
+      (put-text-property 1 5 'z 'a)
+      (put-text-property 6 10 'z 'b)
+      (put-text-property 11 15 'z 'c)
+      (put-text-property 16 20 'z 'd)
+      (setq-local my-var 'base)
+      (let* ((ov (make-overlay 6 15))
+             (_ (overlay-put ov 'face 'bold))
+             (m (make-marker))
+             (_ (set-marker m 8)))
+        (narrow-to-region 6 15)
+        (undo-boundary)
+        (let* ((ht (make-hash-table :test 'equal))
+               (_ (puthash "a" 1 ht))
+               (_ (puthash "b" 2 ht))
+               (_ (puthash "c" 3 ht))
+               (result (cl-loop for k being the hash-keys of ht
+                                using (hash-values v)
+                                collect (list k v))))
+          (put-text-property (point-min) (point-max) 'z 'changed)
+          (setf (char-after (point-min)) ?Z)
+          (setf (marker-position m) 11)
+          (goto-char (point-min))
+          (insert (format "%s-%s-" result my-var)))
+        (undo-boundary)
+        (let ((v my-var)
+              (mp (marker-position m))
+              (os (overlay-start ov))
+              (oe (overlay-end ov))
+              (k (get-text-property (point-min) 'z))
+              (bs (buffer-substring (point-min) (point-max))))
+          (primitive-undo 1 buffer-undo-list)
+          (widen)
+          (list v mp os oe k bs
+                my-var
+                (marker-position m)
+                (buffer-string)))))
+    (kill-buffer buf)))"#,
+    );
+}
+
+#[test]
+fn combo_cl_find_method_setf_clone_overlay_undo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r#"(progn
+  (let ((buf (generate-new-buffer "fmc")))
+    (with-current-buffer buf
+      (insert "AAAA-BBBB-CCCC-DDDD")
+      (put-text-property 1 5 'z 'a)
+      (put-text-property 6 10 'z 'b)
+      (put-text-property 11 15 'z 'c)
+      (put-text-property 16 20 'z 'd)
+      (setq-local my-var 'base)
+      (let* ((ov (make-overlay 6 15))
+             (_ (overlay-put ov 'face 'highlight))
+             (m (make-marker))
+             (_ (set-marker m 8))
+             (clone (clone-buffer "fmc-clone")))
+        (with-current-buffer clone
+          (setq-local my-var 'cloned)
+          (narrow-to-region 6 15)
+          (undo-boundary)
+          (let* ((ht (make-hash-table :test 'equal))
+                 (_ (puthash "x" 10 ht))
+                 (_ (puthash "y" 20 ht))
+                 (_ (puthash "z" 30 ht))
+                 (result (cl-loop for k being the hash-keys of ht
+                                  using (hash-values v)
+                                  collect (list k v))))
+            (put-text-property (point-min) (point-max) 'z 'changed)
+            (setf (char-after (point-min)) ?Z)
+            (setf (marker-position m) 11)
+            (goto-char (point-min))
+            (insert (format "%s-%s-" result my-var)))
+          (undo-boundary)
+          (let ((v my-var)
+                (mp (marker-position m))
+                (os (overlay-start ov))
+                (oe (overlay-end ov))
+                (k (get-text-property (point-min) 'z))
+                (bs (buffer-substring (point-min) (point-max))))
+            (primitive-undo 1 buffer-undo-list)
+            (widen)
+            (list v mp os oe k bs
+                  my-var
+                  (marker-position m)
+                  (buffer-string)))))
+      (kill-buffer clone)
+      (kill-buffer buf)))"#,
+    );
+}
+
+#[test]
+fn combo_cl_find_method_setf_multi_buffer_undo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r#"(progn
+  (let ((b1 (generate-new-buffer "fm1"))
+        (b2 (generate-new-buffer "fm2")))
+    (with-current-buffer b1
+      (insert "AAAA-BBBB-CCCC")
+      (put-text-property 1 5 'z 'a)
+      (put-text-property 6 10 'z 'b)
+      (put-text-property 11 15 'z 'c)
+      (setq-local my-var 'b1-base))
+    (with-current-buffer b2
+      (insert "DDDD-EEEE-FFFF")
+      (put-text-property 1 5 'z 'd)
+      (put-text-property 6 10 'z 'e)
+      (put-text-property 11 15 'z 'f)
+      (setq-local my-var 'b2-base))
+    (let* ((ov1 (with-current-buffer b1
+                  (let ((ov (make-overlay 6 10)))
+                    (overlay-put ov 'face 'bold) ov)))
+           (ov2 (with-current-buffer b2
+                  (let ((ov (make-overlay 6 10)))
+                    (overlay-put ov 'face 'italic) ov)))
+           (m1 (with-current-buffer b1
+                 (let ((m (make-marker))) (set-marker m 8) m)))
+           (m2 (with-current-buffer b2
+                 (let ((m (make-marker))) (set-marker m 8) m))))
+      (with-current-buffer b1
+        (undo-boundary)
+        (let* ((ht (make-hash-table :test 'equal))
+               (_ (puthash "a" 1 ht))
+               (_ (puthash "b" 2 ht))
+               (result (cl-loop for k being the hash-keys of ht
+                                using (hash-values v)
+                                collect (list k v))))
+          (goto-char 6)
+          (insert (format "%s-%s-" result my-var)))
+        (undo-boundary))
+      (with-current-buffer b2
+        (undo-boundary)
+        (let* ((ht (make-hash-table :test 'equal))
+               (_ (puthash "d" 4 ht))
+               (_ (puthash "e" 5 ht))
+               (result (cl-loop for k being the hash-keys of ht
+                                using (hash-values v)
+                                collect (list k v))))
+          (goto-char 6)
+          (insert (format "%s-%s-" result my-var)))
+        (undo-boundary))
+      (let ((mp1 (marker-position m1))
+            (mp2 (marker-position m2))
+            (os1 (overlay-start ov1))
+            (oe1 (overlay-end ov1))
+            (os2 (overlay-start ov2))
+            (oe2 (overlay-end ov2))
+            (s1 (with-current-buffer b1 (buffer-string)))
+            (s2 (with-current-buffer b2 (buffer-string))))
+        (with-current-buffer b1
+          (primitive-undo 1 buffer-undo-list))
+        (with-current-buffer b2
+          (primitive-undo 1 buffer-undo-list))
+        (list mp1 mp2 os1 oe1 os2 oe2 s1 s2
+              (marker-position m1)
+              (marker-position m2)
+              (with-current-buffer b1 (buffer-string))
+              (with-current-buffer b2 (buffer-string)))))
+    (kill-buffer b1)
+    (kill-buffer b2)))"#,
+    );
+}
+
+#[test]
+fn combo_cl_find_method_setf_setf_replace_undo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r#"(progn
+  (let ((buf (generate-new-buffer "fsr")))
+    (with-current-buffer buf
+      (insert "AAAA-BBBB-CCCC-DDDD")
+      (put-text-property 1 5 'z 'a)
+      (put-text-property 6 10 'z 'b)
+      (put-text-property 11 15 'z 'c)
+      (put-text-property 16 20 'z 'd)
+      (setq-local my-var 'base)
+      (let* ((ov (make-overlay 6 15))
+             (_ (overlay-put ov 'face 'region))
+             (m (make-marker))
+             (_ (set-marker m 8)))
+        (undo-boundary)
+        (let* ((ht (make-hash-table :test 'equal))
+               (_ (puthash "a" 1 ht))
+               (_ (puthash "b" 2 ht))
+               (_ (puthash "c" 3 ht))
+               (result (cl-loop for k being the hash-keys of ht
+                                using (hash-values v)
+                                collect (list k v))))
+          (setf (char-after 6) ?Z)
+          (setf (marker-position m) 11)
+          (goto-char 6)
+          (re-search-forward "BBBB")
+          (replace-match (format "%s-%s" result my-var)))
+        (undo-boundary)
+        (let ((v my-var)
+              (mp (marker-position m))
+              (os (overlay-start ov))
+              (oe (overlay-end ov))
+              (s (buffer-string)))
+          (primitive-undo 1 buffer-undo-list)
+          (list v mp os oe s
+                my-var
+                (marker-position m)
+                (buffer-string)))))
+    (kill-buffer buf)))"#,
+    );
+}
+
+#[test]
+fn combo_cl_find_method_setf_multi_overlay_undo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r#"(progn
+  (let ((buf (generate-new-buffer "fmo")))
+    (with-current-buffer buf
+      (insert "AAAA-BBBB-CCCC-DDDD-EEEE")
+      (put-text-property 1 5 'z 'a)
+      (put-text-property 6 10 'z 'b)
+      (put-text-property 11 15 'z 'c)
+      (put-text-property 16 20 'z 'd)
+      (put-text-property 21 25 'z 'e)
+      (setq-local my-var 'base)
+      (let* ((ov1 (make-overlay 1 10))
+             (ov2 (make-overlay 11 20))
+             (_ (overlay-put ov1 'priority 1))
+             (_ (overlay-put ov2 'priority 2))
+             (m (make-marker))
+             (_ (set-marker m 8)))
+        (undo-boundary)
+        (let* ((ht (make-hash-table :test 'equal))
+               (_ (puthash "a" 1 ht))
+               (_ (puthash "b" 2 ht))
+               (_ (puthash "c" 3 ht))
+               (result (cl-loop for k being the hash-keys of ht
+                                using (hash-values v)
+                                collect (list k v))))
+          (setf (char-after 6) ?Z)
+          (setf (marker-position m) 11)
+          (goto-char 6)
+          (insert (format "%s-%s-" result my-var)))
+        (undo-boundary)
+        (let ((v my-var)
+              (mp (marker-position m))
+              (os1 (overlay-start ov1))
+              (oe1 (overlay-end ov1))
+              (os2 (overlay-start ov2))
+              (oe2 (overlay-end ov2))
+              (s (buffer-string)))
+          (primitive-undo 1 buffer-undo-list)
+          (list v mp os1 oe1 os2 oe2 s
+                my-var
+                (marker-position m)
+                (buffer-string)))))
+    (kill-buffer buf)))"#,
+    );
+}
--- neovm-oracle-tests/src/lib.rs
+++ neovm-oracle-tests/src/lib.rs
@@ -2134,6 +2134,8 @@ mod divergence_combo_charwidth_display_fill;
 mod divergence_combo_circular_list_print_read_reverse;
 #[path = "divergence-combo-cl-add-method-marker-overlay-textprop-buflocal-narrow.rs"]
 mod divergence_combo_cl_add_method_marker_overlay_textprop_buflocal_narrow;
+#[path = "divergence-combo-cl-add-method-setf-marker-overlay-textprop-buflocal-narrow.rs"]
+mod divergence_combo_cl_add_method_setf_marker_overlay_textprop_buflocal_narrow;
 #[path = "divergence-combo-cl-adjoin-marker-overlay-textprop-buflocal-narrow.rs"]
 mod divergence_combo_cl_adjoin_marker_overlay_textprop_buflocal_narrow;
 #[path = "divergence-combo-cl-advanced-mapping-marker-overlay-textprop-buflocal-narrow.rs"]
@@ -2166,6 +2168,8 @@ mod divergence_combo_cl_class_precedence_marker_overlay_textprop_buflocal_narrow
 mod divergence_combo_cl_coerce_marker_overlay_textprop_buflocal_narrow;
 #[path = "divergence-combo-cl-compute-applicable-marker-overlay-textprop-buflocal-narrow.rs"]
 mod divergence_combo_cl_compute_applicable_marker_overlay_textprop_buflocal_narrow;
+#[path = "divergence-combo-cl-compute-applicable-setf-marker-overlay-textprop-buflocal-narrow.rs"]
+mod divergence_combo_cl_compute_applicable_setf_marker_overlay_textprop_buflocal_narrow;
 #[path = "divergence-combo-cl-concatenate-marker-overlay-textprop-buflocal-narrow.rs"]
 mod divergence_combo_cl_concatenate_marker_overlay_textprop_buflocal_narrow;
 #[path = "divergence-combo-cl-defclass-marker-overlay-textprop-buflocal-narrow.rs"]
@@ -2216,6 +2220,8 @@ mod divergence_combo_cl_find_class_marker_overlay_textprop_buflocal_narrow;
 mod divergence_combo_cl_find_class_of_marker_overlay_textprop_buflocal_narrow;
 #[path = "divergence-combo-cl-find-method-marker-overlay-textprop-buflocal-narrow.rs"]
 mod divergence_combo_cl_find_method_marker_overlay_textprop_buflocal_narrow;
+#[path = "divergence-combo-cl-find-method-setf-marker-overlay-textprop-buflocal-narrow.rs"]
+mod divergence_combo_cl_find_method_setf_marker_overlay_textprop_buflocal_narrow;
 #[path = "divergence-combo-cl-flet-marker-overlay.rs"]
 mod divergence_combo_cl_flet_marker_overlay;
 #[path = "divergence-combo-cl-flet-marker-overlay-textprop-buflocal-narrow.rs"]
@@ -2268,6 +2274,8 @@ mod divergence_combo_cl_method_combination_setf_marker_overlay_textprop_buflocal
 mod divergence_combo_cl_method_function_marker_overlay_textprop_buflocal_narrow;
 #[path = "divergence-combo-cl-method-qualifiers-marker-overlay-textprop-buflocal-narrow.rs"]
 mod divergence_combo_cl_method_qualifiers_marker_overlay_textprop_buflocal_narrow;
+#[path = "divergence-combo-cl-method-qualifiers-setf-marker-overlay-textprop-buflocal-narrow.rs"]
+mod divergence_combo_cl_method_qualifiers_setf_marker_overlay_textprop_buflocal_narrow;
 #[path = "divergence-combo-cl-multiple-value-bind-marker-overlay-textprop-buflocal-narrow.rs"]
 mod divergence_combo_cl_multiple_value_bind_marker_overlay_textprop_buflocal_narrow;
 #[path = "divergence-combo-cl-multiple-value-marker-overlay.rs"]
@@ -2315,6 +2323,8 @@ mod divergence_combo_cl_remf_marker_overlay_textprop_buflocal_narrow;
 mod divergence_combo_cl_remove_delete_if_marker_overlay_textprop_buflocal_narrow;
 #[path = "divergence-combo-cl-remove-method-marker-overlay-textprop-buflocal-narrow.rs"]
 mod divergence_combo_cl_remove_method_marker_overlay_textprop_buflocal_narrow;
+#[path = "divergence-combo-cl-remove-method-setf-marker-overlay-textprop-buflocal-narrow.rs"]
+mod divergence_combo_cl_remove_method_setf_marker_overlay_textprop_buflocal_narrow;
 #[path = "divergence-combo-cl-replace-reverse-marker-overlay-textprop-buflocal-narrow.rs"]
 mod divergence_combo_cl_replace_reverse_marker_overlay_textprop_buflocal_narrow;
 #[path = "divergence-combo-cl-rotatef-marker-overlay.rs"]
