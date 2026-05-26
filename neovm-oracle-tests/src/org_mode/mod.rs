e140576a5e8bfd0577b02cb5a690d5ca5bcd6528
Eval Exec <execvy@gmail.com>
2026-05-27 00:05:15 +0800

    Add complex Org mode oracle scenarios

--- neovm-oracle-tests/src/org_mode/mod.rs
+++ neovm-oracle-tests/src/org_mode/mod.rs
@@ -139,3 +139,173 @@ fn org_babel_emacs_lisp_result_insertion() {
     (buffer-substring-no-properties (point-min) (point-max))))"##,
     );
 }
+
+#[test]
+fn org_subtree_cut_paste_preserves_hierarchy() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r#"(progn
+  (require 'org)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* Alpha\n")
+    (insert "** A1\nbody A1\n")
+    (insert "** A2\n")
+    (insert "* Beta\n")
+    (insert "** B1\n")
+    (insert "* Gamma\n")
+    (goto-char (point-min))
+    (search-forward "* Beta")
+    (beginning-of-line)
+    (org-cut-subtree)
+    (goto-char (point-max))
+    (org-paste-subtree 1)
+    (let ((headlines nil))
+      (org-element-map (org-element-parse-buffer) 'headline
+        (lambda (headline)
+          (push (list (org-element-property :level headline)
+                      (org-element-property :raw-value headline))
+                headlines)))
+      (list (nreverse headlines)
+            (buffer-substring-no-properties (point-min) (point-max))))))"#,
+    );
+}
+
+#[test]
+fn org_properties_tags_and_todo_mutation_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r#"(progn
+  (require 'org)
+  (with-temp-buffer
+    (let ((org-log-done nil)
+          (org-use-property-inheritance t))
+      (org-mode)
+      (insert "* Project :root:\n")
+      (insert ":PROPERTIES:\n:Owner: Ada\n:END:\n")
+      (insert "** TODO Design :work:\n")
+      (insert "SCHEDULED: <2026-05-26 Tue>\n")
+      (insert "** WAIT Review\n")
+      (goto-char (point-min))
+      (search-forward "Design")
+      (beginning-of-line)
+      (org-todo "DONE")
+      (org-set-property "Effort" "2:00")
+      (org-set-tags '("work" "urgent"))
+      (list (org-entry-get nil "Owner" t)
+            (org-entry-get nil "Effort")
+            (org-get-tags nil t)
+            (substring-no-properties (org-get-todo-state))
+            (buffer-substring-no-properties (point-min) (point-max))))))"#,
+    );
+}
+
+#[test]
+fn org_nested_checkbox_counts_after_toggles() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r#"(progn
+  (require 'org)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* Tasks [0/2]\n")
+    (insert "- [ ] first\n")
+    (insert "  - [ ] child\n")
+    (insert "- [ ] second\n")
+    (goto-char (point-min))
+    (search-forward "first")
+    (beginning-of-line)
+    (org-toggle-checkbox)
+    (search-forward "child")
+    (beginning-of-line)
+    (org-toggle-checkbox)
+    (goto-char (point-min))
+    (org-update-checkbox-count)
+    (list (buffer-substring-no-properties (point-min) (point-max))
+          (org-element-map (org-element-parse-buffer) 'item
+            (lambda (item)
+              (org-element-property :checkbox item))))))"#,
+    );
+}
+
+#[test]
+fn org_table_multi_formula_recalculation_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (with-temp-buffer
+    (org-mode)
+    (insert "| item | value | tax |\n")
+    (insert "|------+-------+-----|\n")
+    (insert "| a | 2 | 1 |\n")
+    (insert "| b | 3 | 2 |\n")
+    (insert "|------+-------+-----|\n")
+    (insert "| total |  |  |\n")
+    (insert "#+TBLFM: @>$2=vsum(@2..@-1)::@>$3=vsum(@2..@-1)\n")
+    (goto-char (point-min))
+    (org-table-recalculate-buffer-tables)
+    (buffer-substring-no-properties (point-min) (point-max))))"##,
+    );
+}
+
+#[test]
+fn org_document_element_mix_with_properties_blocks_and_footnotes() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (with-temp-buffer
+    (org-mode)
+    (insert "#+TITLE: Demo\n")
+    (insert "#+AUTHOR: Ada\n")
+    (insert "* TODO Build :tag:\n")
+    (insert "DEADLINE: <2026-05-27 Wed>\n")
+    (insert ":PROPERTIES:\n:ID: build-1\n:END:\n")
+    (insert "Paragraph with [fn:1].\n")
+    (insert "#+begin_src emacs-lisp\n(+ 1 2)\n#+end_src\n")
+    (insert "| a | b |\n| 1 | 2 |\n")
+    (insert "[fn:1] Footnote text\n")
+    (let ((tree (org-element-parse-buffer))
+          (out nil))
+      (dolist (type '(keyword headline planning property-drawer node-property
+                      paragraph src-block table footnote-definition))
+        (org-element-map tree type
+          (lambda (element)
+            (push (list type
+                        (org-element-property :key element)
+                        (org-element-property :raw-value element)
+                        (org-element-property :language element)
+                        (org-element-property :value element))
+                  out))))
+      (nreverse out))))"##,
+    );
+}
+
+#[test]
+fn org_html_export_markup_and_link_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'ox-html)
+  (with-temp-buffer
+    (org-mode)
+    (insert "#+TITLE: Demo\n")
+    (insert "* Head\n")
+    (insert "Paragraph with *bold* and [[https://example.org][link]].\n")
+    (let* ((org-export-with-toc nil)
+           (org-export-show-temporary-export-buffer nil)
+           (html (org-export-as 'html nil nil t nil)))
+      (list (not (null (string-match-p "<h2" html)))
+            (not (null (string-match-p "Head</h2>" html)))
+            (not (null (string-match-p "<b>bold</b>" html)))
+            (not (null (string-match-p "<a href=\"https://example.org\">link</a>" html)))
+            (length html)))))"##,
+    );
+}
