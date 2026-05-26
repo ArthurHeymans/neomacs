5c0be43bf32b55a0045e5d126768adf714636c56
Eval Exec <execvy@gmail.com>
2026-05-27 01:18:48 +0800

    oracle-tests: add Org fold edit recovery workflows

--- neovm-oracle-tests/src/org_mode/fold_font.rs
+++ neovm-oracle-tests/src/org_mode/fold_font.rs
@@ -109,3 +109,133 @@ fn org_font_lock_heading_faces_level_four_plus_combo() {
       (nreverse out))))"#,
     );
 }
+
+#[test]
+fn org_local_cycle_then_edit_preserves_newline_structure_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r#"(progn
+  (require 'org)
+  (require 'org-cycle)
+  (require 'org-fold)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* A\nbody A\n** B\nbody B\n*** C\nbody C\n**** D\nbody D\n")
+    (goto-char (point-min))
+    (search-forward "B")
+    (beginning-of-line)
+    (let (states)
+      (dotimes (_ 5)
+        (org-cycle)
+        (push
+         (mapcar
+          (lambda (needle)
+            (invisible-p
+             (save-excursion
+               (goto-char (point-min))
+               (search-forward needle)
+               (point))))
+          '("body A" "body B" "C" "body C" "D" "body D"))
+         states))
+      (org-fold-show-all)
+      (goto-char (point-min))
+      (search-forward "body B")
+      (end-of-line)
+      (insert "\ninserted under B")
+      (list (nreverse states)
+            (mapcar
+             (lambda (needle)
+               (invisible-p
+                (save-excursion
+                  (goto-char (point-min))
+                  (search-forward needle)
+                  (point))))
+             '("inserted" "D" "body D"))
+            (buffer-substring-no-properties (point-min) (point-max))))))"#,
+    );
+}
+
+#[test]
+fn org_cycle_cut_paste_subtree_reexpand_integrity_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r#"(progn
+  (require 'org)
+  (require 'org-cycle)
+  (require 'org-fold)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* A\nbody A\n** B\nbody B\n*** C\nbody C\n* E\nbody E\n")
+    (goto-char (point-min))
+    (search-forward "B")
+    (beginning-of-line)
+    (org-cycle)
+    (org-cycle)
+    (org-fold-show-all)
+    (goto-char (point-min))
+    (search-forward "C")
+    (beginning-of-line)
+    (org-cut-subtree)
+    (goto-char (point-max))
+    (org-paste-subtree 2)
+    (org-cycle-overview)
+    (org-fold-show-all)
+    (list
+     (mapcar
+      (lambda (needle)
+        (invisible-p
+         (save-excursion
+           (goto-char (point-min))
+           (search-forward needle)
+           (point))))
+      '("body A" "B" "body B" "C" "body C" "E" "body E"))
+     (buffer-substring-no-properties (point-min) (point-max)))))"#,
+    );
+}
+
+#[test]
+fn org_cycle_hide_drawers_show_all_recovery_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r#"(progn
+  (require 'org)
+  (require 'org-cycle)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* A\n")
+    (insert ":PROPERTIES:\n:X: y\n:END:\n")
+    (insert "body\n")
+    (insert "** B\n")
+    (insert ":LOGBOOK:\n")
+    (insert "CLOCK: [2026-05-27 Wed 09:00]--[2026-05-27 Wed 10:00] =>  1:00\n")
+    (insert ":END:\n")
+    (insert "body B\n")
+    (goto-char (point-min))
+    (org-cycle-hide-drawers 'children)
+    (let ((hidden
+           (mapcar
+            (lambda (needle)
+              (invisible-p
+               (save-excursion
+                 (goto-char (point-min))
+                 (search-forward needle)
+                 (point))))
+            '(":X:" "CLOCK" "body" "B"))))
+      (org-fold-show-all)
+      (let ((shown
+             (mapcar
+              (lambda (needle)
+                (invisible-p
+                 (save-excursion
+                   (goto-char (point-min))
+                   (search-forward needle)
+                   (point))))
+              '(":X:" "CLOCK" "body" "B"))))
+        (list hidden
+              shown
+              (buffer-substring-no-properties (point-min) (point-max)))))))"#,
+    );
+}
