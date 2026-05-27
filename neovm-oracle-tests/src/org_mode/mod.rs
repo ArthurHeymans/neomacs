fbc649d08554ed64d55e00b4edf7e12b45c73de7
Eval Exec <execvy@gmail.com>
2026-05-27 18:31:25 -0400

    oracle: 47 uncovered-features-8 tests, total 6334
    Tests for previously uncovered org-mode features:
    - org-shifttab cycling
    - org-meta-return at heading/item
    - org-shiftmetaright/left
    - org-shiftmetaup/down
    - org-metaright/left on list
    - org-metaup/down on list
    - org-return-at-point
    - org-element-context
    - org-get-category
    - org-entry-get ITEM/TAGS/PRIORITY
    - org-entry-get SCHEDULED/DEADLINE/CLOSED
    - org-entry-get custom properties
    - org-entry-properties standard
    - org-entry-put/get/delete
    - org-get-repeat
    - org-clock-sum-current-entry
    - org-columns-get-format
    - org-refile-get-targets
    - org-map-entries with match
    - org-id-get-create
    - org-store-link
    - org-list-struct
    - org-element-lineage
    - org-element-contents
    - org-element-type-p
    - org-element-map no-recursion
    - org-element-map first-match
    - org-element-map with info
    - org-element-parent-chain deep
    - org-sort-entries
    - org-clone-subtree
    - org-copy/paste-subtree
    - org-mark-subtree
    - org-narrow-to-subtree
    - org-end-of-subtree
    - org-cycle-hide-drawers
    - org-toggle-heading
    - org-move-item
    - org-insert-todo-heading
    - org-move-subtree
    - org-dblock-update
    - org-macro-replace-all
    - org-try-structure-completion
    - org-update-statistics-cookies
    - org-match-sparse-tree

--- neovm-oracle-tests/src/org_mode/mod.rs
+++ neovm-oracle-tests/src/org_mode/mod.rs
@@ -1530,6 +1530,7 @@ mod strong_uncovered_features_4;
 mod strong_uncovered_features_5;
 mod strong_uncovered_features_6;
 mod strong_uncovered_features_7;
+mod strong_uncovered_features_8;
 mod strong_workflow_combos;
 mod strong_xenna_combos;
 mod strong_xero_combos;
