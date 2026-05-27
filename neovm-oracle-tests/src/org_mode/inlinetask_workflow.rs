8ef13d6cb1f0cd5cd10eaa4970c52b85e3f66332
Eval Exec <execvy@gmail.com>
2026-05-27 14:19:58 +0800

    oracle-tests: add Org inlinetask font workflow
    Cover inline task fontification, folding, TODO and tag edits,
    
    element parsing, and HTML export.
    
    No divergence surfaced in this batch.

--- neovm-oracle-tests/src/org_mode/inlinetask_workflow.rs
+++ neovm-oracle-tests/src/org_mode/inlinetask_workflow.rs
@@ -123,3 +123,80 @@ fn org_inlinetask_visibility_and_remove_end_combo() {
                  (point-min) (point-max)))))))"##,
     );
 }
+
+#[test]
+fn org_inlinetask_fontify_edit_export_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'org-inlinetask)
+  (require 'ox-html)
+  (with-temp-buffer
+    (let ((org-inlinetask-min-level 4)
+          (org-inlinetask-show-first-star t)
+          (org-todo-keywords '((sequence "TODO" "WAIT" "|" "DONE")))
+          (org-export-with-toc nil))
+      (org-mode)
+      (insert "#+TITLE: Inline Font\n")
+      (insert "* Parent\n")
+      (insert "Before.\n")
+      (insert "**** TODO Inline Alpha :old:\n")
+      (insert "SCHEDULED: <2026-05-27 Wed>\n")
+      (insert "Body with *bold* and [[https://example.org][link]].\n")
+      (insert "**** END\n")
+      (insert "**** WAIT Inline Beta\n")
+      (insert ":PROPERTIES:\n:Effort: 0:10\n:END:\n")
+      (insert "Beta body.\n")
+      (insert "**** END\n")
+      (font-lock-ensure (point-min) (point-max))
+      (let ((before
+             (mapcar
+              (lambda (needle)
+                (save-excursion
+                  (goto-char (point-min))
+                  (search-forward needle)
+                  (list needle
+                        (org-inlinetask-in-task-p)
+                        (get-text-property (match-beginning 0) 'face)
+                        (get-text-property (match-beginning 0)
+                                           'font-lock-fontified))))
+              '("Inline Alpha" "Inline Beta" "bold" "link"))))
+        (goto-char (point-min))
+        (search-forward "Inline Alpha")
+        (beginning-of-line)
+        (org-inlinetask-toggle-visibility 'fold)
+        (let ((folded (list (org-fold-folded-p (line-end-position)
+                                               'headline)
+                            (invisible-p
+                             (save-excursion
+                               (search-forward "Body with")
+                               (point))))))
+          (org-inlinetask-toggle-visibility 'unfold)
+          (org-todo "DONE")
+          (org-toggle-tag "old" 'off)
+          (org-toggle-tag "new" 'on)
+          (let* ((tree (org-element-parse-buffer))
+                 (tasks
+                  (org-element-map tree 'inlinetask
+                    (lambda (task)
+                      (list (org-element-property :todo-keyword task)
+                            (org-element-property :raw-value task)
+                            (org-element-property :tags task)
+                            (org-element-property :scheduled task)))))
+                 (html (replace-regexp-in-string
+                        "org[[:alnum:]]+"
+                        "org-id"
+                        (org-export-as 'html nil nil t nil))))
+            (list before
+                  folded
+                  tasks
+                  (not (null (string-match-p "Inline Alpha" html)))
+                  (not (null (string-match-p "DONE" html)))
+                  (not (null (string-match-p "new" html)))
+                  html
+                  (buffer-substring-no-properties
+                   (point-min) (point-max))))))))"##,
+    );
+}
