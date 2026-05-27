98a556fad676a7031853c71ccbd28137ebe10177
Eval Exec <execvy@gmail.com>
2026-05-27 12:53:11 +0800

    oracle-tests: add Org agenda interaction workflows
    Covers filters, priority and effort source edits,
    
    bulk marks, toggles, and regexp marking.
    
    No divergence surfaced in this batch.

--- neovm-oracle-tests/src/org_mode/agenda_workflow.rs
+++ neovm-oracle-tests/src/org_mode/agenda_workflow.rs
@@ -146,3 +146,173 @@ DEADLINE: <2026-05-28 Thu>
       (delete-file file))))"##,
     );
 }
+
+#[test]
+fn org_agenda_filter_apply_remove_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'org-agenda)
+  (let* ((file (make-temp-file
+                "org-agenda-filter" nil ".org"
+                "#+CATEGORY: Filter
+* TODO Alpha :work:billable:
+:PROPERTIES:
+:Effort: 0:30
+:END:
+* TODO Beta :work:internal:
+:PROPERTIES:
+:Effort: 2:00
+:END:
+* TODO Home :home:
+:PROPERTIES:
+:Effort: 0:15
+:END:
+"))
+         (org-agenda-files (list file))
+         (org-agenda-prefix-format "%-8:c%5e %s")
+         (org-agenda-show-all-dates nil))
+    (unwind-protect
+        (progn
+          (org-tags-view t "+TODO")
+          (with-current-buffer org-agenda-buffer-name
+            (let ((all (buffer-substring-no-properties
+                        (point-min) (point-max))))
+              (org-agenda-filter-apply '("+work") 'tag)
+              (let ((work (buffer-substring-no-properties
+                           (point-min) (point-max)))
+                    (filter-tag org-agenda-tag-filter))
+                (org-agenda-filter-apply '("<1:00") 'effort)
+                (let ((effort (buffer-substring-no-properties
+                               (point-min) (point-max)))
+                      (filter-effort org-agenda-effort-filter))
+                  (org-agenda-filter-remove-all)
+                  (list (mapcar (lambda (needle)
+                                  (not (null (string-match-p needle all))))
+                                '("Alpha" "Beta" "Home"))
+                        (mapcar (lambda (needle)
+                                  (not (null (string-match-p needle work))))
+                                '("Alpha" "Beta" "Home"))
+                        (mapcar (lambda (needle)
+                                  (not (null (string-match-p needle effort))))
+                                '("Alpha" "Beta" "Home"))
+                        filter-tag
+                        filter-effort
+                        org-agenda-tag-filter
+                        org-agenda-effort-filter)))))
+      (when (get-buffer org-agenda-buffer-name)
+        (kill-buffer org-agenda-buffer-name))
+      (delete-file file))))"##,
+    );
+}
+
+#[test]
+fn org_agenda_priority_effort_source_mutation_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'org-agenda)
+  (let* ((file (make-temp-file
+                "org-agenda-edit" nil ".org"
+                "#+CATEGORY: Edit
+* TODO Alpha
+:PROPERTIES:
+:Effort: 0:30
+:END:
+* TODO Beta
+:PROPERTIES:
+:Effort: 1:00
+:END:
+"))
+         (org-agenda-files (list file))
+         (org-agenda-prefix-format "%-8:c%5e %s")
+         (org-priority-enable-commands t))
+    (unwind-protect
+        (progn
+          (org-todo-list)
+          (with-current-buffer org-agenda-buffer-name
+            (goto-char (point-min))
+            (search-forward "Alpha")
+            (beginning-of-line)
+            (org-agenda-priority ?A)
+            (cl-letf (((symbol-function 'completing-read)
+                       (lambda (&rest _) "2:30")))
+              (org-agenda-set-effort))
+            (let ((agenda-after-alpha
+                   (buffer-substring-no-properties
+                    (point-min) (point-max))))
+              (search-forward "Beta")
+              (beginning-of-line)
+              (org-agenda-priority 'down)
+              (list agenda-after-alpha
+                    (buffer-substring-no-properties
+                     (point-min) (point-max))
+                    (with-current-buffer (find-file-noselect file)
+                      (buffer-substring-no-properties
+                       (point-min) (point-max)))))))
+      (when (get-buffer org-agenda-buffer-name)
+        (kill-buffer org-agenda-buffer-name))
+      (when (get-file-buffer file) (kill-buffer (get-file-buffer file)))
+      (delete-file file))))"##,
+    );
+}
+
+#[test]
+fn org_agenda_bulk_mark_toggle_regexp_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'org-agenda)
+  (let* ((file (make-temp-file
+                "org-agenda-bulk" nil ".org"
+                "#+CATEGORY: Bulk
+* TODO Alpha :work:
+* TODO Beta :home:
+* WAIT Gamma :work:
+"))
+         (org-agenda-files (list file))
+         (org-agenda-prefix-format "%-8:c% s"))
+    (unwind-protect
+        (progn
+          (org-todo-list)
+          (with-current-buffer org-agenda-buffer-name
+            (goto-char (point-min))
+            (search-forward "Alpha")
+            (beginning-of-line)
+            (org-agenda-bulk-mark 2)
+            (let ((after-two
+                   (list (length org-agenda-bulk-marked-entries)
+                         (org-agenda-bulk-marked-p)
+                         (mapcar (lambda (m)
+                                   (with-current-buffer (marker-buffer m)
+                                     (save-excursion
+                                       (goto-char m)
+                                       (org-get-heading t t t t))))
+                                 org-agenda-bulk-marked-entries))))
+              (org-agenda-bulk-unmark-all)
+              (org-agenda-bulk-mark-regexp "Gamma")
+              (let ((after-regexp
+                     (list (length org-agenda-bulk-marked-entries)
+                           (mapcar (lambda (m)
+                                     (with-current-buffer (marker-buffer m)
+                                       (save-excursion
+                                         (goto-char m)
+                                         (org-get-heading t t t t))))
+                                   org-agenda-bulk-marked-entries))))
+                (org-agenda-bulk-toggle-all)
+                (list after-two
+                      after-regexp
+                      (length org-agenda-bulk-marked-entries)
+                      (buffer-substring-no-properties
+                       (point-min) (point-max)))))))
+      (when (get-buffer org-agenda-buffer-name)
+        (kill-buffer org-agenda-buffer-name))
+      (when (file-exists-p file) (delete-file file)))))"##,
+    );
+}
