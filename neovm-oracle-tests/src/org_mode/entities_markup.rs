3e86523537dff2609668e0a83def14bfaadf9766
Eval Exec <execvy@gmail.com>
2026-05-27 13:49:21 +0800

    oracle-tests: add Org markup entities workflow
    Cover mixed emphasis, entities, links, and font-lock props.
    
    Cover HTML, ASCII, UTF-8, and LaTeX export output.
    
    Cover custom entity handling inside marked-up text.
    
    No divergence surfaced in this batch.

--- neovm-oracle-tests/src/org_mode/entities_markup.rs
+++ neovm-oracle-tests/src/org_mode/entities_markup.rs
@@ -136,3 +136,98 @@ fn org_pretty_entities_fontify_display_combo() {
                (point-min) (point-max))))))"##,
     );
 }
+
+#[test]
+fn org_markup_entities_fontlock_export_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'org-entities)
+  (require 'ox-html)
+  (require 'ox-ascii)
+  (require 'ox-latex)
+  (with-temp-buffer
+    (let ((org-hide-emphasis-markers t)
+          (org-pretty-entities t)
+          (org-export-with-broken-links t)
+          (org-entities-user
+           '(("brandx" "\\mathsf{X}" t "&Xopf;" "X" "X" "𝕏"))))
+      (org-mode)
+      (insert "#+TITLE: Mixed Markup\n")
+      (insert "* H\n")
+      (insert "/italic \\alpha/ and *bold \\beta* and _under \\gamma_.\n")
+      (insert "=code \\delta= plus ~verb \\epsilon~ and +strike \\ndash+.\n")
+      (insert "Nested link [[https://example.org/a_b?x=1][site \\rightarrow *not-bold*]] and custom \\brandx{}.\n")
+      (font-lock-ensure (point-min) (point-max))
+      (let* ((tree (org-element-parse-buffer))
+             (objects
+              (org-element-map tree
+                  '(italic bold underline code verbatim strike-through entity link)
+                (lambda (object)
+                  (let ((type (org-element-type object)))
+                    (list type
+                          (cond
+                           ((eq type 'entity)
+                            (org-element-property :name object))
+                           ((eq type 'link)
+                            (list (org-element-property :type object)
+                                  (org-element-property :path object)
+                                  (buffer-substring-no-properties
+                                   (org-element-property :contents-begin object)
+                                   (org-element-property :contents-end object))))
+                           (t
+                            (buffer-substring-no-properties
+                             (org-element-property :begin object)
+                             (org-element-property :end object)))))))))
+             (props
+              (mapcar
+               (lambda (needle)
+                 (save-excursion
+                   (goto-char (point-min))
+                   (search-forward needle)
+                   (list needle
+                         (get-text-property (match-beginning 0) 'invisible)
+                         (get-text-property (match-beginning 0) 'face)
+                         (get-text-property (match-beginning 0) 'display)
+                         (get-text-property (match-beginning 0) 'composition)
+                         (get-text-property (match-beginning 0) 'org-emphasis))))
+               '("/" "italic" "\\alpha" "*" "bold" "\\beta" "_"
+                 "under" "=" "code" "~" "verb" "+" "strike"
+                 "\\ndash" "\\brandx")))
+             (html (org-export-as 'html nil nil t '(:with-toc nil)))
+             (ascii (let ((org-ascii-charset 'ascii))
+                      (org-export-as 'ascii nil nil t '(:with-toc nil))))
+             (utf8 (let ((org-ascii-charset 'utf-8))
+                     (org-export-as 'ascii nil nil t '(:with-toc nil))))
+             (latex (replace-regexp-in-string
+                     "sec:org[[:alnum:]]+"
+                     "sec:org-id"
+                     (org-export-as 'latex nil nil t '(:with-toc nil)))))
+        (list objects
+              props
+              (mapcar (lambda (needle)
+                        (not (null (string-match-p needle html))))
+                      '("<i>italic" "<b>bold" "<span class=\"underline\">under"
+                        "<code>code" "<code>verb" "<del>strike"
+                        "&alpha;" "&beta;" "&gamma;" "&ndash;" "&Xopf;"))
+              (mapcar (lambda (needle)
+                        (not (null (string-match-p needle ascii))))
+                      '("italic alpha" "bold beta" "_under gamma_"
+                        "=code delta=" "~verb epsilon~" "+strike -+"
+                        "site ->" "X"))
+              (mapcar (lambda (needle)
+                        (not (null (string-match-p needle utf8))))
+                      '("italic α" "bold β" "_under γ_"
+                        "=code δ=" "~verb ε~" "+strike –+"
+                        "site →" "𝕏"))
+              (mapcar (lambda (needle)
+                        (not (null (string-match-p needle latex))))
+                      '("\\\\emph{italic" "\\\\textbf{bold"
+                        "\\\\uline{under" "\\\\texttt{code"
+                        "\\\\texttt{verb" "\\\\sout{strike"
+                        "\\\\alpha" "\\\\beta" "\\\\gamma"
+                        "--" "\\\\mathsf{X}"))))))"##,
+    );
+}
