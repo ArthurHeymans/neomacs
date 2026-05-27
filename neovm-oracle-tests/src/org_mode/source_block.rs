35218fec9ec54c6bff98772ee7018c5126eace33
Eval Exec <execvy@gmail.com>
2026-05-27 11:18:45 +0800

    oracle-tests: add Org source edit workflows
    Covers src preserve indentation, edit-special, and result removal.
    
    No divergence surfaced in this batch.

--- neovm-oracle-tests/src/org_mode/source_block.rs
+++ neovm-oracle-tests/src/org_mode/source_block.rs
@@ -109,3 +109,129 @@ fn org_babel_inline_and_block_result_replace_combo() {
              (point-min) (point-max))))))"##,
     );
 }
+
+#[test]
+fn org_src_preserve_indentation_save_abort_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'org-src)
+  (with-temp-buffer
+    (let ((org-src-preserve-indentation t)
+          (org-edit-src-content-indentation 4)
+          (org-src-window-setup 'current-window))
+      (org-mode)
+      (insert "* Code\n")
+      (insert "#+begin_src emacs-lisp\n")
+      (insert "    (message \"one\")\n")
+      (insert "      (message \"two\")\n")
+      (insert "#+end_src\n")
+      (goto-char (point-min))
+      (search-forward "one")
+      (let (edit-before edit-after-save)
+        (org-edit-src-code)
+        (setq edit-before (buffer-substring-no-properties
+                           (point-min) (point-max)))
+        (goto-char (point-max))
+        (insert "  (message \"saved\")\n")
+        (org-edit-src-save)
+        (setq edit-after-save
+              (with-current-buffer (marker-buffer org-src--beg-marker)
+                (buffer-substring-no-properties (point-min) (point-max))))
+        (insert "  (message \"aborted\")\n")
+        (org-edit-src-abort)
+        (list edit-before
+              edit-after-save
+              (buffer-substring-no-properties (point-min) (point-max))))))"##,
+    );
+}
+
+#[test]
+fn org_edit_special_example_and_src_modes_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'org-src)
+  (with-temp-buffer
+    (let ((org-src-window-setup 'current-window)
+          (org-edit-fixed-width-region-mode 'fundamental-mode))
+      (org-mode)
+      (insert "* Mixed\n")
+      (insert "#+begin_example\n")
+      (insert "example line\n")
+      (insert "#+end_example\n\n")
+      (insert ": fixed\n: width\n\n")
+      (insert "#+begin_src emacs-lisp\n(+ 1 2)\n#+end_src\n")
+      (let (example-mode example-text fixed-mode fixed-text src-mode)
+        (goto-char (point-min))
+        (search-forward "example line")
+        (org-edit-special)
+        (setq example-mode major-mode
+              example-text (buffer-substring-no-properties
+                            (point-min) (point-max)))
+        (goto-char (point-max))
+        (insert "example added\n")
+        (org-edit-src-exit)
+        (goto-char (point-min))
+        (search-forward "fixed")
+        (org-edit-special)
+        (setq fixed-mode major-mode
+              fixed-text (buffer-substring-no-properties
+                          (point-min) (point-max)))
+        (goto-char (point-max))
+        (insert "third\n")
+        (org-edit-src-exit)
+        (goto-char (point-min))
+        (search-forward "(+ 1 2)")
+        (org-edit-special)
+        (setq src-mode major-mode)
+        (org-edit-src-abort)
+        (list example-mode
+              example-text
+              fixed-mode
+              fixed-text
+              src-mode
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))"##,
+    );
+}
+
+#[test]
+fn org_babel_update_body_remove_result_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (insert "#+NAME: calc\n")
+    (insert "#+begin_src emacs-lisp :results value replace\n")
+    (insert "(+ 1 2)\n")
+    (insert "#+end_src\n")
+    (insert "#+RESULTS: calc\n: 3\n\n")
+    (insert "Inline src_emacs-lisp[:results raw replace]{(+ 2 3)} {{{results(=5=)}}}.\n")
+    (goto-char (point-min))
+    (search-forward "(+ 1 2)")
+    (let ((info-before (org-babel-get-src-block-info)))
+      (org-babel-update-block-body "(let ((x 4))\n  (+ x 6))")
+      (let ((after-update
+             (buffer-substring-no-properties (point-min) (point-max))))
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-remove-result)
+        (goto-char (point-min))
+        (search-forward "src_emacs-lisp")
+        (org-babel-remove-inline-result)
+        (list (nth 1 info-before)
+              after-update
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))"##,
+    );
+}
