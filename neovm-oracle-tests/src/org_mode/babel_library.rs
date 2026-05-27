c1f3d0a7a1dcc9e750721b0a0b3ae3fca44aa361
Eval Exec <execvy@gmail.com>
2026-05-27 19:47:28 -0400

    oracle-tests: add Babel arithmetic comparison deep state combo
    Captures actual read-back values for arithmetic (add/mul/div/mod),
    comparison (</>/=/<=/>=), and math functions (abs/max/min/expt/
    sqrt). Also captures full buffer after all executions.
    
    Passing parity locks down Babel arithmetic fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,47 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_arithmetic_comparison_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Arithmetic
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(list :add (+ 1 2 3) :mul (* 4 5) :div (/ 10 3) :mod (% 10 3))\n")
+      (insert "#+end_src\n\n")
+      ;; Comparison
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(list (< 1 2 3) (> 3 2 1) (= 5 5) (<= 3 3) (>= 4 3))\n")
+      (insert "#+end_src\n\n")
+      ;; Math functions
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(list (abs -5) (max 1 3 2) (min 5 2 8) (expt 2 10) (sqrt 144))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 3)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_string_concat_format_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
