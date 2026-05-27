0dcdcb53821250d8e24567fa64d9b18100312da0
Eval Exec <execvy@gmail.com>
2026-05-27 17:56:55 -0400

    oracle-tests: add Babel header arg inherit override deep state combo
    Captures actual result values for inherited (3), output override,
    and file override modes. Also captures file content and normalized
    buffer text.
    
    Passing parity locks down Babel header arg inherit/override fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,67 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_header_arg_inherit_override_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; File-level property
+      (insert "#+PROPERTY: header-args :results value replace\n\n")
+      ;; Block inheriting file-level
+      (insert "#+NAME: inherited\n")
+      (insert "#+begin_src emacs-lisp\n")
+      (insert "(+ 1 2)\n")
+      (insert "#+end_src\n\n")
+      ;; Block with output override
+      (insert "#+NAME: output-override\n")
+      (insert "#+begin_src emacs-lisp :results output replace\n")
+      (insert "(princ \"overridden to output\")\n")
+      (insert "#+end_src\n\n")
+      ;; Block with file override
+      (let* ((root (make-temp-file "org-babel-inherit" t))
+             (out-file (expand-file-name "result.txt" root)))
+        (unwind-protect
+            (progn
+              (insert "#+NAME: file-override\n")
+              (insert "#+begin_src emacs-lisp :file " out-file "\n")
+              (insert "(with-temp-file \"" out-file "\"\n  (insert \"file content\"))\n  \"done\"\n")
+              (insert "#+end_src\n\n")
+              ;; Execute all
+              (dolist (name '("inherited" "output-override" "file-override"))
+                (goto-char (point-min))
+                (search-forward name)
+                (org-babel-execute-src-block))
+              ;; Read results
+              (let ((results nil))
+                (goto-char (point-min))
+                (while (re-search-forward "#\\+RESULTS:" nil t)
+                  (forward-line 1)
+                  (push (org-babel-read-result) results))
+                (let ((file-content
+                       (when (file-exists-p out-file)
+                         (with-temp-buffer
+                           (insert-file-contents out-file)
+                           (buffer-string)))))
+                  (list (nreverse results)
+                        (replace-regexp-in-string
+                         (regexp-quote root) "<root>"
+                         (or file-content "no-file"))
+                        (replace-regexp-in-string
+                         (regexp-quote root) "<root>"
+                         (buffer-substring-no-properties
+                          (point-min) (point-max))))))
+          (delete-directory root t))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_assign_header_var_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
