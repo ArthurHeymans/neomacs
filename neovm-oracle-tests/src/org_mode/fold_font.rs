552db6f00842749c677094986f02b04b2339cb83
Eval Exec <execvy@gmail.com>
2026-05-27 18:31:57 -0400

    oracle-tests: add Org fold subtree hide edit reveal font level v2 combo
    Captures per-heading (line-number/invisible/outline-level/face)
    after hide-subtree, hidden edit, and show-subtree. Scans for
    merged heading lines.
    
    Passing parity locks down hide-edit-reveal fidelity.

--- neovm-oracle-tests/src/org_mode/fold_font.rs
+++ neovm-oracle-tests/src/org_mode/fold_font.rs
@@ -685,6 +685,70 @@ fn org_cycle_startup_visibility_archived_drawers_combo() {
 }
 
 #[test]
+fn org_fold_subtree_hide_edit_reveal_font_level_v2_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'org-fold)
+  (with-temp-buffer
+    (let ((org-cycle-level-faces t)
+          (org-fontify-whole-heading-line t)
+          (org-fontify-todo-headline t))
+      (org-mode)
+      (insert "* TODO Root\n")
+      (insert "** DONE Alpha\n")
+      (insert "Alpha body.\n")
+      (insert "*** TODO Beta\n")
+      (insert "Beta body.\n")
+      (insert "**** WAIT Gamma\n")
+      (insert "Gamma body.\n")
+      (insert "** NEXT Sibling\n")
+      (insert "Sibling body.\n")
+      (font-lock-ensure (point-min) (point-max))
+      ;; Hide Beta subtree
+      (goto-char (point-min))
+      (search-forward "Beta")
+      (beginning-of-line)
+      (org-fold-hide-subtree)
+      ;; Edit while hidden
+      (end-of-line)
+      (insert "\n**** TODO Inserted under Beta\nInserted body.\n")
+      ;; Show subtree
+      (goto-char (point-min))
+      (search-forward "Beta")
+      (beginning-of-line)
+      (org-fold-show-subtree)
+      ;; Capture state
+      (let ((headings
+             (mapcar
+              (lambda (needle)
+                (save-excursion
+                  (goto-char (point-min))
+                  (if (search-forward needle nil t)
+                      (list needle
+                            (line-number-at-pos)
+                            (invisible-p (point))
+                            (org-outline-level)
+                            (get-text-property (line-beginning-position) 'face))
+                      (list needle 'not-found nil nil nil))))
+              '("Root" "Alpha" "Beta" "Gamma" "Inserted under Beta" "Sibling")))
+            (merged nil))
+        (dolist (line (split-string
+                       (buffer-substring-no-properties
+                        (point-min) (point-max))
+                       "\n" t))
+          (when (string-match-p "^\\*+ .*\\*+ " line)
+            (push line merged)))
+        (list headings
+              (nreverse merged)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_fold_indirect_buffer_decouple_font_level_v2_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
