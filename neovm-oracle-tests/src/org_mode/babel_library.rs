ec61a2d9167e14e4aec6b23c9149630bac0aec7e
Eval Exec <execvy@gmail.com>
2026-05-27 19:25:09 -0400

    oracle-tests: add Babel complex list structure deep combo
    Captures actual read-back values for nested assoc list structure
    and processed plist result. Also captures full buffer after all
    executions.
    
    Passing parity locks down Babel complex structure fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,48 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_complex_list_structure_deep_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Nested structure
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "'((users . ((name . \"Ada\") (age . 30)))\n")
+      (insert "  (scores . (95 87 92 88))\n")
+      (insert "  (meta . ((created . \"2026-05-27\") (version . 2))))\n")
+      (insert "#+end_src\n\n")
+      ;; Process structure
+      (insert "#+begin_src emacs-lisp :var data=src_1 :results value replace\n")
+      (insert "(list :user-name (cdr (assoc 'name (cdr (assoc 'users data))))\n")
+      (insert "      :avg-score (/ (apply #'+ (cdr (assoc 'scores data)))\n")
+      (insert "                    (length (cdr (assoc 'scores data))))\n")
+      (insert "      :version (cdr (assoc 'version (cdr (assoc 'meta data)))))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute
+      (dotimes (_ 2)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_result_type_boolean_vector_deep_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
