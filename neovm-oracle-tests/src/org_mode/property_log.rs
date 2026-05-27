3a5a8fb787ef98494baad9367fe5993961b76aac
Eval Exec <execvy@gmail.com>
2026-05-27 13:19:06 +0800

    oracle-tests: add Org property log workflows
    Covers property set/delete cycling, DONE note logs,
    
    repeat scheduling logs, and drawer normalization.
    
    No divergence surfaced in this batch.

--- neovm-oracle-tests/src/org_mode/property_log.rs
+++ neovm-oracle-tests/src/org_mode/property_log.rs
@@ -152,3 +152,108 @@ fn org_property_values_global_delete_roundtrip_combo() {
             (buffer-substring-no-properties (point-min) (point-max))))))"##,
     );
 }
+
+#[test]
+fn org_property_set_delete_allowed_values_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (with-temp-buffer
+    (org-mode)
+    (insert "#+PROPERTY: Phase_ALL Plan Build Ship\n")
+    (insert "* Project\n")
+    (insert "** Task\n")
+    (goto-char (point-min))
+    (search-forward "Task")
+    (beginning-of-line)
+    (let ((org-last-set-property "Phase")
+          (org-last-set-property-value "Build"))
+      (org-set-property "Phase" "Plan")
+      (org-set-property "Owner" "Ada")
+      (let ((after-set (buffer-substring-no-properties
+                        (point-min) (point-max)))
+            (allowed (org-property-get-allowed-values nil "Phase" 'table)))
+        (org-delete-property "Owner")
+        (search-forward ":Phase:")
+        (org-property-next-allowed-value)
+        (org-property-next-allowed-value)
+        (org-property-next-allowed-value)
+        (list after-set
+              allowed
+              (org-entry-properties nil 'standard)
+              org-last-set-property
+              org-last-set-property-value
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))"##,
+    );
+}
+
+#[test]
+fn org_todo_done_note_log_drawer_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* TODO Task\n")
+    (goto-char (point-min))
+    (let ((org-log-into-drawer "LOGBOOK")
+          (org-log-done 'note)
+          (org-log-note-how 'time)
+          (org-log-note-clock-out nil)
+          (org-log-note-headings
+           '((done . "State %-12s from %-12S %t")
+             (note . "Note taken on %t"))))
+      (cl-letf (((symbol-function 'read-string)
+                 (lambda (&rest _) "Finished carefully"))
+                ((symbol-function 'read-char-exclusive)
+                 (lambda (&rest _) ?\C-c)))
+        (org-todo "DONE")
+        (when (and (boundp 'org-log-note-marker)
+                   org-log-note-marker)
+          (with-current-buffer (marker-buffer org-log-note-marker)
+            (goto-char org-log-note-marker)
+            (insert "Finished carefully")
+            (org-add-log-note))))
+      (list (org-entry-get nil "CLOSED")
+            (replace-regexp-in-string
+             "\\[[0-9][^]\n]+\\]"
+             "[stamp]"
+             (buffer-substring-no-properties
+              (point-min) (point-max))))))"##,
+    );
+}
+
+#[test]
+fn org_log_repeat_reschedule_redeadline_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* TODO Repeater\n")
+    (insert "SCHEDULED: <2026-05-27 Wed +1w>\n")
+    (insert "DEADLINE: <2026-05-28 Thu +2w>\n")
+    (goto-char (point-min))
+    (let ((org-log-into-drawer t)
+          (org-log-reschedule 'time)
+          (org-log-redeadline 'time)
+          (org-log-repeat 'time)
+          (org-log-done nil))
+      (org-schedule nil "2026-06-03")
+      (org-deadline nil "2026-06-11")
+      (org-todo "DONE")
+      (list (org-entry-get nil "LAST_REPEAT")
+            (replace-regexp-in-string
+             "\\[[0-9][^]\n]+\\]"
+             "[stamp]"
+             (buffer-substring-no-properties
+              (point-min) (point-max))))))"##,
+    );
+}
