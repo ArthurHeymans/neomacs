cdd33e83f84181f43d61fce50c9297d9d65776ca
Eval Exec <execvy@gmail.com>
2026-05-27 18:07:05 -0400

    oracle-tests: add Org fold cut paste subtree reexpand font integrity combo
    Captures per-heading (line-number/invisible/outline-level/face)
    after cycle, show-all, cut-subtree, paste-subtree, cycle-overview,
    and show-all. Scans for merged heading lines.
    
    Passing parity locks down cut-paste subtree reexpand fidelity.

--- neovm-oracle-tests/src/org_mode/fold_font.rs
+++ neovm-oracle-tests/src/org_mode/fold_font.rs
@@ -685,6 +685,74 @@ fn org_cycle_startup_visibility_archived_drawers_combo() {
 }
 
 #[test]
+fn org_fold_cut_paste_subtree_reexpand_font_integrity_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'org-cycle)
+  (require 'org-fold)
+  (with-temp-buffer
+    (let ((org-cycle-level-faces t)
+          (org-fontify-whole-heading-line t))
+      (org-mode)
+      (insert "* A\nbody A\n")
+      (insert "** B\nbody B\n")
+      (insert "*** C\nbody C\n")
+      (insert "* E\nbody E\n")
+      (font-lock-ensure (point-min) (point-max))
+      ;; Cycle B
+      (goto-char (point-min))
+      (search-forward "B")
+      (beginning-of-line)
+      (org-cycle)
+      (org-cycle)
+      ;; Show all
+      (org-fold-show-all)
+      ;; Cut C subtree
+      (goto-char (point-min))
+      (search-forward "C")
+      (beginning-of-line)
+      (org-cut-subtree)
+      ;; Paste under E
+      (goto-char (point-min))
+      (search-forward "E")
+      (beginning-of-line)
+      (org-paste-subtree 2)
+      ;; Cycle overview
+      (org-cycle-overview)
+      (org-fold-show-all)
+      (font-lock-ensure (point-min) (point-max))
+      ;; Check state
+      (let ((headings
+             (mapcar
+              (lambda (needle)
+                (save-excursion
+                  (goto-char (point-min))
+                  (if (search-forward needle nil t)
+                      (list needle
+                            (line-number-at-pos)
+                            (invisible-p (point))
+                            (org-outline-level)
+                            (get-text-property (line-beginning-position) 'face))
+                      (list needle 'not-found nil nil nil))))
+              '("A" "body A" "B" "body B" "C" "body C" "E" "body E")))
+            (merged nil))
+        (dolist (line (split-string
+                       (buffer-substring-no-properties
+                        (point-min) (point-max))
+                       "\n" t))
+          (when (string-match-p "^\\*+ .*\\*+ " line)
+            (push line merged)))
+        (list headings
+              (nreverse merged)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_fold_startup_visibility_property_cycle_font_deep_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
