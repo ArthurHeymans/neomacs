b08dfe37b8034406f3531d389a6b26cb312c60d8
Eval Exec <execvy@gmail.com>
2026-05-27 18:26:35 -0400

    oracle: 40 uncovered-features-7 tests, total 6287
    Tests for previously uncovered org-mode features:
    - org-at-heading-p at various positions
    - org-at-table-p at various positions
    - org-at-item-p at various positions
    - org-at-planning-p at various positions
    - org-at-property-p at various positions
    - org-at-comment-p at various positions
    - org-at-timestamp-p at various positions
    - org-at-block-p at various positions
    - org-at-drawer-p at various positions
    - footnote-at-reference-p/definition-p
    - org-at-clock-log-p
    - org-list-struct
    - org-list-prevs-alist
    - org-list-parents-alist
    - org-element-parent-chain deep
    - org-element-lineage
    - org-element-contents
    - org-element-type-p
    - org-element-map no-recursion
    - org-element-map first-match
    - org-element-map with info
    - org-id-get-create
    - org-clock-sum
    - org-store-link
    - org-sort-entries
    - org-clone-subtree
    - org-copy/paste-subtree
    - org-mark-subtree
    - org-narrow-to-subtree
    - org-end-of-subtree
    - org-cycle-hide-drawers
    - org-toggle-heading
    - org-move-item
    - org-insert-todo-heading
    - org-move-subtree
    - org-dblock-update
    - org-macro-replace-all
    - org-try-structure-completion
    - org-update-statistics-cookies
    - org-match-sparse-tree

--- neovm-oracle-tests/src/org_mode/mod.rs
+++ neovm-oracle-tests/src/org_mode/mod.rs
@@ -1529,6 +1529,7 @@ mod strong_uncovered_features_3;
 mod strong_uncovered_features_4;
 mod strong_uncovered_features_5;
 mod strong_uncovered_features_6;
+mod strong_uncovered_features_7;
 mod strong_workflow_combos;
 mod strong_xenna_combos;
 mod strong_xero_combos;
