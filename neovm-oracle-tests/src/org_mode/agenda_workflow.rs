c319c6f8fcf6c3f4ddd4c881dcd50259046d2fee
Eval Exec <execvy@gmail.com>
2026-05-27 14:32:31 -0400

    oracle-tests: add Org agenda date shift redo source mutation deep state
    Captures full agenda text before/after date shift, after redo,
    and source file content to verify date mutation propagates
    correctly back to the org file.
    
    Passing parity locks down agenda date shift and redo fidelity.

--- neovm-oracle-tests/src/org_mode/agenda_workflow.rs
+++ neovm-oracle-tests/src/org_mode/agenda_workflow.rs
@@ -1128,3 +1128,64 @@ fn org_agenda_filter_tag_todo_match_deep_state_combo() {
       (delete-directory root t))))"##,
     );
 }
+
+#[test]
+fn org_agenda_date_shift_redo_source_mutation_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'org-agenda)
+  (let* ((root (make-temp-file "org-agenda-shift" t))
+         (file (expand-file-name "tasks.org" root))
+         (org-agenda-files (list file))
+         (org-agenda-span 'week)
+         (org-agenda-start-day "2026-05-25")
+         (org-agenda-start-on-weekday 1)
+         (org-agenda-use-time-grid nil))
+    (unwind-protect
+        (progn
+          (with-temp-file file
+            (insert "* TODO Alpha\n")
+            (insert "SCHEDULED: <2026-05-27 Wed>\n")
+            (insert "* TODO Beta\n")
+            (insert "SCHEDULED: <2026-05-28 Thu>\n")
+            (insert "* TODO Gamma\n")
+            (insert "DEADLINE: <2026-05-29 Fri>\n"))
+          (org-agenda-list nil "2026-05-25" 7)
+          (with-current-buffer org-agenda-buffer-name
+            (let ((initial (replace-regexp-in-string
+                            (regexp-quote root) "<root>"
+                            (buffer-substring-no-properties
+                             (point-min) (point-max)))))
+              (goto-char (point-min))
+              (search-forward "Alpha")
+              (beginning-of-line)
+              (org-agenda-date-later 1)
+              (let ((after-shift (replace-regexp-in-string
+                                  (regexp-quote root) "<root>"
+                                  (buffer-substring-no-properties
+                                   (point-min) (point-max)))))
+                (org-agenda-redo)
+                (let ((after-redo (replace-regexp-in-string
+                                   (regexp-quote root) "<root>"
+                                   (buffer-substring-no-properties
+                                    (point-min) (point-max)))))
+                  (let ((source-content
+                         (with-current-buffer (find-file-noselect file)
+                           (prog1 (buffer-substring-no-properties
+                                   (point-min) (point-max))
+                             (kill-buffer)))))
+                    (list initial
+                          after-shift
+                          after-redo
+                          (replace-regexp-in-string
+                           (regexp-quote root) "<root>"
+                           source-content))))))))
+      (when (get-buffer org-agenda-buffer-name)
+        (kill-buffer org-agenda-buffer-name))
+      (when (get-file-buffer file) (kill-buffer (get-file-buffer file)))
+      (delete-directory root t))))"##,
+    );
+}
