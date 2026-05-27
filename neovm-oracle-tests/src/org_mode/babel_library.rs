c969e5b91e1784045993d116cba1c33ea3f3ef3c
Eval Exec <execvy@gmail.com>
2026-05-27 18:19:33 -0400

    oracle-tests: add Babel output format string deep state combo
    Captures actual read-back values for multiline output (5 lines)
    and formatted output (key=test val=42 pi=3.14). Also captures
    full buffer after all executions.
    
    Passing parity locks down Babel output format fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,45 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_output_format_string_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Multi-line output
+      (insert "#+NAME: multiline\n")
+      (insert "#+begin_src emacs-lisp :results output replace\n")
+      (insert "(dotimes (i 5)\n  (princ (format \"line-%d\\n\" i)))\n")
+      (insert "#+end_src\n\n")
+      ;; Formatted output
+      (insert "#+NAME: formatted\n")
+      (insert "#+begin_src emacs-lisp :results output replace\n")
+      (insert "(princ (format \"key=%s val=%d pi=%.2f\" \"test\" 42 3.14159))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute
+      (dolist (name '("multiline" "formatted"))
+        (goto-char (point-min))
+        (search-forward name)
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_multiple_named_results_order_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
