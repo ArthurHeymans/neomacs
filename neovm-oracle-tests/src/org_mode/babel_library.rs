b47240a54b4b4425f943cbe4f13b07c8c6d78068
Eval Exec <execvy@gmail.com>
2026-05-27 18:58:16 -0400

    oracle-tests: add Babel block var ref result order deep combo
    Captures actual read-back values for producer (assoc list),
    consumer (doubled assoc list), and aggregator (sum=120).
    Also captures full buffer after all executions.
    
    Passing parity locks down Babel block var ref chain fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,52 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_block_var_ref_result_order_deep_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Producer block
+      (insert "#+NAME: producer\n")
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "'((a . 10) (b . 20) (c . 30))\n")
+      (insert "#+end_src\n\n")
+      ;; Consumer block with var ref
+      (insert "#+NAME: consumer\n")
+      (insert "#+begin_src emacs-lisp :var data=producer :results value replace\n")
+      (insert "(mapcar (lambda (pair)\n")
+      (insert "          (cons (car pair) (* 2 (cdr pair))))\n")
+      (insert "        data)\n")
+      (insert "#+end_src\n\n")
+      ;; Aggregator block
+      (insert "#+NAME: aggregator\n")
+      (insert "#+begin_src emacs-lisp :var data=consumer :results value replace\n")
+      (insert "(apply #'+ (mapcar #'cdr data))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute chain
+      (dolist (name '("producer" "consumer" "aggregator"))
+        (goto-char (point-min))
+        (search-forward name)
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_table_named_result_header_deep_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
