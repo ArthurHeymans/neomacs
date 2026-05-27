63b804f8e3b59ad1feb6cc737d592f3cf2577869
Eval Exec <execvy@gmail.com>
2026-05-27 13:12:50 +0800

    oracle-tests: add Org include macro workflows
    Covers include location expansion, line-restricted
    
    literal blocks, footnotes, and nested macro counters.
    
    No divergence surfaced in this batch.

--- neovm-oracle-tests/src/org_mode/macro_include.rs
+++ neovm-oracle-tests/src/org_mode/macro_include.rs
@@ -73,3 +73,106 @@ fn org_macro_html_export_markup_combo() {
              html)))))"##,
     );
 }
+
+#[test]
+fn org_include_location_only_contents_footnotes_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'ox)
+  (let* ((root (make-temp-file "org-include-location" t))
+         (inc (expand-file-name "chapters.org" root)))
+    (unwind-protect
+        (progn
+          (with-temp-file inc
+            (insert "#+TITLE: Included\n")
+            (insert "* Prelude\nSkip me.\n")
+            (insert "* Target\n")
+            (insert "SCHEDULED: <2026-05-27 Wed>\n")
+            (insert ":PROPERTIES:\n:CUSTOM_ID: target\n:END:\n")
+            (insert "First body [fn:local].\n")
+            (insert "** Child\nChild body.\n")
+            (insert "[fn:local] Included footnote.\n"))
+          (with-temp-buffer
+            (org-mode)
+            (insert "#+TITLE: Main\n")
+            (insert "* Parent\n")
+            (insert "#+INCLUDE: \"" inc "::* Target\" :only-contents t :minlevel 3\n")
+            (insert "* After\n")
+            (goto-char (point-min))
+            (org-export-expand-include-keyword nil root nil nil nil)
+            (let ((tree (org-element-parse-buffer)))
+              (list (org-element-map tree 'headline
+                      (lambda (h)
+                        (list (org-element-property :level h)
+                              (org-element-property :raw-value h))))
+                    (org-element-map tree 'footnote-definition
+                      (lambda (f) (org-element-property :label f)))
+                    (buffer-substring-no-properties
+                     (point-min) (point-max)))))))
+      (delete-directory root t))))"##,
+    );
+}
+
+#[test]
+fn org_include_literal_blocks_lines_parse_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'ox)
+  (let* ((root (make-temp-file "org-include-literal" t))
+         (src (expand-file-name "snippet.el" root))
+         (txt (expand-file-name "notes.txt" root)))
+    (unwind-protect
+        (progn
+          (with-temp-file src
+            (insert ";; one\n(message \"two\")\n(message \"three\")\n;; four\n"))
+          (with-temp-file txt
+            (insert "alpha\nbeta <tag>\ngamma\n"))
+          (with-temp-buffer
+            (org-mode)
+            (insert "#+INCLUDE: \"" src "\" src emacs-lisp :lines \"2-3\" -n\n")
+            (insert "#+INCLUDE: \"" txt "\" example :lines \"1-2\"\n")
+            (goto-char (point-min))
+            (let ((parsed-src (org-export-parse-include-value
+                               (concat "\"" src "\" src emacs-lisp :lines \"2-3\" -n")
+                               root))
+                  (parsed-example (org-export-parse-include-value
+                                   (concat "\"" txt "\" example :lines \"1-2\"")
+                                   root)))
+              (org-export-expand-include-keyword nil root nil nil nil)
+              (list parsed-src
+                    parsed-example
+                    (buffer-substring-no-properties
+                     (point-min) (point-max)))))))
+      (delete-directory root t))))"##,
+    );
+}
+
+#[test]
+fn org_macro_counter_nested_replacement_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'org-macro)
+  (with-temp-buffer
+    (org-mode)
+    (insert "#+MACRO: wrap <<$1>>\n")
+    (insert "#+MACRO: pair $1={{{$2}}}\n")
+    (insert "A {{{counter(seq)}}}; B {{{counter(seq,+3)}}}; ")
+    (insert "C {{{counter(seq)}}}; D {{{counter(seq,-1)}}}; ")
+    (insert "E {{{wrap(text)}}}; F {{{pair(label,wrap(value))}}}.\n")
+    (let ((templates (org-macro--collect-macros)))
+      (list (mapcar #'car templates)
+            (org-macro-expand "wrap(text)" templates)
+            (org-macro-expand "pair(label,wrap(value))" templates)
+            (progn
+              (org-macro-replace-all templates)
+              (buffer-substring-no-properties
+               (point-min) (point-max)))))))"##,
+    );
+}
