18acc10b438025c30bd7827a6b0fcc835d12fce0
Eval Exec <execvy@gmail.com>
2026-05-27 22:30:30 -0400

    oracle-tests: add Babel hash table operations deep state combo
    Captures actual read-back values for hash table create/get/
    count/remove/missing-default (Ada/30/default/3/2) and hash-to-
    alist conversion with maphash. Also captures full buffer.
    
    Passing parity locks down Babel hash table operation fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,57 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_hash_table_operations_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Create and populate hash
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((ht (make-hash-table :test #'equal)))\n")
+      (insert "  (puthash \"name\" \"Ada\" ht)\n")
+      (insert "  (puthash \"age\" 30 ht)\n")
+      (insert "  (puthash \"lang\" \"elisp\" ht)\n")
+      (insert "  (list (gethash \"name\" ht)\n")
+      (insert "        (gethash \"age\" ht)\n")
+      (insert "        (gethash \"missing\" ht \"default\")\n")
+      (insert "        (hash-table-count ht)\n")
+      (insert "        (remhash \"age\" ht)\n")
+      (insert "        (hash-table-count ht)))\n")
+      (insert "#+end_src\n\n")
+      ;; Hash to alist
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((ht (make-hash-table)))\n")
+      (insert "  (puthash 'x 10 ht) (puthash 'y 20 ht) (puthash 'z 30 ht)\n")
+      (insert "  (let ((al nil))\n")
+      (insert "    (maphash (lambda (k v) (push (cons k v) al)) ht)\n")
+      (insert "    (sort al (lambda (a b) (string< (symbol-name (car a))\n")
+      (insert "                                      (symbol-name (car b))))))))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 2)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_property_list_alist_conversion_deep_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
