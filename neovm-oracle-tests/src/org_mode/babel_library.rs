aaa33d02e51a676cda4e6716a3ef565e335c8244
Eval Exec <execvy@gmail.com>
2026-05-27 21:12:45 -0400

    oracle-tests: add Babel char alist plist deep state combo
    Captures actual read-back values for character operations (A/B/C/α),
    alist lookup (y.20/30.z/3), and plist get/member (Ada/30/t).
    Also captures full buffer after all executions.
    
    Passing parity locks down Babel char/alist/plist fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,49 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_char_alist_plist_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Character operations
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(list (char-to-string 65) (string-to-char \"B\") ?C (char-to-string 945))\n")
+      (insert "#+end_src\n\n")
+      ;; Alist
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((al '((x . 10) (y . 20) (z . 30))))\n")
+      (insert "  (list (assq 'y al) (rassoc 30 al) (length al)))\n")
+      (insert "#+end_src\n\n")
+      ;; Plist
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((pl (list :name \"Ada\" :age 30 :lang \"elisp\")))\n")
+      (insert "  (list (plist-get pl :name) (plist-get pl :age) (plist-member pl :lang)))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 3)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_recursive_fib_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
