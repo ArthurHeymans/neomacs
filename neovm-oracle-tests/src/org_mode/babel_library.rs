c4dcca1bff05e144ccaa39014d7bf778d44309c3
Eval Exec <execvy@gmail.com>
2026-05-28 16:37:09 -0400

    oracle-tests: add Babel eight block chain deep combo
    Captures actual read-back values for 8-block chain: seed (5/3/8
    /1/9/2/7/4/6), sorted (1-9), doubled (2-18), big (10/12/14/16/
    18), reversed (18/16/14/12/10), total (70). Also captures full
    buffer.
    
    Passing parity locks down Babel eight-block chain fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,65 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_eight_block_chain_deep_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Seed
+      (insert "#+NAME: seed\n")
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "'(5 3 8 1 9 2 7 4 6)\n")
+      (insert "#+end_src\n\n")
+      ;; Sorted asc
+      (insert "#+NAME: sorted\n")
+      (insert "#+begin_src emacs-lisp :var data=seed :results value replace\n")
+      (insert "(sort (copy-sequence data) #'<)\n")
+      (insert "#+end_src\n\n")
+      ;; Doubled
+      (insert "#+NAME: doubled\n")
+      (insert "#+begin_src emacs-lisp :var data=sorted :results value replace\n")
+      (insert "(mapcar (lambda (x) (* x 2)) data)\n")
+      (insert "#+end_src\n\n")
+      ;; Filter > 8
+      (insert "#+NAME: big\n")
+      (insert "#+begin_src emacs-lisp :var data=doubled :results value replace\n")
+      (insert "(cl-remove-if-not (lambda (x) (> x 8)) data)\n")
+      (insert "#+end_src\n\n")
+      ;; Reversed
+      (insert "#+NAME: reversed\n")
+      (insert "#+begin_src emacs-lisp :var data=big :results value replace\n")
+      (insert "(reverse data)\n")
+      (insert "#+end_src\n\n")
+      ;; Total
+      (insert "#+NAME: total\n")
+      (insert "#+begin_src emacs-lisp :var data=reversed :results value replace\n")
+      (insert "(apply #'+ data)\n")
+      (insert "#+end_src\n\n")
+      ;; Execute chain
+      (dolist (name '("sorted" "doubled" "big" "reversed" "total"))
+        (goto-char (point-min))
+        (search-forward name)
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_seven_block_chain_deep_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
