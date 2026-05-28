68efbd04a51f31689a96cc7bd23e37431f24dd7a
Eval Exec <execvy@gmail.com>
2026-05-28 15:17:38 -0400

    oracle-tests: add Babel two table join sort deep combo
    Captures actual read-back values for ranked workers (W1.8/
    W3.7/W2.5) sorted by hours descending via assoc join. Also
    captures full buffer.
    
    Passing parity locks down Babel two-table join-sort fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,62 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_two_table_join_sort_deep_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Workers
+      (insert "#+NAME: workers\n")
+      (insert "| ID | Name | Dept |\n")
+      (insert("|----+------+------|\n")
+      (insert "| 1 | W1 | A |\n")
+      (insert "| 2 | W2 | B |\n")
+      (insert "| 3 | W3 | A |\n\n")
+      ;; Hours
+      (insert "#+NAME: hours\n")
+      (insert "| ID | Hrs |\n")
+      (insert("|----+-----|\n")
+      (insert "| 1 | 8 |\n")
+      (insert "| 2 | 5 |\n")
+      (insert "| 3 | 7 |\n\n")
+      ;; Join and sort by hours desc
+      (insert "#+NAME: ranked\n")
+      (insert "#+begin_src emacs-lisp :var w=workers h=hours :results value replace\n")
+      (insert "(sort\n")
+      (insert " (mapcar\n")
+      (insert "  (lambda (hour)\n")
+      (insert "    (let* ((id (car hour))\n")
+      (insert "           (hrs (cadr hour))\n")
+      (insert "           (wk (assoc id w))\n")
+      (insert "           (name (cadr wk)))\n")
+      (insert "      (list name hrs)))\n")
+      (insert "  h)\n")
+      (insert " (lambda (a b) (> (cadr a) (cadr b))))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute
+      (goto-char (point-min))
+      (search-forward "ranked")
+      (org-babel-execute-src-block)
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_multi_block_chain_aggregate_deep_v2_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
