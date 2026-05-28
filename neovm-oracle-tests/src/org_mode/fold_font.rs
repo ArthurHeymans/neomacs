fae599df338fe65f72548f06896cdf3d63244267
Eval Exec <execvy@gmail.com>
2026-05-28 17:20:27 -0400

    oracle-tests: add Org fold subtree hide edit show font face v35 combo
    Captures per-heading (line-number/invisible/outline-level/face)
    after hide-Beta-subtree, hidden DONE insert, and show-all.
    Scans for merged heading lines.
    
    Passing parity locks down Beta hide+DONE-insert+show v35.

--- neovm-oracle-tests/src/org_mode/fold_font.rs
+++ neovm-oracle-tests/src/org_mode/fold_font.rs
@@ -685,6 +685,62 @@ fn org_cycle_startup_visibility_archived_drawers_combo() {
 }
 
 #[test]
+fn org_fold_subtree_hide_edit_show_font_face_v35_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'org-fold)
+  (with-temp-buffer
+    (let ((org-cycle-level-faces t)
+          (org-fontify-whole-heading-line t)
+          (org-fontify-done-headline t))
+      (org-mode)
+      (insert "* DONE Root\n")
+      (insert "** TODO Alpha\n")
+      (insert "*** DONE Beta\n")
+      (insert "**** WAIT Gamma\n")
+      (insert "** NEXT Sibling\n")
+      (font-lock-ensure (point-min) (point-max))
+      ;; Hide Beta, edit
+      (goto-char (point-min))
+      (search-forward "Beta")
+      (beginning-of-line)
+      (org-fold-hide-subtree)
+      (end-of-line)
+      (insert "\n**** DONE Inserted under Beta\nInserted body.\n")
+      ;; Show all
+      (org-fold-show-all)
+      (font-lock-ensure (point-min) (point-max))
+      ;; Check
+      (let ((headings
+             (mapcar
+              (lambda (needle)
+                (save-excursion
+                  (goto-char (point-min))
+                  (if (search-forward needle nil t)
+                      (list needle (line-number-at-pos)
+                            (invisible-p (point))
+                            (org-outline-level)
+                            (get-text-property (line-beginning-position) 'face))
+                      (list needle 'not-found nil nil nil))))
+              '("Root" "Alpha" "Beta" "Gamma" "Inserted" "SIBLING")))
+            (merged nil))
+        (dolist (line (split-string
+                       (buffer-substring-no-properties
+                        (point-min) (point-max))
+                       "\n" t))
+          (when (string-match-p "^\\*+ .*\\*+ " line)
+            (push line merged)))
+        (list headings
+              (nreverse merged)
+              (buffer-substring-no-properties
+               (point-min) (point-max)))))))"##,
+    );
+}
+
+#[test]
 fn org_fold_subtree_hide_edit_show_font_face_v34_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
