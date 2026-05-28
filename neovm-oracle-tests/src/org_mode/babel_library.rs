fdd44b25003b7775736da9e809a09726b7649aa1
Eval Exec <execvy@gmail.com>
2026-05-28 13:46:26 -0400

    oracle-tests: add Babel table var column sort chain deep v2 combo
    Captures actual read-back values for sorted rows (3.9/4.8/2.6/
    1.5) and v-total (28) via sort descending. Also captures full
    buffer.
    
    Passing parity locks down Babel table sort-chain v2 fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,50 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_table_var_column_sort_chain_deep_v2_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Source table
+      (insert "#+NAME: src\n")
+      (insert "| N | V |\n")
+      (insert("|---+---|\n")
+      (insert "| 4 | 8 |\n")
+      (insert "| 1 | 5 |\n")
+      (insert "| 3 | 9 |\n")
+      (insert "| 2 | 6 |\n\n")
+      ;; Sort by V desc, compute
+      (insert "#+NAME: sorted\n")
+      (insert "#+begin_src emacs-lisp :var tbl=src :results value replace\n")
+      (insert "(let ((s (sort (copy-sequence tbl)\n")
+      (insert "               (lambda (a b) (> (cadr a) (cadr b))))))\n")
+      (insert "  (list :sorted s\n")
+      (insert "        :v-total (apply #'+ (mapcar #'cadr s))))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute
+      (goto-char (point-min))
+      (search-forward "sorted")
+      (org-babel-execute-src-block)
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_table_filter_sort_chain_deep_v3_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
