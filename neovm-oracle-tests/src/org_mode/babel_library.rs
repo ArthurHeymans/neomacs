c7a443afd595503e50f8b6009078fb061edef3a2
Eval Exec <execvy@gmail.com>
2026-05-28 17:25:40 -0400

    oracle-tests: add Babel fifteen block chain deep combo
    Captures actual read-back values for seed/sorted/running chain:
    seed=(10 5 8 3 7 1 9 2 6 4), sorted=(1-10), running sum pairs.
    Also captures full buffer.
    
    Passing parity locks down Babel fifteen-block chain fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,50 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_fifteen_block_chain_deep_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Seed
+      (insert "#+NAME: seed\n")
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "'(10 5 8 3 7 1 9 2 6 4)\n")
+      (insert "#+end_src\n\n")
+      ;; Sorted
+      (insert "#+NAME: sorted\n")
+      (insert "#+begin_src emacs-lisp :var data=seed :results value replace\n")
+      (insert "(sort (copy-sequence data) #'<)\n")
+      (insert "#+end_src\n\n")
+      ;; Running sum
+      (insert "#+NAME: running\n")
+      (insert "#+begin_src emacs-lisp :var data=sorted :results value replace\n")
+      (insert "(let ((sum 0))\n  (mapcar (lambda (x) (setq sum (+ sum x)) (list x sum)) data))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute chain
+      (dolist (name '("sorted" "running"))
+        (goto-char (point-min))
+        (search-forward name)
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_fourteen_block_chain_deep_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
