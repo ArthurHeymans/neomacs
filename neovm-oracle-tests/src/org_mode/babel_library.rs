18ae32a7e7ec58a44002d0cd6fa418207d3a786d
Eval Exec <execvy@gmail.com>
2026-05-28 03:56:49 -0400

    oracle-tests: add Babel map assoc sort reverse deep combo
    Captures actual read-back values for sorted assoc list by value,
    reversed sorted list, and nested mapcar matrix with sum (285).
    Also captures full buffer.
    
    Passing parity locks down Babel map/sort/reverse fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,52 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_map_assoc_sort_reverse_deep_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Map + assoc
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((data (mapcar (lambda (x) (cons x (* x x)))\n")
+      (insert "                    '(3 1 4 1 5 9 2 6))))\n")
+      (insert "  (list data\n")
+      (insert "        (sort (copy-sequence data) (lambda (a b) (< (cdr a) (cdr b))))\n")
+      (insert "        (reverse (sort (copy-sequence data) (lambda (a b) (< (cdr a) (cdr b)))))))\n")
+      (insert "#+end_src\n\n")
+      ;; Nested mapcar with lambda
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((matrix (mapcar (lambda (row)\n")
+      (insert "                        (mapcar (lambda (cell) (* cell cell))\n")
+      (insert "                                row))\n")
+      (insert "                      '((1 2 3) (4 5 6) (7 8 9)))))\n")
+      (insert "  (list matrix\n")
+      (insert "        (apply #'+ (mapcar (lambda (row) (apply #'+ row)) matrix))))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 2)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_string_replace_concat_length_deep_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
