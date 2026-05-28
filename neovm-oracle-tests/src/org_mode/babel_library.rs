91051fd21fb525c4b67f404628e72dfdea1209d5
Eval Exec <execvy@gmail.com>
2026-05-28 02:37:37 -0400

    oracle-tests: add Babel catch throw block deep state combo
    Captures actual read-back values for catch/throw (5), catch
    multi-handler (42/6/(x y)), and nested catch/throw (escaped).
    Also captures full buffer.
    
    Passing parity locks down Babel catch/throw fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,47 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_catch_throw_block_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; catch/throw success
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(catch 'done\n  (dotimes (i 10)\n    (when (= i 5) (throw 'done i))))\n")
+      (insert "#+end_src\n\n")
+      ;; catch/throw with handler
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(list\n  (catch 'a (throw 'a 42))\n  (catch 'b (+ 1 2 3))\n  (catch 'c (throw 'c (list 'x 'y))))\n")
+      (insert "#+end_src\n\n")
+      ;; Nested catch
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(catch 'outer\n  (catch 'inner\n    (throw 'outer 'escaped))\n  'not-reached)\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 3)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_cl_loop_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
