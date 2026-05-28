75e3997a2c68a30bb4d9482b395dc98e3abf59e8
Eval Exec <execvy@gmail.com>
2026-05-27 20:30:18 -0400

    oracle-tests: add Babel mapcar filter reduce deep state combo
    Captures actual read-back values for mapcar (1 4 9 16 25),
    seq-filter (12 20 15), seq-reduce (15), and combined
    evens+squares+total (30). Also captures full buffer.
    
    Passing parity locks down Babel higher-order function fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,55 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_mapcar_filter_reduce_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; mapcar
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(mapcar (lambda (x) (* x x)) '(1 2 3 4 5))\n")
+      (insert "#+end_src\n\n")
+      ;; filter (remove-if-not)
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(seq-filter (lambda (x) (> x 10)) '(5 12 8 20 3 15))\n")
+      (insert "#+end_src\n\n")
+      ;; reduce
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(seq-reduce #'+ '(1 2 3 4 5) 0)\n")
+      (insert "#+end_src\n\n")
+      ;; Combined
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let* ((data '(1 2 3 4 5 6 7 8 9 10))\n")
+      (insert "       (evens (seq-filter #'evenp data))\n")
+      (insert "       (squares (mapcar (lambda (x) (* x x)) evens))\n")
+      (insert "       (total (seq-reduce #'+ squares 0)))\n")
+      (insert "  (list :evens evens :squares squares :total total))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 4)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_file_output_tangle_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
