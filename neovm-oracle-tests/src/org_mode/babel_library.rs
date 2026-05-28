34bcadf7414d67ea2ff8fdaf4d900f01b5c27754
Eval Exec <execvy@gmail.com>
2026-05-28 04:15:20 -0400

    oracle-tests: add Babel seq partition flatten deep state combo
    Captures actual read-back values for seq-partition ((1 2 3)(4 5 6)
    (7 8 9)), append flatten, cl-loop flatten, and seq-uniq of
    nested flatten. Also captures full buffer.
    
    Passing parity locks down Babel seq partition/flatten fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,46 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_seq_partition_flatten_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; seq-partition
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(seq-partition '(1 2 3 4 5 6 7 8 9) 3)\n")
+      (insert "#+end_src\n\n")
+      ;; Flatten nested
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((nested '((1 2) (3 (4 5)) (6))))\n")
+      (insert "  (list (apply #'append nested)\n")
+      (insert "        (cl-loop for x in nested append x)\n")
+      (insert "        (seq-uniq (apply #'append (apply #'append nested)))))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 2)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_map_assoc_sort_reverse_deep_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
