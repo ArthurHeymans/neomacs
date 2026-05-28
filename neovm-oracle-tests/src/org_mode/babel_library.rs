f72e1ecb29d955506c35fe386ee8d35836e7dfda
Eval Exec <execvy@gmail.com>
2026-05-28 02:10:45 -0400

    oracle-tests: add Babel cl-loop deep state combo
    Captures actual read-back values for cl-loop collect (1-25),
    cl-loop sum (150), and cl-loop when/append with finally
    ((2 4 6 8 10) 5). Also captures full buffer.
    
    Passing parity locks down Babel cl-loop fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,47 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_cl_loop_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; cl-loop collect
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(cl-loop for i from 1 to 5 collect (* i i))\n")
+      (insert "#+end_src\n\n")
+      ;; cl-loop with sum
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(cl-loop for x in '(10 20 30 40 50) sum x)\n")
+      (insert "#+end_src\n\n")
+      ;; cl-loop with when/append
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(cl-loop for i from 1 to 10\n         when (evenp i)\n         collect i into evens\n         finally (return (list evens (length evens))))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 3)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_multi_type_result_chain_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
