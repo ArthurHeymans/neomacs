51a86499f8664815096231ce0fcd8d25deb6a3d8
Eval Exec <execvy@gmail.com>
2026-05-28 10:34:33 -0400

    oracle-tests: add Babel mapcar table column chain deep v2 combo
    Captures actual read-back values for sorted invoices (Pc.120/
    Pa.50/Pb.40) by total descending via assoc join and sort.
    Also captures full buffer.
    
    Passing parity locks down Babel invoice-sort chain fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,63 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_mapcar_table_column_chain_deep_v2_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Products
+      (insert "#+NAME: prods\n")
+      (insert "| ID | Name | Price |\n")
+      (insert("|----+------+-------|\n")
+      (insert "| 1 | Pa | 10 |\n")
+      (insert "| 2 | Pb | 20 |\n")
+      (insert "| 3 | Pc | 15 |\n\n")
+      ;; Orders
+      (insert "#+NAME: orders\n")
+      (insert "| PID | Qty |\n")
+      (insert("|-----+-----|\n")
+      (insert "| 1 | 5 |\n")
+      (insert "| 2 | 2 |\n")
+      (insert "| 3 | 8 |\n\n")
+      ;; Compute invoices sorted by total desc
+      (insert "#+NAME: invoices\n")
+      (insert "#+begin_src emacs-lisp :var p=prods o=orders :results value replace\n")
+      (insert "(let ((bills\n")
+      (insert "       (mapcar\n")
+      (insert "        (lambda (order)\n")
+      (insert "          (let* ((pid (car order))\n")
+      (insert "                 (qty (cadr order))\n")
+      (insert "                 (prod (assoc pid p))\n")
+      (insert "                 (name (cadr prod))\n")
+      (insert "                 (price (caddr prod)))\n")
+      (insert "            (list name qty price (* qty price))))\n")
+      (insert "        o)))\n")
+      (insert "  (sort bills (lambda (a b) (> (cadddr a) (cadddr b)))))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute
+      (goto-char (point-min))
+      (search-forward "invoices")
+      (org-babel-execute-src-block)
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_table_column_pivot_compute_deep_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
