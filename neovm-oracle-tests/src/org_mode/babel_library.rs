9209785cdb086500c409a3dc97acec9511beb620
Eval Exec <execvy@gmail.com>
2026-05-28 18:33:51 -0400

    oracle-tests: add Babel header args results format edit deep
    Captures results1 (scalar=15, list=(a b c d e)) and results2
    (scalar=60 after edit, list unchanged). Tests scalar/list result
    formats with edit+re-execute.
    
    Passing parity locks down Babel header args+results format fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,60 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_header_args_results_format_edit_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Output scalar
+      (insert "#+NAME: scalar-out\n")
+      (insert "#+begin_src emacs-lisp :results value scalar replace\n")
+      (insert "(+ 1 2 3 4 5)\n")
+      (insert "#+end_src\n\n")
+      ;; Output file
+      (insert "#+NAME: list-out\n")
+      (insert "#+begin_src emacs-lisp :results value list replace\n")
+      (insert "'(a b c d e)\n")
+      (insert "#+end_src\n\n")
+      ;; Execute
+      (dolist (name '("scalar-out" "list-out"))
+        (goto-char (point-min))
+        (search-forward name)
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results1 nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results1))
+        ;; Edit scalar
+        (goto-char (point-min))
+        (search-forward "(+ 1 2 3 4 5)")
+        (replace-match "(+ 10 20 30)")
+        ;; Re-execute
+        (dolist (name '("scalar-out" "list-out"))
+          (goto-char (point-min))
+          (search-forward name)
+          (org-babel-execute-src-block))
+        (let ((results2 nil))
+          (goto-char (point-min))
+          (while (re-search-forward "#\\+RESULTS:" nil t)
+            (forward-line 1)
+            (push (org-babel-read-result) results2))
+          (list (nreverse results1)
+                (nreverse results2)
+                (buffer-substring-no-properties
+                 (point-min) (point-max)))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_variable_propagation_edit_reexecute_deep() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
