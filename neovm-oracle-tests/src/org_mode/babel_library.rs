8c97cf8c30127db5f2bb0ed9fefdb10cd7f522b2
Eval Exec <execvy@gmail.com>
2026-05-27 22:11:09 -0400

    oracle-tests: add Babel property list alist conversion deep combo
    Captures actual read-back values for plist-to-alist conversion,
    plist-get, plist-member, alist-to-plist, alist lookup, and missing
    key handling. Also captures full buffer.
    
    Passing parity locks down Babel plist/alist conversion fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,49 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_property_list_alist_conversion_deep_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Plist to alist
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((pl (list :a 1 :b 2 :c 3)))\n")
+      (insert "  (list (cl-loop for (k v) on pl by #'cddr collect (cons k v))\n")
+      (insert "        (plist-get pl :b)\n")
+      (insert "        (plist-member pl :c)))\n")
+      (insert "#+end_src\n\n")
+      ;; Alist to plist
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((al '((x . 10) (y . 20) (z . 30))))\n")
+      (insert "  (list (apply #'append al)\n")
+      (insert "        (cdr (assq 'y al))\n")
+      (insert "        (assoc 'w al)))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 2)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_number_theory_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
