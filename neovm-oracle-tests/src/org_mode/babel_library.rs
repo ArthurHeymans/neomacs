9537d2760dd3be2e4dcc4618542a1641ebdfc32c
Eval Exec <execvy@gmail.com>
2026-05-28 02:52:18 -0400

    oracle-tests: add Babel map table reduce pipeline deep combo
    Captures actual read-back values for products table (with X/Y/
    Product columns), total plist (:count/:total/:avg), raw table
    to-lisp dump, and full buffer. Also captures full buffer.
    
    Passing parity locks down Babel map-table-reduce pipeline.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,59 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_map_table_reduce_pipeline_deep_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Source data as table
+      (insert "#+NAME: raw\n")
+      (insert "| X | Y |\n|---+---|\n| 2 | 3 |\n| 4 | 5 |\n| 6 | 7 |\n\n")
+      ;; Map: compute products
+      (insert "#+NAME: products\n")
+      (insert "#+begin_src emacs-lisp :var data=raw :results value table replace\n")
+      (insert "(cons '(\"X\" \"Y\" \"Product\")\n")
+      (insert "      (cons 'hline\n")
+      (insert "            (mapcar (lambda (r) (list (car r) (cadr r) (* (car r) (cadr r)))) data)))\n")
+      (insert "#+end_src\n\n")
+      ;; Reduce: sum products
+      (insert "#+NAME: total\n")
+      (insert "#+begin_src emacs-lisp :var data=products :results value replace\n")
+      (apply #'concat
+             (list "(let ((nums (mapcar #'caddr (cdr (memq 'hline data)))))\n"
+                   "  (list :count (length nums)\n"
+                   "        :total (apply #'+ nums)\n"
+                   "        :avg (/ (apply #'+ nums) (length nums))))\n"))
+      (insert "#+end_src\n\n")
+      ;; Execute chain
+      (dolist (name '("products" "total"))
+        (goto-char (point-min))
+        (search-forward name)
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        ;; Get table lisp
+        (goto-char (point-min))
+        (search-forward "| X")
+        (let ((raw-lisp (org-table-to-lisp)))
+          (list (nreverse results)
+                raw-lisp
+                (buffer-substring-no-properties
+                 (point-min) (point-max)))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_struct_access_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
