f2e3b17cb73bd98189b1f21d73f989f1aa6455dc
Eval Exec <execvy@gmail.com>
2026-05-28 02:46:15 -0400

    oracle-tests: add Babel struct access deep state combo
    Captures actual read-back values for cl-defstruct access
    (Ada/30/elisp/t/t) and nested assoc structure (Ada/(95 87 92)/2).
    Also captures full buffer.
    
    Passing parity locks down Babel struct access fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,51 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_struct_access_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Record-like structure via cl-struct
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(progn\n  (cl-defstruct person name age lang)\n")
+      (insert "  (let ((p (make-person :name \"Ada\" :age 30 :lang \"elisp\")))\n")
+      (insert "    (list (person-name p) (person-age p) (person-lang p)\n")
+      (insert "          (person-p p) (cl-typep p 'person))))\n")
+      (insert "#+end_src\n\n")
+      ;; Nested structure
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((data (list (cons 'users (list (cons 'name \"Ada\") (cons 'age 30)))\n")
+      (insert "                 (cons 'scores (list 95 87 92))\n")
+      (insert "                 (cons 'meta (list (cons 'v 2) (cons 'ts \"now\"))))))\n")
+      (insert "  (list (cdr (assoc 'name (cdr (assoc 'users data))))\n")
+      (insert "        (cdr (assoc 'scores data))\n")
+      (insert "        (cdr (assoc 'v (cdr (assoc 'meta data))))))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 2)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_catch_throw_block_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
