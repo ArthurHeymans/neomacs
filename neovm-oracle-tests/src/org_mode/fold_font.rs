f9e5dc32ed472e17fb9213fdd3bc6875a997d581
Eval Exec <execvy@gmail.com>
2026-05-28 18:40:52 -0400

    oracle-tests: add fold hide all edit cycle font face v37
    Captures per-heading (line-number/invisible/level/face) at five
    stages: initial, after-hide-all, after-hidden-edit, after-show-all,
    after-global-cycle. Eight-heading tree with TODO/DONE/WAIT states.
    
    Passing parity locks down fold-hide-all+edit+cycle+fontify v37.

--- neovm-oracle-tests/src/org_mode/fold_font.rs
+++ neovm-oracle-tests/src/org_mode/fold_font.rs
@@ -685,6 +685,68 @@ fn org_cycle_startup_visibility_archived_drawers_combo() {
 }
 
 #[test]
+fn org_fold_hide_all_edit_cycle_font_face_v37() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'org-fold)
+  (with-temp-buffer
+    (let ((org-fontify-whole-heading-line t)
+          (org-fontify-done-headline t))
+      (org-mode)
+      (insert "* TODO Project\n")
+      (insert "** DONE Module A\n")
+      (insert "*** TODO Component X\n")
+      (insert "**** DONE Sub 1\nBody 1.\n")
+      (insert "**** TODO Sub 2\nBody 2.\n")
+      (insert "*** WAIT Component Y\nBody Y.\n")
+      (insert "** TODO Module B\n")
+      (insert "*** DONE Component Z\nBody Z.\n")
+      (font-lock-ensure (point-min) (point-max))
+      (let ((snap (lambda (tag)
+                    (mapcar
+                     (lambda (needle)
+                       (save-excursion
+                         (goto-char (point-min))
+                         (if (search-forward needle nil t)
+                             (list needle
+                                   (line-number-at-pos)
+                                   (invisible-p (point))
+                                   (org-outline-level)
+                                   (get-text-property (line-beginning-position) 'face))
+                             (list needle 'not-found nil nil nil))))
+                     '("Project" "Module A" "Component X" "Sub 1"
+                       "Sub 2" "Component Y" "Module B" "Component Z")))))
+        (let ((initial (funcall snap 'initial)))
+          ;; Hide all
+          (org-fold-hide-all)
+          (let ((after-hide-all (funcall snap 'hide-all)))
+            ;; Edit under hidden Project
+            (goto-char (point-min))
+            (search-forward "Project")
+            (end-of-line)
+            (insert "\n** DONE New Module\nNew body.\n")
+            (let ((after-edit (funcall snap 'edit)))
+              ;; Show all
+              (org-fold-show-all)
+              (font-lock-ensure (point-min) (point-max))
+              (let ((after-show (funcall snap 'show)))
+                ;; Cycle globally
+                (org-global-cycle nil)
+                (let ((after-cycle (funcall snap 'cycle)))
+                  (list initial
+                        after-hide-all
+                        after-edit
+                        after-show
+                        after-cycle
+                        (buffer-substring-no-properties
+                         (point-min) (point-max))))))))))))))"##,
+    );
+}
+
+#[test]
 fn org_fold_cycle_subtree_edit_show_all_v36() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
