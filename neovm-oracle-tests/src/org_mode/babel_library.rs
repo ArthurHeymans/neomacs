4475c23f756c9c305c48ea00ae06d9e833e79ae2
Eval Exec <execvy@gmail.com>
2026-05-27 21:21:04 -0400

    oracle-tests: add Babel setq let star dolist deep state combo
    Captures actual read-back values for setq (10 20 30), let*
    chained binding (5 10 15), and dolist accumulation (a b c d e).
    Also captures full buffer after all executions.
    
    Passing parity locks down Babel setq/let*/dolist fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,47 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_setq_let_star_dolist_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; setq
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(progn (setq a 10 b 20 c 30) (list a b c))\n")
+      (insert "#+end_src\n\n")
+      ;; let*
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let* ((x 5) (y (* x 2)) (z (+ x y)))\n  (list x y z))\n")
+      (insert "#+end_src\n\n")
+      ;; dolist accumulation
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((acc nil))\n  (dolist (item '(a b c d e))\n    (push (symbol-name item) acc))\n  (nreverse acc))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 3)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_char_alist_plist_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
