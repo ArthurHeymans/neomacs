7c0619218fe670cf5447b25163f147954c7437c6
Eval Exec <execvy@gmail.com>
2026-05-27 22:23:10 -0400

    oracle-tests: add Org fold font face after multiple todo toggle show combo
    Captures per-heading (outline-level/face at star and text) after
    4 todo toggles (DONE->TODO, TODO->DONE, WAIT->CANCELED,
    NEXT->TODO). Scans for merged heading lines.
    
    Passing parity locks down multiple todo toggle font fidelity.

--- neovm-oracle-tests/src/org_mode/fold_font.rs
+++ neovm-oracle-tests/src/org_mode/fold_font.rs
@@ -685,6 +685,75 @@ fn org_cycle_startup_visibility_archived_drawers_combo() {
 }
 
 #[test]
+fn org_fold_font_face_after_multiple_todo_toggle_show_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'org-fold)
+  (with-temp-buffer
+    (let ((org-cycle-level-faces t)
+          (org-fontify-whole-heading-line t)
+          (org-fontify-todo-headline t)
+          (org-fontify-done-headline t)
+          (org-log-done nil))
+      (org-mode)
+      (insert "* TODO A\n")
+      (insert "** DONE B\n")
+      (insert "*** TODO C\n")
+      (insert "**** WAIT D\n")
+      (insert "** NEXT E\n")
+      (font-lock-ensure (point-min) (point-max))
+      ;; Toggle B: DONE->TODO
+      (goto-char (point-min))
+      (search-forward "B")
+      (beginning-of-line)
+      (org-todo "TODO")
+      ;; Toggle C: TODO->DONE
+      (goto-char (point-min))
+      (search-forward "C")
+      (beginning-of-line)
+      (org-todo "DONE")
+      ;; Toggle D: WAIT->CANCELED
+      (goto-char (point-min))
+      (search-forward "D")
+      (beginning-of-line)
+      (org-todo "CANCELED")
+      ;; Toggle E: NEXT->TODO
+      (goto-char (point-min))
+      (search-forward "E")
+      (beginning-of-line)
+      (org-todo "TODO")
+      (font-lock-ensure (point-min) (point-max))
+      ;; Check state
+      (let ((headings
+             (mapcar
+              (lambda (needle)
+                (save-excursion
+                  (goto-char (point-min))
+                  (if (search-forward needle nil t)
+                      (list needle
+                            (org-outline-level)
+                            (get-text-property (line-beginning-position) 'face)
+                            (get-text-property (point) 'face))
+                      (list needle 'not-found nil nil))))
+              '("A" "B" "C" "D" "E")))
+            (merged nil))
+        (dolist (line (split-string
+                       (buffer-substring-no-properties
+                        (point-min) (point-max))
+                       "\n" t))
+          (when (string-match-p "^\\*+ .*\\*+ " line)
+            (push line merged)))
+        (list headings
+              (nreverse merged)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_fold_font_face_after_todo_toggle_cycle_show_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
