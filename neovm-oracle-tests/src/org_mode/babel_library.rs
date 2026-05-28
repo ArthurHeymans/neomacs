afc50e91caf490b5a0c674a48c8e2f43bd3c7a0f
Eval Exec <execvy@gmail.com>
2026-05-28 13:21:31 -0400

    oracle-tests: add Babel table filter sort aggregate chain deep combo
    Captures actual read-back values for filtered+sorted rows
    (3.8/5.6/2.3) and total (22) via cl-remove-if-not and sort.
    Also captures full buffer.
    
    Passing parity locks down Babel filter-sort-aggregate chain.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,52 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_table_filter_sort_aggregate_chain_deep_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Input table
+      (insert "#+NAME: input\n")
+      (insert "| N | V |\n")
+      (insert("|---+---|\n")
+      (insert "| 1 | 5 |\n")
+      (insert "| 2 | 3 |\n")
+      (insert "| 3 | 8 |\n")
+      (insert "| 4 | 1 |\n")
+      (insert "| 5 | 6 |\n\n")
+      ;; Filter > 2, sort desc, sum
+      (insert "#+NAME: result\n")
+      (insert "#+begin_src emacs-lisp :var tbl=input :results value replace\n")
+      (insert "(let* ((big (cl-remove-if-not (lambda (r) (> (cadr r) 2)) tbl))\n")
+      (insert "       (sorted (sort (copy-sequence big)\n")
+      (insert "                     (lambda (a b) (> (cadr a) (cadr b)))))\n")
+      (insert "       (total (apply #'+ (mapcar #'cadr sorted))))\n")
+      (insert "  (list :filtered sorted :total total))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute
+      (goto-char (point-min))
+      (search-forward "result")
+      (org-babel-execute-src-block)
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_table_column_filter_chain_deep_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
