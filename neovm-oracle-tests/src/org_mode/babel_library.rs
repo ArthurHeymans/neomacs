6b34d669391b50b782d40de261b9836d4caa63a6
Eval Exec <execvy@gmail.com>
2026-05-28 05:32:43 -0400

    oracle-tests: add Babel vector operations deep state combo
    Captures actual read-back values for vector create/access/length,
    seq-into, vconcat, and mapcar on vector. Also captures full buffer.
    
    Passing parity locks down Babel vector operation fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,49 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_vector_operations_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Vector create and access
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((v [10 20 30 40 50]))\n")
+      (insert "  (list (aref v 0) (aref v 4) (length v)\n")
+      (insert "        (seq-into '(1 2 3) 'vector)\n")
+      (insert "        (append v nil)))\n")
+      (insert "#+end_src\n\n")
+      ;; Vector operations
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((v1 [1 2 3]) (v2 [4 5 6]))\n")
+      (insert "  (list (vconcat v1 v2)\n")
+      (insert "        (mapcar (lambda (x) (* x 2)) (append v1 nil))\n")
+      (insert "        (seq-sort #'< (vconcat v2 v1))))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 2)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_type_check_coerce_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
