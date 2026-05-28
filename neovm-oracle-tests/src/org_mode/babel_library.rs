255cce8611d34cc5c282b165f4f84ae45810d6b6
Eval Exec <execvy@gmail.com>
2026-05-28 16:21:32 -0400

    oracle-tests: add Babel five block chain deep combo
    Captures actual read-back values for 5-block chain: seed (1-10),
    evens (2/4/6/8/10), squared (4/16/36/64/100), sorted (100/64/
    36/16/4), and total (220). Also captures full buffer.
    
    Passing parity locks down Babel five-block chain fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,60 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_five_block_chain_deep_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Seed
+      (insert "#+NAME: seed\n")
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "'(1 2 3 4 5 6 7 8 9 10)\n")
+      (insert "#+end_src\n\n")
+      ;; Even
+      (insert "#+NAME: evens\n")
+      (insert "#+begin_src emacs-lisp :var data=seed :results value replace\n")
+      (insert "(seq-filter #'evenp data)\n")
+      (insert "#+end_src\n\n")
+      ;; Squared
+      (insert "#+NAME: squared\n")
+      (insert "#+begin_src emacs-lisp :var data=evens :results value replace\n")
+      (insert "(mapcar (lambda (x) (* x x)) data)\n")
+      (insert "#+end_src\n\n")
+      ;; Sorted
+      (insert "#+NAME: sorted\n")
+      (insert "#+begin_src emacs-lisp :var data=squared :results value replace\n")
+      (insert "(sort (copy-sequence data) #'>)\n")
+      (insert "#+end_src\n\n")
+      ;; Total
+      (insert "#+NAME: total\n")
+      (insert "#+begin_src emacs-lisp :var data=sorted :results value replace\n")
+      (insert "(apply #'+ data)\n")
+      (insert "#+end_src\n\n")
+      ;; Execute chain
+      (dolist (name '("evens" "squared" "sorted" "total"))
+        (goto-char (point-min))
+        (search-forward name)
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_four_block_chain_deep_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
