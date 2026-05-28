265b5780497c726c93bff06516556091e472fe1e
Eval Exec <execvy@gmail.com>
2026-05-28 05:37:02 -0400

    oracle-tests: add Babel IO process deep state combo
    Captures actual read-back values for buffer operations
    (insert/count-lines/search-forward/line-number-at-pos) and
    file operations (file-exists-p/insert-file-contents). Also
    captures full buffer.
    
    Passing parity locks down Babel IO operation fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,56 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_io_process_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Buffer operations
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(with-temp-buffer\n")
+      (insert "  (insert \"hello\\nworld\\nfoo\\n\")\n")
+      (insert "  (list (buffer-string)\n")
+      (insert "        (count-lines (point-min) (point-max))\n")
+      (insert "        (progn (goto-char (point-min))\n")
+      (insert "               (search-forward \"world\")\n")
+      (insert "               (line-number-at-pos))))\n")
+      (insert "#+end_src\n\n")
+      ;; File operations
+      (let* ((root (make-temp-file "org-babel-io" t))
+             (tmp (expand-file-name "test.txt" root)))
+        (insert "#+begin_src emacs-lisp :results value replace\n")
+        (insert "(let ((f \"" tmp "\"))\n")
+        (insert "  (with-temp-file f (insert \"line1\\nline2\\n\"))\n")
+        (insert "  (list (file-exists-p f)\n")
+        (insert "        (with-temp-buffer\n")
+        (insert "          (insert-file-contents f)\n")
+        (insert "          (buffer-string))))\n")
+        (insert "#+end_src\n\n"))
+      ;; Execute all
+      (dotimes (_ 2)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_vector_operations_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
