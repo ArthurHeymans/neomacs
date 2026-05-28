0e450af5135271994f1aba55cbc495b6738d4ed0
Eval Exec <execvy@gmail.com>
2026-05-27 23:26:27 -0400

    oracle-tests: add Babel condition case unwind protect deep combo
    Captures actual read-back values for condition-case success (3),
    condition-case error catch (t/arithmetic-error), and
    unwind-protect (42/t). Also captures full buffer.
    
    Passing parity locks down Babel error handling fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,51 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_condition_case_unwind_protect_deep_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; condition-case success
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(condition-case err\n    (+ 1 2)\n  (error (list :err (cdr err))))\n")
+      (insert "#+end_src\n\n")
+      ;; condition-case error
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(condition-case err\n    (/ 1 0)\n  (error (list :caught t :msg (error-message-string err))))\n")
+      (insert "#+end_src\n\n")
+      ;; unwind-protect
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((result nil) (cleanup nil))\n")
+      (insert "  (unwind-protect\n")
+      (insert "      (progn (setq result 42) result)\n")
+      (insert "    (setq cleanup t))\n")
+      (insert "  (list result cleanup))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 3)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_multi_block_dependency_chain_deep_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
