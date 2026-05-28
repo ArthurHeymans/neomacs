eb743e80240f24c431416e3e84c83845006f2f11
Eval Exec <execvy@gmail.com>
2026-05-28 07:39:25 -0400

    oracle-tests: add Babel pcase cond cl-case deep state combo
    Captures actual read-back values for pcase match (b/6/(key val)),
    pcase backquote pattern, and cond select (medium). Also captures
    full buffer.
    
    Passing parity locks down Babel pcase/cond fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,51 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_pcase_cond_cl_case_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; pcase
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(list (pcase 2 (1 'a) (2 'b) (3 'c))\n")
+      (insert "      (pcase '(1 2 3)\n")
+      (insert "        (`(,x ,y ,z) (+ x y z))\n")
+      (insert "        (_ 0))\n")
+      (insert "      (pcase '(key . val)\n")
+      (insert "        (`(,k . ,v) (list k v))))\n")
+      (insert "#+end_src\n\n")
+      ;; cond
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((x 5))\n")
+      (insert "  (cond ((< x 3) 'small)\n")
+      (insert "        ((< x 7) 'medium)\n")
+      (insert "        (t 'large)))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 2)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_multi_table_join_stats_deep_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
