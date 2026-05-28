2fe263ae0725cfea361c9f70eead8caf98b7078b
Eval Exec <execvy@gmail.com>
2026-05-28 19:41:40 -0400

    oracle-tests: add Babel list results chain edit reexecute deep
    Captures results1 (names + scored) and results2 (X/Y/Z + scored).
    Tests list result format with chain edit (5 names -> 3 names)
    and re-execute fidelity.
    
    Passing parity locks down Babel list results+chain+edit fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,60 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_list_results_chain_edit_reexecute_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Source: generate names
+      (insert "#+NAME: names\n")
+      (insert "#+begin_src emacs-lisp :results value list replace\n")
+      (insert "'(\"Alice\" \"Bob\" \"Carol\" \"Dave\" \"Eve\")\n")
+      (insert "#+end_src\n\n")
+      ;; Transform: add score
+      (insert "#+NAME: scored\n")
+      (insert "#+begin_src emacs-lisp :var names=names :results value list replace\n")
+      (insert "(mapcar (lambda (n) (list n (random 100))) names)\n")
+      (insert "#+end_src\n\n")
+      ;; Execute chain
+      (dolist (name '("names" "scored"))
+        (goto-char (point-min))
+        (search-forward name)
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results1 nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results1))
+        ;; Edit: change names
+        (goto-char (point-min))
+        (search-forward "\"Alice\" \"Bob\" \"Carol\" \"Dave\" \"Eve\"")
+        (replace-match "\"X\" \"Y\" \"Z\"")
+        ;; Re-execute
+        (dolist (name '("names" "scored"))
+          (goto-char (point-min))
+          (search-forward name)
+          (org-babel-execute-src-block))
+        (let ((results2 nil))
+          (goto-char (point-min))
+          (while (re-search-forward "#\\+RESULTS:" nil t)
+            (forward-line 1)
+            (push (org-babel-read-result) results2))
+          (list (nreverse results1)
+                (nreverse results2)
+                (buffer-substring-no-properties
+                 (point-min) (point-max)))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_table_results_edit_reexecute_deep() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
