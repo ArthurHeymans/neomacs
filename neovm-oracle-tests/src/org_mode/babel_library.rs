5df26dca34202965356d598450630182fd312ff7
Eval Exec <execvy@gmail.com>
2026-05-28 06:48:35 -0400

    oracle-tests: add Babel table column stats deep state combo
    Captures actual read-back values for per-column min/avg/max
    statistics and table-to-lisp dump. Also captures full buffer.
    
    Passing parity locks down Babel table column stats fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,56 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_table_column_stats_deep_state_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Data table
+      (insert "#+NAME: data\n")
+      (insert "| X | Y | Z |\n")
+      (insert "|---+---+---|\n")
+      (insert "| 2 | 5 | 8 |\n")
+      (insert "| 3 | 7 | 1 |\n")
+      (insert "| 4 | 9 | 6 |\n\n")
+      ;; Column stats
+      (insert "#+NAME: stats\n")
+      (insert "#+begin_src emacs-lisp :var tbl=data :results value replace\n")
+      (insert "(let ((xs (mapcar #'car tbl))\n")
+      (insert "      (ys (mapcar #'cadr tbl))\n")
+      (insert "      (zs (mapcar #'caddr tbl)))\n")
+      (insert "  (list :x (list (apply #'min xs) (/ (apply #'+ xs) (length xs)) (apply #'max xs))\n")
+      (insert "        :y (list (apply #'min ys) (/ (apply #'+ ys) (length ys)) (apply #'max ys))\n")
+      (insert "        :z (list (apply #'min zs) (/ (apply #'+ zs) (length zs)) (apply #'max zs))))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute
+      (goto-char (point-min))
+      (search-forward "stats")
+      (org-babel-execute-src-block)
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        ;; Table lisp
+        (goto-char (point-min))
+        (search-forward "| X")
+        (let ((table-lisp (org-table-to-lisp)))
+          (list (nreverse results)
+                table-lisp
+                (buffer-substring-no-properties
+                 (point-min) (point-max)))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_assoc_list_transform_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
