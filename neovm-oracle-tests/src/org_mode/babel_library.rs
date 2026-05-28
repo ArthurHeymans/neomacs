0c91cbb9c0aab5f0f932f8062ea268206a3e6a39
Eval Exec <execvy@gmail.com>
2026-05-28 07:17:37 -0400

    oracle-tests: add Babel mapcar filter chain table deep combo
    Captures actual read-back values for filtered+computed table
    (X/Y/X+Y/X*Y) via cl-remove-if-not even filter and mapcar.
    Also captures full buffer.
    
    Passing parity locks down Babel filter+map table fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,56 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_mapcar_filter_chain_table_deep_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; Data
+      (insert "#+NAME: raw\n")
+      (insert "| X | Y |\n")
+      (insert "|---+---|\n")
+      (insert "| 2 | 7 |\n")
+      (insert "| 5 | 3 |\n")
+      (insert "| 8 | 1 |\n")
+      (insert "| 4 | 9 |\n\n")
+      ;; Filter even X, compute
+      (insert "#+NAME: computed\n")
+      (insert "#+begin_src emacs-lisp :var tbl=raw :results value table replace\n")
+      (insert "(let ((filtered (cl-remove-if-not\n")
+      (insert "                 (lambda (row) (evenp (car row)))\n")
+      (insert "                 tbl)))\n")
+      (insert "  (cons '(\"X\" \"Y\" \"X+Y\" \"X*Y\")\n")
+      (insert "        (cons 'hline\n")
+      (insert "              (mapcar (lambda (r)\n")
+      (insert "                        (list (car r) (cadr r)\n")
+      (insert "                              (+ (car r) (cadr r))\n")
+      (insert "                              (* (car r) (cadr r))))\n")
+      (insert "                      filtered))))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute
+      (goto-char (point-min))
+      (search-forward "computed")
+      (org-babel-execute-src-block)
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_letrec_named_let_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
