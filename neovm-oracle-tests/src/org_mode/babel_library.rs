f89c102eff566ba9eb2058cc4edfb9f809e22813
Eval Exec <execvy@gmail.com>
2026-05-28 03:44:38 -0400

    oracle-tests: add Babel string replace concat length deep combo
    Captures actual read-back values for replace-regexp-in-string
    (Hello Elisp Foo Bar / XXXX XXXX XXX XXX), concat (a-b-c),
    and format (00042/3.142/left      |). Also captures full buffer.
    
    Passing parity locks down Babel string replace/concat/format fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,49 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_string_replace_concat_length_deep_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; String replace
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((s \"Hello World Foo Bar\"))\n")
+      (insert "  (list (replace-regexp-in-string \"World\" \"Elisp\" s)\n")
+      (insert "        (replace-regexp-in-string \"[A-Z]\" \"X\" s)\n")
+      (insert "        (length s)))\n")
+      (insert "#+end_src\n\n")
+      ;; String concat and format
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(list (concat \"a\" \"-\" \"b\" \"-\" \"c\")\n")
+      (insert "      (format \"%05d\" 42)\n")
+      (insert "      (format \"%.3f\" 3.14159)\n")
+      (insert "      (format \"%-10s|\" \"left\"))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 2)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_multi_var_header_chain_deep_state_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
