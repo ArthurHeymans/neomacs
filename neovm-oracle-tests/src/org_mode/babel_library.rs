8af1b6a5c7226b5fa355d86000ffd0d521535f6a
Eval Exec <execvy@gmail.com>
2026-05-28 00:07:25 -0400

    oracle-tests: add Babel list append reverse sort deep combo
    Captures actual read-back values for append/reverse, sort with
    predicates (int/string/alist), and nested list operations
    (nth/last/butlast/subseq). Also captures full buffer.
    
    Passing parity locks down Babel list manipulation fidelity.

--- neovm-oracle-tests/src/org_mode/babel_library.rs
+++ neovm-oracle-tests/src/org_mode/babel_library.rs
@@ -1080,6 +1080,61 @@ fn org_babel_execute_multiple_blocks_result_chain_deep_state_combo() {
 }
 
 #[test]
+fn org_babel_execute_list_append_reverse_sort_deep_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'ob-core)
+  (require 'ob-emacs-lisp)
+  (with-temp-buffer
+    (org-mode)
+    (let ((org-confirm-babel-evaluate nil))
+      ;; List append and reverse
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let ((l (list 1 2 3)))\n")
+      (insert "  (list (append l '(4 5))\n")
+      (insert "        (reverse l)\n")
+      (insert "        (append nil l)\n")
+      (insert "        (append l nil)))\n")
+      (insert "#+end_src\n\n")
+      ;; Sort with predicate
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(list (sort '(3 1 4 1 5 9 2 6) #'<)\n")
+      (insert "      (sort '(\"banana\" \"apple\" \"cherry\") #'string<)\n")
+      (insert "      (sort '((b . 2) (a . 1) (c . 3))\n")
+      (insert "            (lambda (x y) (< (cdr x) (cdr y))))))\n")
+      (insert "#+end_src\n\n")
+      ;; Nested list operations
+      (insert "#+begin_src emacs-lisp :results value replace\n")
+      (insert "(let* ((a '(1 2 3))\n")
+      (insert "       (b '(4 5 6))\n")
+      (insert "       (c (append a b)))\n")
+      (insert "  (list (length c)\n")
+      (insert "        (nth 3 c)\n")
+      (insert "        (last c 2)\n")
+      (insert "        (butlast c 2)\n")
+      (insert "        (subseq c 1 4)))\n")
+      (insert "#+end_src\n\n")
+      ;; Execute all
+      (dotimes (_ 3)
+        (goto-char (point-min))
+        (search-forward "begin_src")
+        (org-babel-execute-src-block))
+      ;; Read results
+      (let ((results nil))
+        (goto-char (point-min))
+        (while (re-search-forward "#\\+RESULTS:" nil t)
+          (forward-line 1)
+          (push (org-babel-read-result) results))
+        (list (nreverse results)
+              (buffer-substring-no-properties
+               (point-min) (point-max))))))))"##,
+    );
+}
+
+#[test]
 fn org_babel_execute_condition_case_unwind_protect_deep_combo() {
     return_if_neovm_enable_oracle_proptest_not_set!();
 
