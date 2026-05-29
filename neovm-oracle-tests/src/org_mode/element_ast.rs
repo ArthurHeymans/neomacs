c7a6b8a4126bc16306077246058148e555c61af7
Eval Exec <execvy@gmail.com>
2026-05-29 06:33:45 -0400

    oracle-tests: add element parse eight heading tag prop edit reparse v6
    Captures parsed headlines (raw-value/level/todo/tags) and property
    drawer node-properties before/after tag toggle + property set +
    property delete. Eight headings with company/eng/product/design/
    marketing/ops/sales/goal tags and multiple PROPERTIES.
    
    Passing parity locks down element parse+eight-heading+tag+prop v6.

--- neovm-oracle-tests/src/org_mode/element_ast.rs
+++ neovm-oracle-tests/src/org_mode/element_ast.rs
@@ -1212,3 +1212,75 @@ fn org_element_parse_seven_heading_tag_prop_edit_reparse_v5() {
                  (point-min) (point-max)))))))))"##,
     );
 }
+
+#[test]
+fn org_element_parse_eight_heading_tag_prop_edit_reparse_v6() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org-element)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* TODO Company :company:\n")
+    (insert ":PROPERTIES:\n:Owner: CEO\n:CATEGORY: org\n:END:\n")
+    (insert "Body Company.\n\n")
+    (insert "** DONE Eng :eng:\n")
+    (insert ":PROPERTIES:\n:Effort: 40h\n:Budget: 100k\n:END:\n")
+    (insert "Body Eng.\n\n")
+    (insert "** TODO Product :product:\n")
+    (insert ":PROPERTIES:\n:Effort: 20h\n:Budget: 50k\n:END:\n")
+    (insert "Body Product.\n\n")
+    (insert "** WAIT Design :design:\n")
+    (insert ":PROPERTIES:\n:Effort: 15h\n:Budget: 30k\n:Owner: ArtDir\n:END:\n")
+    (insert "Body Design.\n\n")
+    (insert "** NEXT Marketing :marketing:\n")
+    (insert ":PROPERTIES:\n:Effort: 25h\n:Budget: 80k\n:Owner: CMO\n:END:\n")
+    (insert "Body Marketing.\n\n")
+    (insert "** CANCEL Ops :ops:\n")
+    (insert ":PROPERTIES:\n:Effort: 10h\n:Budget: 20k\n:END:\n")
+    (insert "Body Ops.\n\n")
+    (insert "** TODO Sales :sales:\n")
+    (insert ":PROPERTIES:\n:Effort: 30h\n:Budget: 60k\n:Owner: VP-Sales\n:END:\n")
+    (insert "Body Sales.\n\n")
+    (insert "* DONE Q1-Goal :goal:\n")
+    (insert ":PROPERTIES:\n:Owner: CEO\n:CATEGORY: planning\n:END:\n")
+    (insert "Body Q1.\n\n")
+    (let* ((snap (lambda ()
+                   (let ((tree (org-element-parse-buffer)))
+                     (list
+                      (org-element-map tree 'headline
+                        (lambda (hl)
+                          (list (org-element-property :raw-value hl)
+                                (org-element-property :level hl)
+                                (org-element-property :todo-keyword hl)
+                                (org-element-property :tags hl))))
+                      (org-element-map tree 'property-drawer
+                        (lambda (pd)
+                          (org-element-map pd 'node-property
+                            (lambda (np)
+                              (list (org-element-property :key np)
+                                    (org-element-property :value np)))))))))))
+      (let ((initial (funcall snap)))
+        ;; Toggle tag on Company
+        (goto-char (point-min))
+        (search-forward "Body Company.")
+        (beginning-of-line)
+        (org-up-heading-safe)
+        (org-toggle-tag "review" 'on)
+        ;; Set property on Marketing
+        (goto-char (point-min))
+        (search-forward "NEXT Marketing")
+        (beginning-of-line)
+        (org-set-property "Status" "active")
+        ;; Delete Owner from Design
+        (goto-char (point-min))
+        (search-forward "WAIT Design")
+        (beginning-of-line)
+        (org-delete-property "Owner")
+        (let ((after-edit (funcall snap)))
+          (list initial after-edit
+                (buffer-substring-no-properties
+                 (point-min) (point-max)))))))))"##,
+    );
+}
