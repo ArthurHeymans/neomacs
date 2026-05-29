80f18fda121f8ac567ca70ae595920987367ac04
Eval Exec <execvy@gmail.com>
2026-05-29 06:01:37 -0400

    oracle-tests: add element parse six heading tag prop edit reparse v4
    Captures parsed headlines (raw-value/level/todo/tags) and property
    drawer node-properties before/after tag toggle + property set +
    property delete. Six headings with root/fe/be/ops/qa/docs tags
    and multiple PROPERTIES.
    
    Passing parity locks down element parse+six-heading+tag+prop v4.

--- neovm-oracle-tests/src/org_mode/element_ast.rs
+++ neovm-oracle-tests/src/org_mode/element_ast.rs
@@ -1077,3 +1077,69 @@ fn org_element_parse_five_heading_tag_prop_edit_reparse_v3() {
                  (point-min) (point-max)))))))))"##,
     );
 }
+
+#[test]
+fn org_element_parse_six_heading_tag_prop_edit_reparse_v4() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org-element)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* TODO Root :root:\n")
+    (insert ":PROPERTIES:\n:Owner: Alice\n:CATEGORY: main\n:END:\n")
+    (insert "Body Root.\n\n")
+    (insert "** DONE S1 :fe:\n")
+    (insert ":PROPERTIES:\n:Effort: 3h\n:Tier: P0\n:END:\n")
+    (insert "Body S1.\n\n")
+    (insert "** TODO S2 :be:\n")
+    (insert ":PROPERTIES:\n:Effort: 5h\n:Tier: P1\n:END:\n")
+    (insert "Body S2.\n\n")
+    (insert "** WAIT S3 :ops:\n")
+    (insert ":PROPERTIES:\n:Effort: 2h\n:Tier: P2\n:Owner: Bob\n:END:\n")
+    (insert "Body S3.\n\n")
+    (insert "** NEXT S4 :qa:\n")
+    (insert ":PROPERTIES:\n:Effort: 4h\n:Tier: P1\n:Owner: Carol\n:END:\n")
+    (insert "Body S4.\n\n")
+    (insert "** CANCEL S5 :docs:\n")
+    (insert ":PROPERTIES:\n:Effort: 1h\n:Tier: P3\n:END:\n")
+    (insert "Body S5.\n\n")
+    (let* ((snap (lambda ()
+                   (let ((tree (org-element-parse-buffer)))
+                     (list
+                      (org-element-map tree 'headline
+                        (lambda (hl)
+                          (list (org-element-property :raw-value hl)
+                                (org-element-property :level hl)
+                                (org-element-property :todo-keyword hl)
+                                (org-element-property :tags hl))))
+                      (org-element-map tree 'property-drawer
+                        (lambda (pd)
+                          (org-element-map pd 'node-property
+                            (lambda (np)
+                              (list (org-element-property :key np)
+                                    (org-element-property :value np)))))))))))
+      (let ((initial (funcall snap)))
+        ;; Toggle tag on Root
+        (goto-char (point-min))
+        (search-forward "Body Root.")
+        (beginning-of-line)
+        (org-up-heading-safe)
+        (org-toggle-tag "review" 'on)
+        ;; Set property on S4
+        (goto-char (point-min))
+        (search-forward "NEXT S4")
+        (beginning-of-line)
+        (org-set-property "Status" "active")
+        ;; Delete Owner from S3
+        (goto-char (point-min))
+        (search-forward "WAIT S3")
+        (beginning-of-line)
+        (org-delete-property "Owner")
+        (let ((after-edit (funcall snap)))
+          (list initial after-edit
+                (buffer-substring-no-properties
+                 (point-min) (point-max)))))))))"##,
+    );
+}
