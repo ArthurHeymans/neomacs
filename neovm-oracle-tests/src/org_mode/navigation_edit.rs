9f0a3ac727163daab0908ad7fb623667d0a84b6e
Eval Exec <execvy@gmail.com>
2026-05-29 06:14:57 -0400

    oracle-tests: add navigate twelve heading tree fold edit deep
    Captures per-step (line-number/text/level/invisible) for twelve-heading
    tree navigation: P1A1->P1A2->P1A->P1B->P1B1->P2C1, edit P1B2,
    fold P1. Tests complex twelve-heading tree navigation+edit+fold fidelity.
    
    Passing parity locks down navigate+twelve-heading+tree+fold+edit fidelity.

--- neovm-oracle-tests/src/org_mode/navigation_edit.rs
+++ neovm-oracle-tests/src/org_mode/navigation_edit.rs
@@ -858,3 +858,72 @@ fn org_navigate_ten_heading_tree_fold_edit_deep() {
                            (point-min) (point-max))))))))))))))"##,
     );
 }
+
+#[test]
+fn org_navigate_twelve_heading_tree_fold_edit_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'org)
+  (require 'org-fold)
+  (with-temp-buffer
+    (org-mode)
+    (insert "* P1\n")
+    (insert "** P1A\n")
+    (insert "*** P1A1\nBody.\n\n")
+    (insert "*** P1A2\nBody.\n\n")
+    (insert "** P1B\n")
+    (insert "*** P1B1\nBody.\n\n")
+    (insert "* P2\n")
+    (insert "** P2A\n")
+    (insert "*** P2A1\nBody.\n\n")
+    (insert "** P2B\n")
+    (insert "*** P2B1\nBody.\n\n")
+    (insert "** P2C\n")
+    (insert "*** P2C1\nBody.\n\n")
+    (let ((track (lambda ()
+                   (list (line-number-at-pos)
+                         (buffer-substring-no-properties
+                          (line-beginning-position) (line-end-position))
+                         (org-outline-level)
+                         (invisible-p (point))))))
+      ;; At P1A1
+      (goto-char (point-min))
+      (search-forward "P1A1")
+      (beginning-of-line)
+      (let ((at-p1a1 (funcall track)))
+        ;; Forward to P1A2
+        (org-forward-heading-same-level 1)
+        (let ((at-p1a2 (funcall track)))
+          ;; Up to P1A
+          (org-up-heading-safe)
+          (let ((at-p1a (funcall track)))
+            ;; Forward to P1B
+            (org-forward-heading-same-level 1)
+            (let ((at-p1b (funcall track)))
+              ;; Down to P1B1
+              (org-next-visible-heading 1)
+              (let ((at-p1b1 (funcall track)))
+                ;; Jump to P2C1
+                (goto-char (point-min))
+                (search-forward "P2C1")
+                (beginning-of-line)
+                (let ((at-p2c1 (funcall track)))
+                  ;; Edit: insert P1B2 under P1B
+                  (goto-char (point-min))
+                  (search-forward "P1B1")
+                  (end-of-line)
+                  (insert "\n*** P1B2\nBody.\n")
+                  ;; Fold P1 subtree
+                  (goto-char (point-min))
+                  (search-forward "P1")
+                  (beginning-of-line)
+                  (org-fold-subtree)
+                  (let ((after-fold (funcall track)))
+                    (list at-p1a1 at-p1a2 at-p1a at-p1b at-p1b1 at-p2c1
+                          after-fold
+                          (buffer-substring-no-properties
+                           (point-min) (point-max))))))))))))))"##,
+    );
+}
