71cae5c873d22ad4e5afb33e4eaa8815a1d941fd
Eval Exec <execvy@gmail.com>
2026-05-30 19:52:44 -0400

    oracle-tests: face_theme add 5 vault tests
    Tests: overlay face overlays-at, fontify easy-menu-define,
    overlay next/prev overlay change, text property face at
    overlay boundary, fontify with char classes.
    All pass. 1196 total, 37 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -27505,3 +27505,93 @@ fn ft_slam_face_text_property_char_property_at_edge() {
      'intervals (length (object-intervals (current-buffer)))))))"##,
     );
 }
+
+#[test]
+fn ft_vault_face_overlay_face_overlay_at_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6))) (overlay-put ov 'face 'bold))
+    (let ((ovs-at (overlays-at 3)))
+      (list
+       'num-overlays-at (length ovs-at)
+       'face-of-first (if ovs-at (overlay-get (car ovs-at) 'face) nil)
+       (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_vault_font_lock_fontify_easy_menu_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(easy-menu-define my-menu nil \"My Menu\"\n  '(\"Tools\" [\"Do Something\" my-cmd]))\n")
+    (font-lock-fontify-buffer)
+    (list
+     'easy-menu-face (save-excursion (goto-char (point-min)) (search-forward "easy-menu-define") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_vault_face_overlay_next_overlay_change_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAABBBBB")
+    (let ((ov1 (make-overlay 1 6))) (overlay-put ov1 'face 'bold))
+    (let ((ov2 (make-overlay 6 11))) (overlay-put ov2 'face 'italic))
+    (list
+     'next-at-pos3 (next-overlay-change 3)
+     'next-at-pos8 (next-overlay-change 8)
+     'prev-at-pos8 (previous-overlay-change 8)
+     'prev-at-pos3 (previous-overlay-change 3)
+     (progn (mapc #'delete-overlay (overlays-in 1 11)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_vault_face_text_property_face_at_overlay_boundary() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (put-text-property 1 6 'face 'bold)
+    (let ((ov (make-overlay 3 5)))
+      (overlay-put ov 'face 'italic)
+      (list
+       'text-pos1 (get-text-property 1 'face)
+       'txt-pos3-underlay (get-text-property 3 'face)
+       'char-pos3 (get-char-property 3 'face)
+       'txt-pos5-underlay (get-text-property 5 'face)
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_vault_font_lock_fontify_with_char_classes() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(setq var (string-match \"[[:alpha:]]+\" \"hello\"))\n")
+    (font-lock-fontify-buffer)
+    (list
+     'string-face (save-excursion (goto-char (point-min)) (search-forward "\"hello\"") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
