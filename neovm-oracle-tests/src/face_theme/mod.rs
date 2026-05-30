f9a062aece0bb1b1168c859411cab5be3683bf38
Eval Exec <execvy@gmail.com>
2026-05-30 18:47:29 -0400

    oracle-tests: face_theme add 7 crack tests
    Tests: overlay face get after deleted, fontify with jit-lock timer,
    overlay invisible and intangible, text property delete/insert
    stress, fontify interactive form, overlay face put then check all
    overlays, face attributes merge.
    All pass. 1093 total, 36 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -25573,3 +25573,121 @@ fn ft_pry_face_set_face_all_attributes_batch_deep() {
    )))"##,
     );
 }
+
+#[test]
+fn ft_crack_face_overlay_face_get_after_overlay_deleted() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (overlay-put ov 'face 'bold)
+      (delete-overlay ov)
+      (condition-case err (overlay-get ov 'face) (error (list 'err (car err))))))))"##,
+    );
+}
+
+#[test]
+fn ft_crack_font_lock_fontify_with_timer_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (list
+   'timer-fbound (fboundp 'run-with-idle-timer)
+   'jit-stealth-fbound (fboundp 'jit-lock-stealth-fontify)
+   'jit-context-fbound (fboundp 'jit-lock-context-fontify)
+   'jit-register-fbound (fboundp 'jit-lock-register)
+   'jit-mode-fbound (fboundp 'jit-lock-mode))))"##,
+    );
+}
+
+#[test]
+fn ft_crack_face_overlay_face_invisible_and_intangible() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (overlay-put ov 'face 'bold)
+      (overlay-put ov 'invisible t)
+      (overlay-put ov 'intangible t)
+      (list
+       'face (overlay-get ov 'face)
+       'invisible (overlay-get ov 'invisible)
+       'intangible (overlay-get ov 'intangible)
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_crack_face_text_property_delete_insert_stress() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAABBBBB")
+    (put-text-property 1 6 'face 'bold)
+    (put-text-property 6 11 'face 'italic)
+    (list
+     'before (list (get-text-property 3 'face) (get-text-property 8 'face))
+     'after-delete (progn (delete-region 2 9) (list 'buffer (buffer-string) 'face-at-1 (get-text-property 1 'face) 'face-at-last (get-text-property (1- (point-max)) 'face) 'intervals (length (object-intervals (current-buffer)))))))))"##,
+    );
+}
+
+#[test]
+fn ft_crack_font_lock_fontify_interactive_form() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(defun my-cmd (arg)\n  (interactive \"p\")\n  (message \"Arg: %d\" arg))\n")
+    (font-lock-fontify-buffer)
+    (list
+     'interactive-face (save-excursion (goto-char (point-min)) (search-forward "interactive") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_crack_face_overlay_face_put_then_check_all_overlays() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (overlay-put ov 'face 'bold)
+      (let ((all-ovs (overlays-in 1 6)))
+        (list
+         'num-ovls (length all-ovs)
+         'first-ovl-face (overlay-get (car all-ovs) 'face)
+         (progn (delete-overlay ov) 'cleaned)))))))"##,
+    );
+}
+
+#[test]
+fn ft_crack_face_face_attributes_merge_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (condition-case nil (copy-face 'default 'my-merge-face) (error nil))
+  (list
+   'default-fg (condition-case nil (face-attribute 'default :foreground nil 'default-on) (error 'no))
+   'default-bg (condition-case nil (face-attribute 'default :background nil 'default-on) (error 'no))
+   'merge-fg (condition-case nil (progn (set-face-attribute 'my-merge-face nil :foreground "red") (face-attribute 'my-merge-face :foreground nil 'default-on)) (error 'no))
+   'merge-bg-keep (condition-case nil (face-attribute 'my-merge-face :background nil 'default-on) (error 'no))
+   'merge-unspecified (condition-case nil (progn (set-face-attribute 'my-merge-face nil :foreground 'unspecified) (face-attribute 'my-merge-face :foreground nil 'default-on)) (error 'no)))))"##,
+    );
+}
