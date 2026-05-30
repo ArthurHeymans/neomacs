9e679b0a801d2cd0cbf0bb9753821f512171875f
Eval Exec <execvy@gmail.com>
2026-05-30 18:41:58 -0400

    oracle-tests: face_theme add 7 grind tests
    Tests: overlay face plist containing nil, fontify uninterned
    symbol, overlay face EOL propagation, add-face to unfaced region,
    fontify defvar no value, overlay face make/set/read cycle,
    font available families.
    All pass. 1073 total, 36 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -25196,3 +25196,124 @@ fn ft_dig_face_set_face_with_frame_list() {
    'minibuffer-frame (condition-case nil (window-frame (minibuffer-window)) (error 'no)))))"##,
     );
 }
+
+#[test]
+fn ft_grind_face_overlay_face_set_to_plist_containing_nil() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (condition-case err
+          (progn
+            (overlay-put ov 'face '(:foreground nil :background nil :weight nil))
+            (list 'face (overlay-get ov 'face) 'char (get-char-property 3 'face)))
+        (error (list 'err (car err)))))
+    (mapc #'delete-overlay (overlays-in 1 6)))))"##,
+    );
+}
+
+#[test]
+fn ft_grind_font_lock_fontify_uninterned_symbol_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(setq #:my-uninterned 42)\n")
+    (font-lock-fontify-buffer)
+    (list
+     'uninterned-face (save-excursion (goto-char (point-min)) (search-forward "#:my-uninterned") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_grind_face_overlay_face_end_of_line_propagation() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAA\nBBB\nCCC")
+    (put-text-property 2 9 'face 'bold)
+    (list
+     'line1-prop (get-text-property 3 'face)
+     'line2-prop (get-text-property 6 'face)
+     'intervals (length (object-intervals (current-buffer)))))))"##,
+    );
+}
+
+#[test]
+fn ft_grind_face_text_property_add_face_to_unfaced_region() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAABBBBB")
+    (put-text-property 1 6 'face 'bold)
+    (add-face-text-property 8 11 'italic)
+    (list
+     'face-pos1 (get-text-property 1 'face)
+     'face-pos3 (get-text-property 3 'face)
+     'face-pos8 (get-text-property 8 'face)
+     'face-pos10 (get-text-property 10 'face)
+     'intervals (length (object-intervals (current-buffer)))))))"##,
+    );
+}
+
+#[test]
+fn ft_grind_font_lock_fontify_defvar_with_no_value() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(defvar my-var-only)\n")
+    (font-lock-fontify-buffer)
+    (list
+     'var-face (save-excursion (goto-char (point-min)) (search-forward "my-var-only") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_grind_face_overlay_face_make_set_read_cycle() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (overlay-put ov 'face 'bold)
+      (setq ov nil)
+      (let ((ov (make-overlay 1 6)))
+        (overlay-put ov 'face 'italic)
+        (list
+         'face (overlay-get ov 'face))
+        (mapc #'delete-overlay (overlays-in 1 6)))))))"##,
+    );
+}
+
+#[test]
+fn ft_grind_face_face_font_available_families_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (list
+   'font-family-list-fbound (fboundp 'font-family-list)
+   'list-families (condition-case nil (font-family-list) (error 'no))
+   'x-family-fonts-fbound (fboundp 'x-family-fonts)
+   'find-font-fbound (fboundp 'find-font)
+   'available-fonts (condition-case nil (if (fboundp 'x-list-fonts) (x-list-fonts "*") 'none) (error 'no)))))"##,
+    );
+}
