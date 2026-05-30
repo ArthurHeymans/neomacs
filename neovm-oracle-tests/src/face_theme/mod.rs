1df2765b244b944a9867378648fbf4c7a3f1d02c
Eval Exec <execvy@gmail.com>
2026-05-30 19:15:17 -0400

    oracle-tests: face_theme add 4 scorch tests
    Tests: overlay face wrap prefix, fontify track-mouse, overlay
    priority list sort, text property fontified+face mix.
    All pass. 1142 total, 36 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -26511,3 +26511,76 @@ fn ft_burn_face_text_property_face_interval_after_fill() {
      'face-pos-before (list (get-text-property 3 'face) (get-text-property 9 'face) (get-text-property 15 'face))))))"##,
     );
 }
+
+#[test]
+fn ft_scorch_face_overlay_face_wrap_prefix_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (overlay-put ov 'face 'bold)
+      (overlay-put ov 'wrap-prefix "> ")
+      (list
+       'wrap-prefix (overlay-get ov 'wrap-prefix)
+       'face (overlay-get ov 'face)
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_scorch_font_lock_fontify_track_mouse_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(track-mouse\n  (mouse-position))\n")
+    (font-lock-fontify-buffer)
+    (list
+     'track-mouse-face (save-excursion (goto-char (point-min)) (search-forward "track-mouse") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_scorch_face_overlay_priority_list_sort_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov1 (make-overlay 1 6))) (overlay-put ov1 'face '(:foreground "red")) (overlay-put ov1 'priority 10))
+    (let ((ov2 (make-overlay 1 6))) (overlay-put ov2 'face '(:foreground "green")) (overlay-put ov2 'priority 5))
+    (let ((ov3 (make-overlay 1 6))) (overlay-put ov3 'face '(:foreground "blue")) (overlay-put ov3 'priority 15))
+    (list
+     'sorted-highest-first (get-char-property 3 'face)
+     'change-lowest (progn (overlay-put ov2 'priority 20) (get-char-property 3 'face))
+     (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_scorch_face_text_property_fontified_and_face_mix() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "ABCDEF")
+    (put-text-property 1 3 'face 'bold)
+    (put-text-property 1 3 'fontified t)
+    (put-text-property 4 7 'face 'italic)
+    (put-text-property 4 7 'fontified nil)
+    (list
+     'pos1 (list (get-text-property 1 'face) (get-text-property 1 'fontified))
+     'pos3 (list (get-text-property 3 'face) (get-text-property 3 'fontified))
+     'pos5 (list (get-text-property 5 'face) (get-text-property 5 'fontified))
+     'pos6 (list (get-text-property 6 'face) (get-text-property 6 'fontified))))))"##,
+    );
+}
