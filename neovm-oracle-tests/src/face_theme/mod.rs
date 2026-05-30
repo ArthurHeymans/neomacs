595d4038012ed6763919bd41668c32c5db6f570d
Eval Exec <execvy@gmail.com>
2026-05-30 19:37:33 -0400

    oracle-tests: face_theme add 4 rush tests
    Tests: overlay zero-length overlay, fontify plist-get, overlay
    face with multiple empty overlays, text property remove from
    middle of face interval.
    All pass. 1167 total, 37 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -26978,3 +26978,76 @@ fn ft_surge_face_text_property_face_interval_scan_window() {
      'intervals (length (object-intervals (current-buffer)))))))"##,
     );
 }
+
+#[test]
+fn ft_rush_face_overlay_face_zero_length_overlay_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 3 3)))
+      (overlay-put ov 'face 'bold)
+      (list
+       'start (overlay-start ov)
+       'end (overlay-end ov)
+       'face (overlay-get ov 'face)
+       'char-prop (condition-case nil (get-char-property 3 'face) (error 'no))
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_rush_font_lock_fontify_with_property_list() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(plist-get '(:a 1 :b 2) :a)\n")
+    (font-lock-fontify-buffer)
+    (list
+     'plist-get-face (save-excursion (goto-char (point-min)) (search-forward "plist-get") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_rush_face_overlay_face_with_multiple_empty_overlays() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov1 (make-overlay 1 1))) (overlay-put ov1 'face 'bold))
+    (let ((ov2 (make-overlay 3 3))) (overlay-put ov2 'face 'italic))
+    (let ((ov3 (make-overlay 5 5))) (overlay-put ov3 'face 'underline))
+    (list
+     'face-at-start (get-char-property 1 'face)
+     'face-at-mid (get-char-property 3 'face)
+     'face-at-end (get-char-property 5 'face)
+     (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_rush_face_text_property_remove_from_middle_of_face() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAABBBBBCCCCC")
+    (put-text-property 1 16 'face 'bold)
+    (condition-case nil (remove-text-properties 6 11 '(face nil)) (error nil))
+    (list
+     'before-gap (get-text-property 3 'face)
+     'in-gap (get-text-property 8 'face)
+     'after-gap (get-text-property 13 'face)
+     'intervals (length (object-intervals (current-buffer)))))))"##,
+    );
+}
