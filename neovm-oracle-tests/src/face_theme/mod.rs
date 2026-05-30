097ef2dcdd484207a178c5784034f76696568fc8
Eval Exec <execvy@gmail.com>
2026-05-30 19:42:01 -0400

    oracle-tests: face_theme add 4 drill tests
    Tests: overlay face steal from another overlay, fontify nested
    progn triple, overlay face with read-only, text property
    prop stack override.
    All pass. 1179 total, 37 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -27206,3 +27206,76 @@ fn ft_pound_face_text_property_put_get_many_properties() {
        'true-count (/ (length props) 2))))))"##,
     );
 }
+
+#[test]
+fn ft_drill_face_overlay_face_steal_from_another_overlay() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov1 (make-overlay 1 6))) (overlay-put ov1 'face 'bold))
+    (let ((ov2 (make-overlay 1 6))) (overlay-put ov2 'face (overlay-get ov1 'face)))
+    (list
+     'ov1-face (overlay-get ov1 'face)
+     'ov2-face (overlay-get ov2 'face)
+     'same-face (eq (overlay-get ov1 'face) (overlay-get ov2 'face))
+     (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_drill_font_lock_fontify_with_nested_progn_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(progn\n  (progn\n    (progn\n      (setq x 42))))\n")
+    (font-lock-fontify-buffer)
+    (list
+     'outer-progn-face (save-excursion (goto-char (point-min)) (search-forward "progn") (get-text-property (match-beginning 0) 'face))
+     'second-progn-face (save-excursion (goto-char (point-min)) (search-forward "progn" nil nil 2) (get-text-property (match-beginning 0) 'face))
+     'third-progn-face (save-excursion (goto-char (point-min)) (search-forward "progn" nil nil 3) (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_drill_face_overlay_face_with_readonly_property() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (overlay-put ov 'face 'bold)
+      (overlay-put ov 'read-only t)
+      (list
+       'face (overlay-get ov 'face)
+       'read-only (overlay-get ov 'read-only)
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_drill_face_text_property_prop_stack_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (add-text-properties 1 6 '(face bold font-lock-face italic mouse-face highlight))
+    (add-text-properties 1 6 '(face underline))
+    (list
+     'face-after-override (get-text-property 3 'face)
+     'font-lock-still (get-text-property 3 'font-lock-face)
+     'mouse-still (get-text-property 3 'mouse-face)
+     'intervals (length (object-intervals (current-buffer)))))))"##,
+    );
+}
