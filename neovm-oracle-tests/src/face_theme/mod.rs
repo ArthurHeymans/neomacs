eeb0b2ab844d49d7dc31215ac7aff267fc1d9c31
Eval Exec <execvy@gmail.com>
2026-05-31 01:20:26 -0400

    oracle-tests: face_theme add 4 ember tests
    Tests: overlay priority ascending/descending verify, verilog
    mode fontify, overlay face deleted ov no props, text property
    face find by position scan.
    All pass. 1474 total, 47 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -32488,3 +32488,85 @@ fn ft_glow_text_property_interval_detail_each_char() {
      'intervals (length (object-intervals (current-buffer)))))))"##,
     );
 }
+
+#[test]
+fn ft_ember_face_overlay_priority_ascending_descending_verify() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((a (make-overlay 1 6))) (overlay-put a 'face '(:foreground "A")) (overlay-put a 'priority 10))
+    (let ((b (make-overlay 1 6))) (overlay-put b 'face '(:foreground "B")) (overlay-put b 'priority 20))
+    (let ((c (make-overlay 1 6))) (overlay-put c 'face '(:foreground "C")) (overlay-put c 'priority 30))
+    (list
+     'descending-c (get-char-property 3 'face)
+     'ascending-c-to-5 (progn (overlay-put c 'priority 5) (get-char-property 3 'face))
+     'ascending-a-to-35 (progn (overlay-put a 'priority 35) (get-char-property 3 'face))
+     (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_ember_font_lock_verilog_mode_fontify() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (condition-case nil (verilog-mode) (error (fundamental-mode)))
+    (insert "module test;\n  wire clk;\nendmodule\n")
+    (condition-case err
+        (progn (font-lock-fontify-buffer) (list 'fontified (get-text-property 1 'fontified)))
+      (error (list 'err (car err) (cadr err)))))))"##,
+    );
+}
+
+#[test]
+fn ft_ember_face_overlay_face_deleted_ov_no_props() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (overlay-put ov 'face 'bold)
+      (overlay-put ov 'face nil)
+      (list
+       'no-face (overlay-get ov 'face)
+       'char-no-face (get-char-property 3 'face)
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_ember_text_property_face_find_by_position_scan() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAABBBCCCDD")
+    (put-text-property 1 4 'face 'bold)
+    (put-text-property 4 7 'face 'italic)
+    (put-text-property 7 10 'face 'underline)
+    (put-text-property 10 12 'face 'bold)
+    (list
+     'bold-regions (let ((pos 1) (result nil))
+                     (while (< pos 12)
+                       (when (eq (get-text-property pos 'face) 'bold)
+                         (push pos result))
+                       (setq pos (1+ pos)))
+                     (nreverse result))
+     'italic-regions (let ((pos 1) (result nil))
+                       (while (< pos 12)
+                         (when (eq (get-text-property pos 'face) 'italic)
+                           (push pos result))
+                         (setq pos (1+ pos)))
+                       (nreverse result))
+     'intervals (length (object-intervals (current-buffer)))))))"##,
+    );
+}
