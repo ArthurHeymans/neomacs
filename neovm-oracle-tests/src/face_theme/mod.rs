f5ce9a9eefe1558b3bde8e0735ebb78f685feef2
Eval Exec <execvy@gmail.com>
2026-05-30 22:59:40 -0400

    oracle-tests: face_theme add 4 now tests
    Tests: overlay face priority float comparison, fontify
    string-to-number, overlay face string property both sides
    rich, text property find with limit.
    All pass. 1352 total, 46 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -30346,3 +30346,76 @@ fn ft_current_face_text_property_property_chain_length_deep() {
        'intervals (length (object-intervals (current-buffer))))))))"##,
     );
 }
+
+#[test]
+fn ft_now_face_overlay_face_priority_float_comparison() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((f1 (make-overlay 1 6))) (overlay-put f1 'face '(:foreground "float1")) (overlay-put f1 'priority 1.9))
+    (let ((f2 (make-overlay 1 6))) (overlay-put f2 'face '(:foreground "float2")) (overlay-put f2 'priority 2.0))
+    (list
+     'higher-wins (get-char-property 3 'face)
+     (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_now_font_lock_fontify_string_to_number_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(string-to-number \"42\")\n")
+    (font-lock-fontify-buffer)
+    (list
+     'string-to-num-face (save-excursion (goto-char (point-min)) (search-forward "string-to-number") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_now_face_overlay_face_string_property_both_sides_rich() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "MID")
+    (let ((ov (make-overlay 1 4)))
+      (overlay-put ov 'face 'italic)
+      (overlay-put ov 'before-string (propertize "START" 'face 'bold))
+      (overlay-put ov 'after-string (propertize "END" 'face 'underline))
+      (list
+       'before-face (overlay-get ov 'before-string)
+       'after-face (overlay-get ov 'after-string)
+       'core-face (overlay-get ov 'face)
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_now_face_text_property_find_property_with_limit() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAABBBBB")
+    (put-text-property 1 6 'face 'bold)
+    (put-text-property 6 11 'face 'italic)
+    (list
+     'find-bold (text-property-any 1 11 'face 'bold)
+     'find-bold-after-6 (text-property-any 6 11 'face 'bold)
+     'find-italic (text-property-any 1 11 'face 'italic)
+     'find-italic-after-7 (text-property-any 7 11 'face 'italic)
+     'no-underline (text-property-any 1 11 'face 'underline)
+     'intervals (length (object-intervals (current-buffer)))))))"##,
+    );
+}
