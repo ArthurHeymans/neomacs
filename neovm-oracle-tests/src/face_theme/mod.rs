e2c61e719e876095cdd785a3196a5c680807cb2f
Eval Exec <execvy@gmail.com>
2026-05-30 22:54:24 -0400

    oracle-tests: face_theme add 4 current tests
    Tests: overlay face priority big values, fontify copy-sequence,
    overlay face multiple overlays and text prop blend, text
    property chain length.
    All pass. 1348 total, 46 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -30273,3 +30273,76 @@ fn ft_wave_face_text_property_multi_face_interval_sort() {
      'count (length (object-intervals (current-buffer)))))))"##,
     );
 }
+
+#[test]
+fn ft_current_face_overlay_face_priority_big_values_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((big (make-overlay 1 6))) (overlay-put big 'face '(:foreground "big")) (overlay-put big 'priority 1000000))
+    (let ((small (make-overlay 1 6))) (overlay-put small 'face '(:foreground "small")) (overlay-put small 'priority 10))
+    (list
+     'big-wins (get-char-property 3 'face)
+     (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_current_font_lock_fontify_copy_sequence_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(copy-sequence \"hello\")\n")
+    (font-lock-fontify-buffer)
+    (list
+     'copy-seq-face (save-excursion (goto-char (point-min)) (search-forward "copy-sequence") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_current_face_overlay_face_multiple_overlays_and_text_prop_blend() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (put-text-property 1 6 'face 'bold)
+    (let ((ov1 (make-overlay 1 6))) (overlay-put ov1 'face 'italic))
+    (list
+     'char-prop-from-ov (get-char-property 3 'face)
+     'text-prop-unchanged (get-text-property 3 'face)
+     'after-delete-ov (progn (delete-overlay ov1) (get-char-property 3 'face))
+     'text-still (get-text-property 3 'face)))))"##,
+    );
+}
+
+#[test]
+fn ft_current_face_text_property_property_chain_length_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "ABCDE")
+    (put-text-property 1 6 'face 'bold)
+    (let ((count 0) (pos 1) (prev nil))
+      (while (and pos (< pos 6))
+        (let ((cur (get-text-property pos 'face)))
+          (unless (eq cur prev)
+            (setq count (1+ count))
+            (setq prev cur)))
+        (setq pos (1+ pos)))
+      (list
+       'face-changes count
+       'intervals (length (object-intervals (current-buffer))))))))"##,
+    );
+}
