262f30d4be6bea9f9035716090833d1c9cd16c19
Eval Exec <execvy@gmail.com>
2026-05-30 23:14:55 -0400

    oracle-tests: face_theme add 4 blitz tests
    Tests: overlay 4-overlay stress, fontify capitalize, overlay
    face prop inheritance across, text property get all text props.
    All pass. 1368 total, 46 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -30636,3 +30636,75 @@ fn ft_rapid_face_text_property_object_intervals_with_identical_face() {
                  all-bold)))))"##,
     );
 }
+
+#[test]
+fn ft_blitz_face_overlay_face_four_overlay_stress() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((a (make-overlay 1 6))) (overlay-put a 'face '(:foreground "A")) (overlay-put a 'priority 1))
+    (let ((b (make-overlay 1 6))) (overlay-put b 'face '(:foreground "B")) (overlay-put b 'priority 2))
+    (let ((c (make-overlay 1 6))) (overlay-put c 'face '(:foreground "C")) (overlay-put c 'priority 3))
+    (let ((d (make-overlay 1 6))) (overlay-put d 'face '(:foreground "D")) (overlay-put d 'priority 4))
+    (list
+     'D-wins (get-char-property 3 'face)
+     'rotate (progn (overlay-put d 'priority 0) (overlay-put c 'priority 5) (get-char-property 3 'face))
+     (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_blitz_font_lock_fontify_capitalize_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(capitalize \"hello world\")\n")
+    (font-lock-fontify-buffer)
+    (list
+     'capitalize-face (save-excursion (goto-char (point-min)) (search-forward "capitalize") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_blitz_face_overlay_face_prop_inheritance_across_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (overlay-put ov 'face '(:inherit bold :slant italic))
+      (list
+       'face (overlay-get ov 'face)
+       'inherit-val (plist-get (overlay-get ov 'face) :inherit)
+       'slant-val (plist-get (overlay-get ov 'face) :slant)
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_blitz_face_text_property_get_all_text_props_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (put-text-property 1 6 'face 'bold)
+    (put-text-property 1 6 'fontified t)
+    (let ((all-props (text-properties-at 3)))
+      (list
+       'num-pairs (/ (length all-props) 2)
+       'face-key-present (member 'face all-props)
+       'fontified-key-present (member 'fontified all-props))))))"##,
+    );
+}
