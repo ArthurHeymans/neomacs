7c0da49b057b5866e9123d968160382868a24275
Eval Exec <execvy@gmail.com>
2026-05-31 00:09:53 -0400

    oracle-tests: face_theme add 4 dash tests
    Tests: overlay priority inversion stress, CSS mode fontify,
    overlay face with cursor property, text property character
    after insert.
    All pass. 1418 total, 47 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -31501,3 +31501,75 @@ fn ft_sweep_text_property_interval_count_with_many_props() {
                           'face-pos (mapcar (lambda (i) (list (car i) (cadr i) (get-text-property (car i) 'face) (get-text-property (car i) 'fontified))) ints)))))))"##,
     );
 }
+
+#[test]
+fn ft_dash_face_overlay_face_priority_inversion_stress() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((a (make-overlay 1 6))) (overlay-put a 'face '(:foreground "A")) (overlay-put a 'priority 100))
+    (let ((b (make-overlay 1 6))) (overlay-put b 'face '(:foreground "B")) (overlay-put b 'priority 200))
+    (let ((c (make-overlay 1 6))) (overlay-put c 'face '(:foreground "C")) (overlay-put c 'priority 50))
+    (list
+     'B-wins (get-char-property 3 'face)
+     'invert-all (progn (overlay-put b 'priority 10) (overlay-put c 'priority 300) (overlay-put a 'priority 200) (get-char-property 3 'face))
+     'restore (progn (overlay-put a 'priority 100) (overlay-put b 'priority 200) (overlay-put c 'priority 50) (get-char-property 3 'face))
+     (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_dash_font_lock_css_mode_fontify() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (condition-case nil (css-mode) (error (fundamental-mode)))
+    (insert "body { color: red; }\n")
+    (condition-case err
+        (progn (font-lock-fontify-buffer) (list 'fontified (get-text-property 1 'fontified)))
+      (error (list 'err (car err) (cadr err)))))))"##,
+    );
+}
+
+#[test]
+fn ft_dash_face_overlay_face_with_cursor_prop() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (overlay-put ov 'face 'bold)
+      (overlay-put ov 'cursor 'bar)
+      (list
+       'face (overlay-get ov 'face)
+       'cursor (overlay-get ov 'cursor)
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_dash_text_property_face_char_after_insert() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (put-text-property 1 6 'face 'bold)
+    (goto-char 3)
+    (insert "X")
+    (list
+     'char-before-x (get-text-property 2 'face)
+     'x-face (get-text-property 3 'face)
+     'char-after-x (get-text-property 4 'face)
+     'intervals (length (object-intervals (current-buffer)))))))"##,
+    );
+}
