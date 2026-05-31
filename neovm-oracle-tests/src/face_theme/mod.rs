32dd3ea0d61e6f9e63e674d32f06ec6c27ca5b33
Eval Exec <execvy@gmail.com>
2026-05-31 02:11:27 -0400

    oracle-tests: face_theme add 4 stretch tests
    Tests: overlay face put/unface/reface cycle, idlwave mode
    fontify, overlay face compare before/after nil, text property
    get positions with face.
    All pass. 1520 total, 49 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -33337,3 +33337,78 @@ fn ft_climb_text_property_face_with_add_face_deep() {
      'intervals (length (object-intervals (current-buffer)))))))"##,
     );
 }
+
+#[test]
+fn ft_stretch_face_overlay_face_put_unface_then_reface_cycle() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (list
+       'r1 (progn (overlay-put ov 'face 'bold) (overlay-get ov 'face))
+       'r2 (progn (overlay-put ov 'face nil) (overlay-get ov 'face))
+       'r3 (progn (overlay-put ov 'face 'italic) (overlay-get ov 'face))
+       'r4 (progn (overlay-put ov 'face nil) (overlay-get ov 'face))
+       'r5 (progn (overlay-put ov 'face 'underline) (overlay-get ov 'face))
+       'r6 (progn (overlay-put ov 'face nil) (overlay-get ov 'face))
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_stretch_font_lock_idlwave_mode_fontify() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (condition-case nil (idlwave-mode) (error (fundamental-mode)))
+    (insert "pro hello\n  print, 'hello'\nend\n")
+    (condition-case err
+        (progn (font-lock-fontify-buffer) (list 'fontified (get-text-property 1 'fontified)))
+      (error (list 'err (car err) (cadr err)))))))"##,
+    );
+}
+
+#[test]
+fn ft_stretch_face_overlay_face_compare_overlay_before_after_nil() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (overlay-put ov 'face 'bold)
+      (overlay-put ov 'face nil)
+      (list
+       'no-face-left (overlay-get ov 'face)
+       'char-no-face (get-char-property 3 'face)
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_stretch_text_property_face_get_positions_with_face() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAABBBB")
+    (put-text-property 1 9 'face 'bold)
+    (list
+     'all-positions (let ((pos 1) (result nil))
+                      (while (<= pos 8)
+                        (when (eq (get-text-property pos 'face) 'bold)
+                          (push pos result))
+                        (setq pos (1+ pos)))
+                      (nreverse result))
+     'all-not-bold (text-property-not-all 1 9 'face 'bold nil)
+     'not-bold-pos (text-property-any 1 9 'face nil nil)))))"##,
+    );
+}
