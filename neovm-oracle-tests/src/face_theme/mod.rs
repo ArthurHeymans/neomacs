c547757a4a612b35b632a14f282b11e7a1954d4b
Eval Exec <execvy@gmail.com>
2026-05-30 23:13:19 -0400

    oracle-tests: face_theme add 4 rapid tests
    Tests: overlay compare face after reorder, fontify downcase,
    overlay text prop char prop discrepancy, text property
    object intervals with identical face.
    All pass. 1364 total, 46 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -30561,3 +30561,78 @@ fn ft_fast_face_text_property_read_modify_read_cycle() {
         (list v1 v2 (get-text-property 3 'face) (length (object-intervals (current-buffer)))))))))"##,
     );
 }
+
+#[test]
+fn ft_rapid_face_overlay_face_compare_face_after_reorder() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov1 (make-overlay 1 6))) (overlay-put ov1 'face '(:foreground "A")) (overlay-put ov1 'priority 2))
+    (let ((ov2 (make-overlay 1 6))) (overlay-put ov2 'face '(:foreground "B")) (overlay-put ov2 'priority 1))
+    (let ((ov3 (make-overlay 1 6))) (overlay-put ov3 'face '(:foreground "C")) (overlay-put ov3 'priority 3))
+    (list
+     'sorted (get-char-property 3 'face)
+     'reverse (progn (overlay-put ov1 'priority 0) (overlay-put ov2 'priority 4) (overlay-put ov3 'priority 2) (get-char-property 3 'face))
+     (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_rapid_font_lock_fontify_downcase_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(downcase \"HELLO\")\n")
+    (font-lock-fontify-buffer)
+    (list
+     'downcase-face (save-excursion (goto-char (point-min)) (search-forward "downcase") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_rapid_face_overlay_text_prop_char_prop_discrepancy() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (put-text-property 1 6 'face 'bold)
+    (let ((ov (make-overlay 1 6)))
+      (overlay-put ov 'face 'italic)
+      (list
+       'text-prop (get-text-property 3 'face)
+       'char-prop (get-char-property 3 'face)
+       'different (not (eq (get-text-property 3 'face) (get-char-property 3 'face)))
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_rapid_face_text_property_object_intervals_with_identical_face() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (put-text-property 1 3 'face 'bold)
+    (put-text-property 3 6 'face 'bold)
+    (list
+     'intervals (length (object-intervals (current-buffer)))
+     'all-bold (let ((pos 1) (all-bold t))
+                 (while (<= pos 5)
+                   (unless (eq (get-text-property pos 'face) 'bold)
+                     (setq all-bold nil))
+                   (setq pos (1+ pos)))
+                 all-bold)))))"##,
+    );
+}
