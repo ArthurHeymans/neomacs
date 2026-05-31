da72596eeed0b7ce5b293c6cfb302ccf7763a9ee
Eval Exec <execvy@gmail.com>
2026-05-30 23:29:59 -0400

    oracle-tests: face_theme add 4 mach tests
    Tests: overlay priority reverse top two, fontify replace-match,
    overlay face reset via nil, text property multi prop interval
    analysis.
    All pass. 1380 total, 46 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -30854,3 +30854,79 @@ fn ft_turbo_face_text_property_fast_cycle_put_remove() {
       (list 'cycle-results (nreverse results) 'intervals (length (object-intervals (current-buffer))))))))"##,
     );
 }
+
+#[test]
+fn ft_mach_face_overlay_face_priority_reverse_top_two() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((a (make-overlay 1 6))) (overlay-put a 'face '(:foreground "A")) (overlay-put a 'priority 10))
+    (let ((b (make-overlay 1 6))) (overlay-put b 'face '(:foreground "B")) (overlay-put b 'priority 20))
+    (let ((c (make-overlay 1 6))) (overlay-put c 'face '(:foreground "C")) (overlay-put c 'priority 30))
+    (list
+     'C-wins (get-char-property 3 'face)
+     'B-to-40-then-A-to-50 (progn (overlay-put b 'priority 40) (overlay-put a 'priority 50) (get-char-property 3 'face))
+     'C-to-0 (progn (overlay-put c 'priority 0) (get-char-property 3 'face))
+     (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_mach_font_lock_fontify_replace_match_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(replace-match \"new\")\n")
+    (font-lock-fontify-buffer)
+    (list
+     'replace-match-face (save-excursion (goto-char (point-min)) (search-forward "replace-match") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_mach_face_overlay_face_reset_face_via_nil() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (overlay-put ov 'face 'bold)
+      (overlay-put ov 'face 'italic)
+      (overlay-put ov 'face nil)
+      (overlay-put ov 'face 'underline)
+      (list
+       'face (overlay-get ov 'face)
+       'char-prop (get-char-property 3 'face)
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_mach_face_text_property_multi_prop_interval_analysis() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "123456")
+    (put-text-property 1 3 'face 'bold)
+    (put-text-property 1 3 'fontified t)
+    (put-text-property 4 7 'face 'italic)
+    (put-text-property 4 7 'fontified nil)
+    (list
+     'intervals (length (object-intervals (current-buffer)))
+     'pos1 (list (get-text-property 1 'face) (get-text-property 1 'fontified))
+     'pos2 (list (get-text-property 2 'face) (get-text-property 2 'fontified))
+     'pos5 (list (get-text-property 5 'face) (get-text-property 5 'fontified))))))"##,
+    );
+}
