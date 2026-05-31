596b60e655ce637ea67a0e2ab616491421051ba0
Eval Exec <execvy@gmail.com>
2026-05-30 23:09:45 -0400

    oracle-tests: face_theme add 4 fast tests
    Tests: overlay face verify plist cycle, fontify upcase,
    overlay create/read/delete sequence, text property
    read/modify/read cycle.
    All pass. 1360 total, 46 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -30492,3 +30492,72 @@ fn ft_go_face_text_property_exhaustive_prop_changes_history() {
       (nreverse history)))))"##,
     );
 }
+
+#[test]
+fn ft_fast_face_overlay_face_verify_plist_cycle() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (overlay-put ov 'face '(:foreground "red"))
+      (overlay-put ov 'face '(:foreground "green"))
+      (overlay-put ov 'face '(:foreground "blue"))
+      (list
+       'current (overlay-get ov 'face)
+       'fg (plist-get (overlay-get ov 'face) :foreground)
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_fast_font_lock_fontify_upcase_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(upcase \"hello\")\n")
+    (font-lock-fontify-buffer)
+    (list
+     'upcase-face (save-excursion (goto-char (point-min)) (search-forward "upcase") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_fast_face_overlay_create_read_delete_sequence() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (list
+     'step1 (progn (let ((ov (make-overlay 1 6))) (overlay-put ov 'face 'bold) (delete-overlay ov)) 'ok)
+     'step2 (progn (let ((ov (make-overlay 1 6))) (overlay-put ov 'face 'italic) (delete-overlay ov)) 'ok)
+     'step3 (progn (let ((ov (make-overlay 1 6))) (overlay-put ov 'face 'underline) (delete-overlay ov)) 'ok)
+     'no-ov-left (length (overlays-in 1 6)))))"##,
+    );
+}
+
+#[test]
+fn ft_fast_face_text_property_read_modify_read_cycle() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "HELLO")
+    (put-text-property 1 6 'face 'bold)
+    (let ((v1 (get-text-property 3 'face)))
+      (put-text-property 1 6 'face 'italic)
+      (let ((v2 (get-text-property 3 'face)))
+        (put-text-property 1 6 'face 'underline)
+        (list v1 v2 (get-text-property 3 'face) (length (object-intervals (current-buffer)))))))))"##,
+    );
+}
