b71bb4399ec925b844a9712e3c3d560623d1bb5a
Eval Exec <execvy@gmail.com>
2026-05-31 01:00:09 -0400

    oracle-tests: face_theme add 4 zap tests
    Tests: overlay face attribute mix, awk mode fontify, overlay
    get all properties no face, text property face single char.
    All pass. 1458 total, 47 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -32209,3 +32209,72 @@ fn ft_burst_text_property_face_interval_starts_and_ends() {
        'details (mapcar (lambda (i) (list (car i) (cadr i) (plist-get (cddr i) 'face))) ints)))))))"##,
     );
 }
+
+#[test]
+fn ft_zap_face_overlay_face_attribute_mix_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (overlay-put ov 'face '(:foreground "gold" :background "navy" :underline (:color "red" :style wave) :strike-through t))
+      (let ((pl (overlay-get ov 'face)))
+        (list
+         'len (length pl)
+         'fg (plist-get pl :foreground)
+         'bg (plist-get pl :background)
+         'underline (plist-get pl :underline)
+         'strike (plist-get pl :strike-through)
+         (progn (delete-overlay ov) 'cleaned)))))))"##,
+    );
+}
+
+#[test]
+fn ft_zap_font_lock_awk_mode_fontify() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (condition-case nil (awk-mode) (error (fundamental-mode)))
+    (insert "BEGIN { print \"hello\" }\n")
+    (condition-case err
+        (progn (font-lock-fontify-buffer) (list 'fontified (get-text-property 1 'fontified)))
+      (error (list 'err (car err) (cadr err)))))))"##,
+    );
+}
+
+#[test]
+fn ft_zap_face_overlay_get_all_properties_no_face() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (list
+       'no-props (overlay-properties ov)
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_zap_text_property_face_single_char_bold_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "A")
+    (put-text-property 1 2 'face 'bold)
+    (list
+     'face (get-text-property 1 'face)
+     'intervals (length (object-intervals (current-buffer)))
+     'next-prop (next-single-property-change 1 'face)
+     'prev-prop (previous-single-property-change 2 'face)))))"##,
+    );
+}
