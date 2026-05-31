82bea192c0e6308077d4cc2ce0b77e1d66a216c9
Eval Exec <execvy@gmail.com>
2026-05-30 22:24:19 -0400

    oracle-tests: face_theme add 4 charge tests
    Tests: overlay face multi-step priority swap, fontify remhash,
    overlay face typed insert verify propagation, text property
    face multibyte boundary.
    All pass. 1324 total, 46 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -29840,3 +29840,78 @@ fn ft_sprint_face_text_property_next_property_on_single_interval() {
      'intervals (length (object-intervals (current-buffer)))))))"##,
     );
 }
+
+#[test]
+fn ft_charge_face_overlay_face_multi_step_priority_swap() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((a (make-overlay 1 6))) (overlay-put a 'face '(:foreground "A")) (overlay-put a 'priority 1))
+    (let ((b (make-overlay 1 6))) (overlay-put b 'face '(:foreground "B")) (overlay-put b 'priority 2))
+    (let ((c (make-overlay 1 6))) (overlay-put c 'face '(:foreground "C")) (overlay-put c 'priority 3))
+    (list
+     'init (get-char-property 3 'face)
+     'a-to-4-b-to-1-c-to-2 (progn (overlay-put a 'priority 4) (overlay-put b 'priority 1) (overlay-put c 'priority 2) (get-char-property 3 'face))
+     'reset (progn (overlay-put a 'priority 1) (overlay-put b 'priority 2) (overlay-put c 'priority 3) (get-char-property 3 'face))
+     (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_charge_font_lock_fontify_remhash_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(remhash 'key table)\n")
+    (font-lock-fontify-buffer)
+    (list
+     'remhash-face (save-excursion (goto-char (point-min)) (search-forward "remhash") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_charge_face_overlay_face_typed_insert_verify_propagation() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "START")
+    (let ((ov (make-overlay 2 5)))
+      (overlay-put ov 'face 'bold)
+      (goto-char 4)
+      (insert "-MID-")
+      (list
+       'expanded (list (overlay-start ov) (overlay-end ov))
+       'face-pos2 (get-char-property 2 'face)
+       'face-pos5 (get-char-property 5 'face)
+       'face-pos-last (get-char-property (1- (overlay-end ov)) 'face)
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_charge_face_text_property_face_multibyte_boundary_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "A日本語B")
+    (put-text-property 1 7 'face 'bold)
+    (list
+     'ascii-before (get-text-property 1 'face)
+     'multibyte-mid1 (get-text-property 2 'face)
+     'multibyte-mid2 (get-text-property 4 'face)
+     'ascii-after (get-text-property 6 'face)
+     'intervals (length (object-intervals (current-buffer)))))))"##,
+    );
+}
