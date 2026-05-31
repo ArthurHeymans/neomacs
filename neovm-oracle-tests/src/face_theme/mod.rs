9efa5245de6e3adce7059da3550fbf9b3fe42062
Eval Exec <execvy@gmail.com>
2026-05-30 20:43:24 -0400

    oracle-tests: face_theme add 4 ultimate tests
    Tests: overlay face three-way priority tie, fontify
    atomic-change-group, overlay last ordered same priority,
    text property classify faces.
    All pass. 1244 total, 40 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -28401,3 +28401,76 @@ fn ft_countless_face_text_property_near_bob_and_eob() {
      'prev-from-4 (previous-single-property-change 4 'face)))))"##,
     );
 }
+
+#[test]
+fn ft_ultimate_face_overlay_face_three_way_priority_tie() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov1 (make-overlay 1 6))) (overlay-put ov1 'face '(:foreground "red")) (overlay-put ov1 'priority 5))
+    (let ((ov2 (make-overlay 1 6))) (overlay-put ov2 'face '(:foreground "green")) (overlay-put ov2 'priority 5))
+    (let ((ov3 (make-overlay 1 6))) (overlay-put ov3 'face '(:foreground "blue")) (overlay-put ov3 'priority 5))
+    (list
+     'three-tie (get-char-property 3 'face)
+     (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_ultimate_font_lock_fontify_with_atomic_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(atomic-change-group\n  (setq x 1)\n  (setq y 2))\n")
+    (font-lock-fontify-buffer)
+    (list
+     'atomic-face (save-excursion (goto-char (point-min)) (search-forward "atomic-change-group") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_ultimate_face_overlay_last_ordered_same_priority() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((first (make-overlay 1 6))) (overlay-put first 'face '(:foreground "first")) (overlay-put first 'priority 1))
+    (let ((last (make-overlay 1 6))) (overlay-put last 'face '(:foreground "last")) (overlay-put last 'priority 1))
+    (list
+     'last-wins (get-char-property 3 'face)
+     (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_ultimate_face_text_property_classify_faces_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "ABCDEF")
+    (put-text-property 1 3 'face 'bold)
+    (put-text-property 4 7 'face 'italic)
+    (list
+     'classify (let ((pos 1) (bolds nil) (italics nil) (others nil))
+                 (while (<= pos 6)
+                   (let ((f (get-text-property pos 'face)))
+                     (cond ((eq f 'bold) (push pos bolds))
+                           ((eq f 'italic) (push pos italics))
+                           (t (push pos others))))
+                   (setq pos (1+ pos)))
+                 (list (nreverse bolds) (nreverse italics) (nreverse others)))
+     'intervals (length (object-intervals (current-buffer)))))))"##,
+    );
+}
