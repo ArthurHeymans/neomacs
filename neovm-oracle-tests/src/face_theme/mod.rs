3b95377900af5fac63ec59607d1e02a1124f3049
Eval Exec <execvy@gmail.com>
2026-05-30 21:28:42 -0400

    oracle-tests: face_theme add 4 forever tests
    Tests: overlay face priority increase/decrease cycle, fontify
    keyword constant, overlay delete all then check, text property
    face save restore buffer.
    All pass. 1280 total, 44 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -29054,3 +29054,73 @@ fn ft_ceaseless_face_text_property_object_equals_deep() {
        'different-obj-intervals-differ (/= (length (object-intervals (current-buffer))) (length (object-intervals str))))))))"##,
     );
 }
+
+#[test]
+fn ft_forever_face_overlay_face_priority_increase_decrease_cycle() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov1 (make-overlay 1 6))) (overlay-put ov1 'face '(:foreground "A")) (overlay-put ov1 'priority 1))
+    (let ((ov2 (make-overlay 1 6))) (overlay-put ov2 'face '(:foreground "B")) (overlay-put ov2 'priority 2))
+    (let ((ov3 (make-overlay 1 6))) (overlay-put ov3 'face '(:foreground "C")) (overlay-put ov3 'priority 3))
+    (list
+     'init (get-char-property 3 'face)
+     'ov1-to-4 (progn (overlay-put ov1 'priority 4) (get-char-property 3 'face))
+     'ov3-to-0 (progn (overlay-put ov3 'priority 0) (get-char-property 3 'face))
+     'ov2-to-5 (progn (overlay-put ov2 'priority 5) (get-char-property 3 'face))
+     (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_forever_font_lock_fontify_keyword_constant_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(defconst pi 3.14159)\n")
+    (font-lock-fontify-buffer)
+    (list
+     'const-name-face (save-excursion (goto-char (point-min)) (search-forward "pi") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_forever_face_overlay_delete_all_then_check() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6))) (overlay-put ov 'face 'bold) (delete-overlay ov))
+    (list
+     'no-overlays (length (overlays-in 1 6))
+     'no-face (get-char-property 3 'face)))))"##,
+    );
+}
+
+#[test]
+fn ft_forever_face_text_property_face_save_restore_buffer() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "SAVE-ME")
+    (put-text-property 1 8 'face 'bold)
+    (list
+     'before-copy (get-text-property 3 'face)
+     'copy-has-face (with-temp-buffer
+                      (insert-buffer-substring (current-buffer))
+                      (list 'has-face (get-text-property 3 'face) 'intervals (length (object-intervals (current-buffer)))))
+     'original-still-has (get-text-property 3 'face)))))"##,
+    );
+}
