69eac22e9c754666cc1f6b596e9bc640f81d7f51
Eval Exec <execvy@gmail.com>
2026-05-30 22:22:08 -0400

    oracle-tests: face_theme add 4 sprint tests
    Tests: overlay face high numeric vs symbolic prio, fontify
    plist-member, overlay face OOB access, text property next
    property on single interval.
    All pass. 1320 total, 46 divergences.

--- neovm-oracle-tests/src/face_theme/mod.rs
+++ neovm-oracle-tests/src/face_theme/mod.rs
@@ -29769,3 +29769,74 @@ fn ft_advance_face_text_property_text_property_any_on_entire_buffer() {
      'intervals (length (object-intervals (current-buffer)))))))"##,
     );
 }
+
+#[test]
+fn ft_sprint_face_overlay_face_high_numeric_vs_symbolic_prio() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov-num (make-overlay 1 6))) (overlay-put ov-num 'face '(:foreground "red")) (overlay-put ov-num 'priority 100))
+    (let ((ov-sym (make-overlay 1 6))) (overlay-put ov-sym 'face '(:foreground "green")) (overlay-put ov-sym 'priority 'high))
+    (list
+     'winning (get-char-property 3 'face)
+     (progn (mapc #'delete-overlay (overlays-in 1 6)) 'cleaned)))))"##,
+    );
+}
+
+#[test]
+fn ft_sprint_font_lock_fontify_plist_member_deep() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (require 'font-lock)
+  (with-temp-buffer
+    (emacs-lisp-mode)
+    (insert "(plist-member '(:a 1 :b 2) :a)\n")
+    (font-lock-fontify-buffer)
+    (list
+     'plist-member-face (save-excursion (goto-char (point-min)) (search-forward "plist-member") (get-text-property (match-beginning 0) 'face))
+     'fontified (get-text-property 1 'fontified)))))"##,
+    );
+}
+
+#[test]
+fn ft_sprint_face_overlay_face_oob_access() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (let ((ov (make-overlay 1 6)))
+      (overlay-put ov 'face 'bold)
+      (list
+       'oob-start (condition-case nil (get-char-property 0 'face) (error 'out))
+       'oob-end (condition-case nil (get-char-property 7 'face) (error 'out))
+       'valid (get-char-property 5 'face)
+       (progn (delete-overlay ov) 'cleaned))))))"##,
+    );
+}
+
+#[test]
+fn ft_sprint_face_text_property_next_property_on_single_interval() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(progn
+  (with-temp-buffer
+    (insert "AAAAA")
+    (put-text-property 1 6 'face 'bold)
+    (list
+     'next-from-1 (next-single-property-change 1 'face)
+     'next-from-2 (next-single-property-change 2 'face)
+     'next-from-5 (next-single-property-change 5 'face)
+     'prev-from-6 (previous-single-property-change 6 'face)
+     'prev-from-5 (previous-single-property-change 5 'face)
+     'prev-from-2 (previous-single-property-change 2 'face)
+     'intervals (length (object-intervals (current-buffer)))))))"##,
+    );
+}
