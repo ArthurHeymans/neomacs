9e76befc16de3bc7ede4c026a3d594c9f77e0666
Eval Exec <execvy@gmail.com>
2026-06-18 10:02:35 -0400

    test(oracle): charset/mule encode + registry combos; +2 divergences
    Passing parity coverage (9): encode unencodable chars (substitution bytes for
    us-ascii/iso-8859-1), find-charset-string / find-charset-region, make-char /
    charset-after / decode-char, encode-char per charset, char-charset with
    restriction list, coding-system-eol-type vector for undecided, decode invalid
    utf-8 bytes.
    
    Divergences found (kept as #[ignore]d parity tests; both fail today):
    
    30. split-char returns nil instead of (CHARSET CODE...): GNU gives
        (split-char ?A) => (ascii 65), hiragana-a => (unicode-bmp 48 66).
    31. charset-priority-list returns only a handful of charsets (ascii, emacs,
        latin-iso8859-1, eight-bit, ...); GNU returns the full registered charset
        set (~180, incl. big5, windows-1252, CJK). neomacs has a limited charset
        registry (cf. find-coding-systems-string #27).

---
+++ neovm-oracle-tests/src/divergence_charset_split_priority_runtime.rs
@@ -0,0 +1,120 @@
+//! Charset/mule parity: encode unencodable (substitution bytes), find-charset-
+//! string/region, make-char/charset-after/decode-char, encode-char per charset,
+//! char-charset with restriction, eol-type vector, decode invalid utf-8; plus
+//! split-char and charset-priority-list registry divergences.
+
+use super::common::assert_oracle_parity;
+use super::common::return_if_neovm_enable_oracle_proptest_not_set;
+
+#[test]
+fn char_charset_restrict() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(list (char-charset ?A '(ascii latin-iso8859-1))
+        (char-charset ?\s) (char-charset ?\t))"##,
+    );
+}
+
+#[test]
+fn decode_invalid_utf8() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(let ((d (decode-coding-string (unibyte-string 255 254 65) 'utf-8)))
+  (list (length d) (multibyte-string-p d)))"##,
+    );
+}
+
+#[test]
+fn encode_char_charsets() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(list (encode-char ?A 'ascii) (encode-char ?あ 'japanese-jisx0208)
+        (decode-char 'ascii 65) (charsetp 'japanese-jisx0208))"##,
+    );
+}
+
+#[test]
+fn encode_unencodable() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(list (append (encode-coding-string "a日b" 'us-ascii) nil)
+        (append (encode-coding-string "café" 'iso-8859-1) nil)
+        (append (encode-coding-string "日" 'iso-8859-1) nil))"##,
+    );
+}
+
+#[test]
+fn encode_unencodable_ascii() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(let ((r (encode-coding-string "héllo" 'us-ascii)))
+  (list (length r) (multibyte-string-p r) (append (string-to-unibyte r) nil)))"##,
+    );
+}
+
+#[test]
+fn eol_type_undecided_vector() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(list (coding-system-eol-type 'undecided)
+        (coding-system-eol-type 'utf-8)
+        (vectorp (coding-system-eol-type 'undecided)))"##,
+    );
+}
+
+#[test]
+fn find_charset_region() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(with-temp-buffer
+  (insert "hello 日本")
+  (sort (mapcar #'symbol-name (find-charset-region (point-min) (point-max))) #'string<))"##,
+    );
+}
+
+#[test]
+fn find_charset_string() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(list (find-charset-string "abc")
+        (find-charset-string "café")
+        (sort (mapcar #'symbol-name (find-charset-string "a日")) #'string<))"##,
+    );
+}
+
+#[test]
+fn make_char_basic() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(list (make-char 'ascii 65) (characterp (make-char 'ascii 65)) (charset-after) (decode-char 'ascii 65))"##,
+    );
+}
+
+#[test]
+#[ignore = "DIVERGENCE: split-char returns nil instead of (CHARSET CODE...): GNU gives (split-char ?A) => (ascii 65), ?-hiragana-a => (unicode-bmp 48 66)."]
+fn divergence_split_char_returns_nil() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(r##"(list (split-char ?A) (split-char ?あ) (split-char ?0))"##);
+}
+
+#[test]
+#[ignore = "DIVERGENCE: charset-priority-list returns only a handful of charsets (ascii/emacs/latin-iso8859-1/eight-bit/...); GNU returns the full registered set (~180, incl. big5, windows-1252, CJK). neomacs has a limited charset registry."]
+fn divergence_charset_priority_list_size() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(list (> (length (charset-priority-list)) 50)
+      (and (memq 'big5 (charset-priority-list)) t)
+      (and (memq 'windows-1252 (charset-priority-list)) t))"##,
+    );
+}
--- neovm-oracle-tests/src/lib.rs
+++ neovm-oracle-tests/src/lib.rs
@@ -1230,6 +1230,7 @@ mod divergence_casefold_matrix;
 mod divergence_category_unicode_props;
 mod divergence_charset_plist_matrix;
 mod divergence_charset_roundtrip_runtime;
+mod divergence_charset_split_priority_runtime;
 mod divergence_chartab_charset_deep;
 mod divergence_chartab_syntax_deep;
 mod divergence_cl_lib_advanced;
