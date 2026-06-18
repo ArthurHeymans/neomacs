c3a80b8235412cd8dcc54add9070e32c478069bb
Eval Exec <execvy@gmail.com>
2026-06-18 08:36:43 -0400

    test(oracle): stdlib (map/rx/seq/ring/gen), error-data, list/arity/gv combos
    33 more parity tests, all matching GNU. These confirm broad standard-library,
    error-handling, and generalized-variable behavior is faithful (no divergences):
    
    - divergence_map_rx_seq_lib_runtime.rs (15): map.el elt/keys/values/merge/
      filter/apply; rx build + match + rx pcase pattern; seq group-by/difference/
      intersection/mapn/position/find/count/sort-by/min/max/into/concat/seq-let;
      ring; generator iter-defun; cl-print; bindat pack/unpack; pcase map pattern.
    - divergence_error_data_runtime.rs (10): condition-case error symbol + data +
      error-message-string for wrong-type-argument, arith-error, void-function/
      variable, wrong-number-of-arguments, args-out-of-range, custom signal /
      user-error / error, no-catch throw, cl-assert / cl-the. Error symbol, data,
      and message text all match.
    - divergence_list_arity_gv_runtime.rs (8): flatten-tree/ensure-list/
      proper-list-p, take/ntake/last/butlast, alist-get/plist-get (incl. testfn),
      assq-delete-all/rassq, number-sequence, func-arity/subr-arity, and gv/setf
      places (nth, aref, alist-get, plist-get, cl-incf, push).

---
+++ neovm-oracle-tests/src/divergence_error_data_runtime.rs
@@ -0,0 +1,111 @@
+//! Error parity: condition-case error symbol + data + message text for
+//! wrong-type-argument, arith-error, void-function/variable, wrong-number-
+//! of-arguments, args-out-of-range, custom signal/user-error/error, no-catch
+//! throw, and cl-assert/cl-the.
+
+use super::common::assert_oracle_parity;
+use super::common::return_if_neovm_enable_oracle_proptest_not_set;
+
+#[test]
+fn err_args_number() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(list (condition-case e (funcall (lambda (a b) (+ a b)) 1) (error (car e)))
+        (condition-case e (car) (error (car e)))
+        (condition-case e (cons 1) (error (car e))))"##,
+    );
+}
+
+#[test]
+fn err_arith() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(list (condition-case e (/ 1 0) (error (cons (car e) (cdr e))))
+        (condition-case e (/ 1.0 0) (error (cons (car e) (cdr e)))))"##,
+    );
+}
+
+#[test]
+fn err_cl_assert() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'cl-lib)
+(list (condition-case e (cl-assert (= 1 2)) (cl-assertion-failed 'assert-failed) (error (car e)))
+      (condition-case e (cl-the integer "x") (error (car e))))"##,
+    );
+}
+
+#[test]
+fn err_message_text() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(list (condition-case e (+ 1 "x") (error (error-message-string e)))
+        (condition-case e (/ 1 0) (error (error-message-string e))))"##,
+    );
+}
+
+#[test]
+fn err_range() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(list (condition-case e (nth -1 '(1 2)) (error (cons (car e) (cdr e))))
+        (condition-case e (substring "ab" 0 9) (error (cons (car e) (cdr e))))
+        (condition-case e (char-to-string -1) (error (cons (car e) (cdr e)))))"##,
+    );
+}
+
+#[test]
+fn err_throw_no_catch() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(condition-case e (throw 'no-such-tag 42) (no-catch (cons 'nocatch (cdr e))) (error (car e)))"##,
+    );
+}
+
+#[test]
+fn err_user_signal() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(list (condition-case e (signal 'my-custom-err '(1 2 3)) (my-custom-err (cons 'caught (cdr e))) (error 'other))
+        (condition-case e (user-error "custom %d" 42) (user-error (cadr e)))
+        (condition-case e (error "plain %s" "msg") (error (cadr e))))"##,
+    );
+}
+
+#[test]
+fn err_void() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(list (condition-case e (funcall 'neo-no-such-fn-xyz) (error (cons (car e) (cdr e))))
+        (condition-case e (symbol-value 'neo-no-such-var-xyz) (error (cons (car e) (cdr e)))))"##,
+    );
+}
+
+#[test]
+fn err_wrong_number() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(list (condition-case e (signal 'wrong-number-of-arguments '(foo 3)) (error (cons (car e) (cdr e))))
+        (condition-case e (let ((x)) (setq x)) (error (car e))))"##,
+    );
+}
+
+#[test]
+fn err_wrong_type() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(list (condition-case e (+ 1 "x") (error (cons (car e) (cdr e))))
+        (condition-case e (car 5) (error (cons (car e) (cdr e))))
+        (condition-case e (aref [1 2] 9) (error (cons (car e) (cdr e)))))"##,
+    );
+}
---
+++ neovm-oracle-tests/src/divergence_map_rx_seq_lib_runtime.rs
@@ -0,0 +1,183 @@
+//! Standard-library parity: map.el (elt/keys/values/merge/filter/apply),
+//! rx (build + match + pcase rx pattern), seq advanced (group-by/difference/
+//! intersection/mapn/into/seq-let), ring, generator (iter-defun), cl-print,
+//! bindat pack/unpack, and pcase map pattern.
+
+use super::common::assert_oracle_parity;
+use super::common::return_if_neovm_enable_oracle_proptest_not_set;
+
+#[test]
+fn map_elt_put() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'map)
+(let ((al '((a . 1) (b . 2))) (pl '(:x 1 :y 2)) (h (make-hash-table :test 'eq)))
+  (puthash 'k 9 h)
+  (list (map-elt al 'a) (map-elt pl :y) (map-elt h 'k) (map-elt al 'z 'default)))"##,
+    );
+}
+
+#[test]
+fn map_filter_apply() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'map)
+(list (map-filter (lambda (k _v) (eq k 'a)) '((a . 1) (b . 2)))
+      (sort (map-apply (lambda (k v) (cons v k)) '((a . 1) (b . 2))) (lambda (x y) (< (car x) (car y)))))"##,
+    );
+}
+
+#[test]
+fn map_merge_keys() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'map)
+(list (map-keys '((a . 1) (b . 2))) (map-values '((a . 1) (b . 2)))
+      (map-merge 'list '((a . 1)) '((b . 2))) (map-length '((a . 1) (b . 2))))"##,
+    );
+}
+
+#[test]
+fn rx_basic() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'rx)
+(list (rx (+ digit)) (rx bol (group (any "a-z")) eol)
+      (rx (or "foo" "bar")) (rx (= 3 "ab")))"##,
+    );
+}
+
+#[test]
+fn rx_match() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'rx)
+(list (string-match (rx (+ digit)) "abc123") (string-match (rx word-boundary "the" word-boundary) "the cat")
+      (replace-regexp-in-string (rx (+ space)) "_" "a  b   c"))"##,
+    );
+}
+
+#[test]
+fn rx_pcase() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'rx)
+(pcase "2024-01-15"
+  ((rx (let y (= 4 digit)) "-" (let m (= 2 digit)) "-" (let d (= 2 digit)))
+   (list y m d)))"##,
+    );
+}
+
+#[test]
+fn seq_advanced() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'seq)
+(list (seq-group-by #'cl-evenp '(1 2 3 4 5 6))
+      (seq-difference '(1 2 3 4) '(2 4))
+      (seq-intersection '(1 2 3) '(2 3 4))
+      (seq-mapn #'+ '(1 2 3) '(10 20 30)))"##,
+    );
+}
+
+#[test]
+fn seq_into_concat() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'seq)
+(list (seq-into '(1 2 3) 'vector) (seq-into [1 2 3] 'list)
+      (seq-into "abc" 'list) (seq-concatenate 'list '(1 2) '(3 4))
+      (seq-reverse [1 2 3]))"##,
+    );
+}
+
+#[test]
+fn seq_let_destructure() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'seq)
+(seq-let (a b &rest c) '(1 2 3 4 5) (list a b c))"##,
+    );
+}
+
+#[test]
+fn seq_more() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'seq)
+(list (seq-position '(a b c) 'b) (seq-find #'cl-evenp '(1 3 4 5))
+      (seq-count #'cl-evenp '(1 2 3 4)) (seq-sort-by #'- #'< '(1 2 3))
+      (seq-min '(3 1 2)) (seq-max '(3 1 2)))"##,
+    );
+}
+
+#[test]
+fn bindat_pack() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'bindat)
+(let ((spec (bindat-type (n uint 16) (m uint 8))))
+  (let ((packed (bindat-pack spec '((n . 258) (m . 5)))))
+    (list (append (string-to-unibyte packed) nil)
+          (let ((u (bindat-unpack spec packed))) (list (bindat-get-field u 'n) (bindat-get-field u 'm))))))"##,
+    );
+}
+
+#[test]
+fn cl_print() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'cl-print)
+(let ((print-circle t))
+  (list (cl-prin1-to-string '(1 2 3)) (cl-prin1-to-string "str")
+        (cl-prin1-to-string [1 2 3])))"##,
+    );
+}
+
+#[test]
+fn generator_iter() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'generator)
+(iter-defun neo-range (n) (dotimes (i n) (iter-yield (* i i))))
+(let ((acc nil) (it (neo-range 4)))
+  (condition-case nil (while t (push (iter-next it) acc)) (iter-end-of-sequence nil))
+  (nreverse acc))"##,
+    );
+}
+
+#[test]
+fn pcase_rx_map() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'map)
+(pcase '((name . "x") (age . 30))
+  ((map name age) (list name age)))"##,
+    );
+}
+
+#[test]
+fn ring_ops() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+
+    assert_oracle_parity(
+        r##"(require 'ring)
+(let ((r (make-ring 3)))
+  (ring-insert r 'a) (ring-insert r 'b) (ring-insert r 'c) (ring-insert r 'd)
+  (list (ring-length r) (ring-ref r 0) (ring-ref r 1) (ring-elements r)))"##,
+    );
+}
