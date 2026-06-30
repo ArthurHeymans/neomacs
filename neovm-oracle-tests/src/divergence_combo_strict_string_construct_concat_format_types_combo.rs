7e7132dd871200c12fa28f4770d0de33a614fcfe
Eval Exec <execvy@gmail.com>
2026-06-30 16:04:30 -0400

    test(oracle): add strict combo probes batches 199-201 (variable binding, mapcar mixed types, string construction+format) using expect! format
    Batch 199 (divergence_combo_strict_variable_binding_boundp_set_makunbound_combo):
    boundp/fboundp, symbol-value/set, default-boundp/default-value, makunbound +
    void-variable signal, fmakunbound + void-function signal, and buffer-local +
    default-value + kill-local-variable combo.
    
    Batch 200 (divergence_combo_strict_mapcar_mapc_mixed_types_mapconcat_mapcan_combo):
    mapcar/mapc over strings/vectors/lists, mapcar with multiple sequences, mapconcat
    over strings+lists, mapcan, and seq-map/seq-map-indexed/seq-filter/remove/elt/
    into over vector+string+list.
    
    Batch 201 (divergence_combo_strict_string_construct_concat_format_types_combo):
    string/unibyte-string from char args, concat of strings + list-of-chars +
    vector-of-chars, make-string, format %s/%S over records/vectors/markers/
    bool-vectors, and multibyte-string-p / string-bytes / mixed unibyte+multibyte
    concat.
    
    Divergences found this cycle: NONE. All 9 new tests pass in NEOVM_ORACLE_MODE=live
    against GNU Emacs on PATH.
    
    cargo fmt --all and cargo check --workspace are clean.

---
+++ neovm-oracle-tests/src/divergence_combo_strict_string_construct_concat_format_types_combo.rs
@@ -0,0 +1,67 @@
+//! Strict combo oracle probes, batch 201: string construction + format-on-types.
+//! concat of strings/list-of-chars/vector-of-chars, string/unibyte-string from
+//! char args, mixed unibyte/multibyte concat, and format %s/%S/%d over
+//! records/vectors/markers.
+//! Uses assert_oracle_parity_expect format.
+
+use super::common::return_if_neovm_enable_oracle_proptest_not_set;
+
+#[test]
+fn div_v8_string_construct_concat_list_vector_chars() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+    let form = r##"
+(list (string ?a ?b ?c)
+      (apply #'string '(?x ?y ?z))
+      (concat "uni" " код" "日本")
+      (concat '(?a ?b ?c))
+      (concat [?x ?y])
+      (unibyte-string 65 66 67)
+      (make-string 5 ?*)
+      (make-string 3 ?日)
+      (concat "a" "b" "c"))
+"##;
+    let expect = expect_test::expect![[r#""""#]];
+    crate::common::assert_oracle_parity_expect(form, expect);
+}
+
+#[test]
+fn div_v8_format_s_over_records_vectors_markers() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+    let form = r##"
+(with-temp-buffer
+  (insert "marker-test")
+  (let ((m (set-marker (make-marker) 3)))
+    (list (format "%s" (vector 1 2 3))
+          (format "%S" (vector 1 2 3))
+          (format "%s" (record 'foo 1 2))
+          (format "%S" (record 'foo 1 2))
+          (format "%s" [a b c])
+          (format "%s" "plain")
+          (format "%S" "with quotes")
+          (format "%d" 42)
+          (format "%s" '(a b c))
+          (format "%s" (make-bool-vector 3 t)))))
+"##;
+    let expect = expect_test::expect![[r#""""#]];
+    crate::common::assert_oracle_parity_expect(form, expect);
+}
+
+#[test]
+fn div_v8_concat_mixed_unibyte_multibyte_bytes() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+    let form = r##"
+(list (multibyte-string-p "abc")
+      (multibyte-string-p "日本")
+      (multibyte-string-p (unibyte-string 200))
+      (string-bytes "abc")
+      (string-bytes "日本")
+      (string-bytes (unibyte-string 200))
+      (length "日本")
+      (length (unibyte-string 200))
+      (concat "abc" (string ?日))
+      (multibyte-string-p (concat "abc" (string ?日)))
+      (aref (unibyte-string 200) 0)))
+"##;
+    let expect = expect_test::expect![[r#""""#]];
+    crate::common::assert_oracle_parity_expect(form, expect);
+}
