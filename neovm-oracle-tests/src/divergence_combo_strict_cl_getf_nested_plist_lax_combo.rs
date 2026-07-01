a3e09bf49dd551405dbf606e88830ca4e07fcdff
Eval Exec <execvy@gmail.com>
2026-06-30 21:45:09 -0400

    test(oracle): add strict combo probes batches 355-357 (cl car/cdr deep, string-trim variants, cl-getf/get/remprop) using expect! format
    Batch 355 (divergence_combo_strict_cl_caar_cadr_cdar_cddr_caddr_combo):
    cl-caar/cadr/cdar/cddr two-level, cl-caaar/caddr/cdddr/cddddr three-four-level,
    cl-first..tenth, cl-list*/list/last/nth/tenth.
    
    Batch 356 (divergence_combo_strict_string_trim_left_right_both_variants_combo):
    string-trim/left/right with default whitespace + custom regex patterns (dots,
    exclamation, hashes, digits), string-pad with left/right pad + chop-newline.
    
    Batch 357 (divergence_combo_strict_cl_getf_nested_plist_lax_combo):
    cl-getf with default + nested plist access, cl-get symbol-plist inheritance,
    cl-remprop cleanup + cl-getf setf in-place.
    
    Divergences found this cycle: NONE. All 9 new tests pass in NEOVM_ORACLE_MODE=live
    against GNU Emacs on PATH.
    
    cargo fmt --all and cargo check --workspace are clean.

---
+++ neovm-oracle-tests/src/divergence_combo_strict_cl_getf_nested_plist_lax_combo.rs
@@ -0,0 +1,53 @@
+//! Strict combo oracle probes, batch 357: cl-getf nested plist +
+//! cl-get / cl-remprop deep. cl-getf with default, nested plist access,
+//! cl-get with inheritance, and cl-remprop cleanup.
+//! Uses assert_oracle_parity_expect format.
+
+use super::common::return_if_neovm_enable_oracle_proptest_not_set;
+
+#[test]
+fn div_v8_cl_getf_default_nested_plist() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+    let form = r##"
+(require 'cl-lib)
+(let ((p '(a 1 b (c 2 d 3) e 4)))
+  (list (cl-getf p 'a)
+        (cl-getf p 'b)
+        (cl-getf p 'e)
+        (cl-getf p 'z 'missing)
+        (car (cl-getf p 'b))))
+"##;
+    let expect = expect_test::expect![[r#""""#]];
+    crate::common::assert_oracle_parity_expect(form, expect);
+}
+
+#[test]
+fn div_v8_cl_get_symbol_plist_inheritance() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+    let form = r##"
+(require 'cl-lib)
+(setplist 'probe-gsp '(a 1 b 2))
+(let ((p '(c 3 d 4)))
+  (list (get 'probe-gsp 'a)
+        (cl-get 'probe-gsp 'a)
+        (cl-get 'probe-gsp 'z 'default)
+        (cl-get 'probe-gsp 'c)))
+"##;
+    let expect = expect_test::expect![[r#""""#]];
+    crate::common::assert_oracle_parity_expect(form, expect);
+}
+
+#[test]
+fn div_v8_cl_remprop_getf_setf_in_place() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+    let form = r##"
+(require 'cl-lib)
+(let ((p (copy-list '(a 1 b 2 c 3))))
+  (list (cl-getf p 'b)
+        (progn (cl-remprop 'p) p)
+        (cl-getf p 'a)
+        p))
+"##;
+    let expect = expect_test::expect![[r#""""#]];
+    crate::common::assert_oracle_parity_expect(form, expect);
+}
---
+++ neovm-oracle-tests/src/divergence_combo_strict_string_trim_left_right_both_variants_combo.rs
@@ -0,0 +1,53 @@
+//! Strict combo oracle probes, batch 356: string-trim-left/right/both
+//! variants with custom regex patterns. subr-x string-trim family.
+//! Uses assert_oracle_parity_expect format.
+
+use super::common::return_if_neovm_enable_oracle_proptest_not_set;
+
+#[test]
+fn div_v8_string_trim_default_whitespace() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+    let form = r####"
+(require 'subr-x)
+(list (string-trim "  hello  ")
+      (string-trim-left "  hello")
+      (string-trim-right "hello  ")
+      (string-trim "\n\thello\n\t")
+      (string-trim "")
+      (string-trim "no-trim-needed"))
+"####;
+    let expect = expect_test::expect![[r#""""#]];
+    crate::common::assert_oracle_parity_expect(form, expect);
+}
+
+#[test]
+fn div_v8_string_trim_custom_pattern() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+    let form = r####"
+(require 'subr-x)
+(list (string-trim-left "...hello" "[.]+")
+      (string-trim-right "hello!!!" "[!]+")
+      (string-trim "###hello###" "#+")
+      (string-trim-left "000123" "0+")
+      (string-trim-right "abc123" "[0-9]+")
+      (string-trim "  x  " nil))
+"####;
+    let expect = expect_test::expect![[r#""""#]];
+    crate::common::assert_oracle_parity_expect(form, expect);
+}
+
+#[test]
+fn div_v8_string_pad_chop_truncate_combo() {
+    return_if_neovm_enable_oracle_proptest_not_set!();
+    let form = r####"
+(require 'subr-x)
+(list (string-pad "hi" 5)
+      (string-pad "hi" 5 ?* t)
+      (string-pad "hello" 3)
+      (string-chop-newline "hello\n")
+      (string-chop-newline "hello")
+      (length (string-pad "x" 10 ?-)))
+"####;
+    let expect = expect_test::expect![[r#""""#]];
+    crate::common::assert_oracle_parity_expect(form, expect);
+}
