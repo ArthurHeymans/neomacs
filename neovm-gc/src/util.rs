32455710c912b8d0febc37f0641269557fb49ae3
Eval Exec <execvy@gmail.com>
2026-07-02 02:38:24 -0400

    remove neovm-gc,neovm-executor,neovm-compiler

--- neovm-gc/src/util.rs
+++
@@ -1,8 +0,0 @@
-/// Round `addr` up to the next multiple of `align`.
-/// `align` must be a non-zero power of two.
-#[inline]
-pub(crate) fn align_up(addr: usize, align: usize) -> usize {
-    debug_assert!(align.is_power_of_two(), "alignment must be a power of two");
-    let mask = align.wrapping_sub(1);
-    (addr.wrapping_add(mask)) & !mask
-}
